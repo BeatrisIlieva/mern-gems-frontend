504ec746c3027f040a0888bf35423508821e65b0
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-20 18:44:41 +0300

    Updated nav

--- src/components/Header/NavItem/NavItem.js
+++ src/components/Header/NavItem/NavItem.js
@@ -6,19 +6,20 @@ export const NavItem = ({ path, title, index }) => {
   return (
     <li>
       <NavLink
-      className={({isActive}) => isActive ?  styles["selected"] : `${styles["nav-item"]} ${styles[`nav-item-${index}`]}`}
+        className={({ isActive }) =>
+          isActive
+            ? styles["selected"]
+            : `${styles["nav-item"]} ${styles[`nav-item-${index}`]}`
+        }
         to={path}
       >
-        <h1 className={styles["nav-title"]}>{title}</h1>
+        {({ isActive }) => isActive ? (
+          <h1 className={styles["nav-title-selected"]}>{title}</h1>
+        ): (
+          <h1 className={styles["nav-title"]}>{title}</h1>
+        )}
+        
       </NavLink>
     </li>
   );
 };
-
-<NavLink
-className={({ isActive }) =>
-  isActive ? styles["selected"] : styles[ "sub-nav-title"]}
-to={"orders"}
->
-Order History
-</NavLink>
