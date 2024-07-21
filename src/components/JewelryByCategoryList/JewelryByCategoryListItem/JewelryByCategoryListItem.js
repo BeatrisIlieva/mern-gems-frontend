8b19e35c7bdaa5c77bbec8fbab5f9422fc6d9794
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-21 12:38:25 +0300

    Added component

--- src/components/JewelryByCategoryList/JewelryByCategoryListItem/JewelryByCategoryListItem.js
+++ src/components/JewelryByCategoryList/JewelryByCategoryListItem/JewelryByCategoryListItem.js
@@ -1,3 +1,18 @@
-export const JewelryByCategoryListItem = () => {
-    
-}
+export const JewelryByCategoryListItem = ({
+  _id,
+  firstImageUrl,
+  jewelryTitle,
+  categoryTitle,
+}) => {
+  const slugifiedCategoryTitle = slugify(categoryTitle);
+  const slugifiedJewelryTitle = slugify(jewelryTitle);
+
+  return (
+    <JewelryCard
+      jewelryId={_id}
+      firstImageUrl={firstImageUrl}
+      slugifiedCategoryTitle={slugifiedCategoryTitle}
+      slugifiedJewelryTitle={slugifiedJewelryTitle}
+    />
+  );
+};
