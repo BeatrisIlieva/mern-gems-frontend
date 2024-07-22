82aab95ccf678edc8adfb42fa4e1f7f4afb7f8dd
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 15:13:30 +0300

    Updated user functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import { RouteGuard } from "./components/RouteGuard/RouteGuard";
 import { ScrollToTop } from "./components/ScrollToTop/ScrollToTop";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
@@ -33,17 +34,77 @@ function App() {
       <main className={styles["main"]}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/bracelets" element={<JewelryList />} />
-          <Route path="/earrings" element={<JewelryList />} />
-          <Route path="/necklaces" element={<JewelryList />} />
-          <Route path="/rings" element={<JewelryList />} />
-          <Route path="/forget-me-not" element={<JewelryList />} />
-          <Route path="/sparkling-cluster" element={<JewelryList />} />
-          <Route path="/sunflower" element={<JewelryList />} />
-          <Route path="/diamond-loop" element={<JewelryList />} />
+          <Route
+            path="/bracelets"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/earrings"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/necklaces"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/rings"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/forget-me-not"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/sparkling-cluster"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/sunflower"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
+          <Route
+            path="/diamond-loop"
+            element={
+              <RouteGuard>
+                <JewelryList />
+              </RouteGuard>
+            }
+          />
           <Route
             path="/:slugifiedCategoryTitle/:slugifiedJewelryTitle/:jewelryId"
-            element={<JewelryItem />}
+            element={
+              <RouteGuard>
+                <JewelryItem />
+              </RouteGuard>
+            }
           />
         </Routes>
       </main>
---
+++ src/components/RouteGuard/RouteGuard.js
@@ -0,0 +1,14 @@
+import { Navigate, Outlet, useLocation} from "react-router-dom";
+// import { useAuthContext } from "../../contexts/AuthContext";
+
+export const RouteGuard = ({ children }) => {
+  const  isAuthenticated  = false;
+  // const { isAuthenticated } = useAuthContext();
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" state={{ from: location }}/>;
+  }
+
+  return children ? children : <Outlet />;
+};
