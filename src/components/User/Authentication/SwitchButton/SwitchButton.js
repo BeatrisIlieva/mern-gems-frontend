63e34cf10c28d92914877ed7a5370954d3dd8bb9
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 18:24:41 +0300

    Updated user functionality

--- src/components/User/Authentication/SwitchButton/SwitchButton.js
+++ src/components/User/Authentication/SwitchButton/SwitchButton.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 import styles from "./SwitchButton.module.css";
 
-export const SwitchButton = ({
-  text,
-  title,
-  switchPopupHandler,
-  option,
-}) => {
+export const SwitchButton = ({ text, title, switchPopupHandler, option }) => {
   return (
     <div className={styles["wrapper"]}>
       <span>{text}</span>
-      <button onClick={() => switchPopupHandler(option)} className={styles["button"]}>{title}</button>
+      <button
+        onClick={() => switchPopupHandler(option)}
+        className={styles["button"]}
+      >
+        {title}
+      </button>
     </div>
   );
 };
