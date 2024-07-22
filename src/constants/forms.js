97f4dfb56fb2e3ed1801b1beaa9fd71f87c03bac
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 19:45:09 +0300

    Updated form functionality

--- src/constants/forms.js
+++ src/constants/forms.js
@@ -11,15 +11,7 @@
 
 
 
-const ZIP_CODE_LENGTH = {
-  MIN: 4,
-  MAX: 15,
-};
 
-export const ZIP_CODE_PATTERN = new RegExp(
-  `^[A-Za-z0-9 ]{${ZIP_CODE_LENGTH.MIN},${ZIP_CODE_LENGTH.MAX}}$`
-);
-const ZIP_CODE_ERROR_MESSAGE = `* This field requires ${ZIP_CODE_LENGTH.MIN}-${ZIP_CODE_LENGTH.MAX} characters`;
 
 export const SUCCESS_MESSAGES = {
   newPassword: PASSWORD_SUCCESS_MESSAGE,
