46069f81bef1759b2aea21938cdfae2be08d5ba9
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 11:27:12 +0300

    Updated jewelry list

--- src/components/JewelryList/JewelryList.js
+++ src/components/JewelryList/JewelryList.js
@@ -54,30 +54,4 @@ export const JewelryList = () => {
       </section>
     </>
   );
-
-  // return (
-  //   <>
-  //     {loading ? (
-  //       <LoadingSpinner />
-  //     ) : (
-  //       <section className={styles["jewelries"]}>
-  //         <div className={styles["jewelries-count"]}>
-  //           Showing 1 -{" "}
-  //           {totalCount >= jewelries.length ? jewelries.length : totalCount} of{" "}
-  //           {totalCount}
-  //         </div>
-  //         <div className={styles["jewelry-grid"]}>
-  //           {jewelries.map((j) => (
-  //             <JewelryListItem key={j._id} {...j} />
-  //           ))}
-  //         </div>
-  //         {loadMore && (
-  //           <div className={styles["button"]} onClick={handleLoadMore}>
-  //             <AnimatedButton title={"Load More"} />
-  //           </div>
-  //         )}
-  //       </section>
-  //     )}
-  //   </>
-  // );
 };
