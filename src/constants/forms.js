6b826b890056f0bf6db9cb4d713d13cb937f72e4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 19:41:40 +0300

    Updated form functionality

--- src/constants/forms.js
+++ src/constants/forms.js
@@ -1,14 +1,6 @@
 
 
-export const EMAIL_PATTERN = /^[A-Za-z0-9]+@+[a-z]+\.[a-z]{2,4}$/;
-const EMAIL_ERROR_MESSAGE = "* Ensure you enter a valid email address";
-const EMAIL_MISMATCH_ERROR_MESSAGE =
-  "* Ensure that both email fields contain the same email address";
-export const EMAIL_ALREADY_EXISTS_ERROR_MESSAGE =
-  "This email address is already registered. Please log in or use a different one";
 
-export const INVALID_CREDENTIALS_ERROR_MESSAGE =
-  "We couldn't find an account matching the email and password you entered";
 
 const NAME_LENGTH = {
   MIN: 2,
