a0bbb69e5d72946bd844f54c727c0a37a2b5ceb7
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 16:34:50 +0300

    Updated user functionality

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -8,23 +8,3 @@ export const Popup = ({ isVisible, children }) => {
     </section>
   );
 };
-
-
-// export const ParentComponent = () => {
-//     const [isPopupVisible, setIsPopupVisible] = useState(false);
-  
-//     const togglePopup = () => {
-//       setIsPopupVisible(!isPopupVisible);
-//     };
-  
-//     return (
-//       <div className={styles["app"]}>
-//         <button onClick={togglePopup}>Toggle Popup</button>
-//         <Popup isVisible={isPopupVisible}>
-//           <h1>This is the Popup Content</h1>
-//           <p>Here you can render any component or content dynamically.</p>
-//           <button onClick={togglePopup}>Close</button>
-//         </Popup>
-//       </div>
-//     );
-//   };
