329425a2acc188442b5694119a97e708edf728bf
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-22 19:47:19 +0300

    Updated form functionality

---
+++ src/constants/cardNumber.js
@@ -0,0 +1,9 @@
+export const LONG_CARD_NUMBER_PATTERN = /^\d{16}$/;
+
+export const LONG_CARD_NUMBER_PATTERN_ERROR_MESSAGE =
+  "The card number should be exactly 16 digits long";
+
+export const CARD_HOLDER_NAME_PATTERN = /^[a-zA-Z\s'-]{2,50}$/;
+
+export const CARD_HOLDER_NAME_PATTERN_ERROR_MESSAGE =
+  "Ensure you enter a valid name";
--- src/constants/forms.js
+++ src/constants/forms.js
@@ -15,14 +15,7 @@
 
 
 
-export const LONG_CARD_NUMBER_PATTERN = /^\d{16}$/;
 
-const LONG_CARD_NUMBER_PATTERN_ERROR_MESSAGE =
-  "The card number should be exactly 16 digits long";
-
-export const CARD_HOLDER_NAME_PATTERN = /^[a-zA-Z\s'-]{2,50}$/;
-
-const CARD_HOLDER_NAME_PATTERN_ERROR_MESSAGE = "Ensure you enter a valid name";
 
 export const CVV_CODE_PATTERN = /^\d{3}$/;
 
