a1b76cd1bca62ee3170d5582c816d396f42a8ed4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-24 19:13:36 +0300

    Updated user functionality

--- src/components/Account/AccountManagement/DeleteAccount/DeleteAccount.js
+++ src/components/Account/AccountManagement/DeleteAccount/DeleteAccount.js
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "./DeleteAccount.module.css";
 
 import { Popup } from "../../../Popup/Popup";
 import { LargeTitle } from "../../../LargeTitle/LargeTitle";
 import { SmallTitle } from "../../../SmallTitle/SmallTitle";
 import { Button } from "../../../Button/Button";
+
+import { useService } from "../../../../hooks/useService";
+import { useAuthenticationContext } from "../../../../contexts/AuthenticationContext";
+import { userServiceFactory } from "../../../../services/userService";
 
 const LargeTitleContent = "Delete Account";
 const SmallTitleContent = "Are you sure you want to delete your Account?";
@@ -14,6 +20,19 @@ export const DeleteAccount = ({
   toggleDeleteAccountPopup,
   displayDeleteAccountPopup,
 }) => {
+  const { userId, clearToken } = useAuthenticationContext();
+  const userService = useService(userServiceFactory);
+
+  const navigate = useNavigate();
+
+  const deleteHandler = async () => {
+    navigate("/");
+
+    await userService.delete(userId);
+
+    await clearToken();
+  };
+
   return (
     <>
       {displayDeleteAccountPopup && (
@@ -25,8 +44,16 @@ export const DeleteAccount = ({
           <LargeTitle title={LargeTitleContent} />
           <SmallTitle title={SmallTitleContent} />
           <div className={styles["button-wrapper"]}>
-            <Button variant={"pink-button"} title={GoBackButtonTitle} />
-            <Button variant={"red-button"} title={DeleteAccountButtonTitle} />
+            <Button
+              variant={"pink-button"}
+              title={GoBackButtonTitle}
+              callBackFunction={toggleDeleteAccountPopup}
+            />
+            <Button
+              variant={"red-button"}
+              title={DeleteAccountButtonTitle}
+              callBackFunction={deleteHandler}
+            />
           </div>
         </Popup>
       )}
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,5 @@
 import styles from "./Button.module.css";
 
-export const Button = ({ variant, title }) => {
-  return <button className={styles[variant]}>{title}</button>;
+export const Button = ({ variant, title, callBackFunction }) => {
+  return <button className={styles[variant]} onClick={() => callBackFunction()}>{title}</button>;
 };
