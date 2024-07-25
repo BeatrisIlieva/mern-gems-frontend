cf5aa1c33f77158132c49e6b86c9d82ae1c4a940
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-25 16:23:48 +0300

    Updated bag functionality

---
+++ src/services/bagService.js
@@ -0,0 +1,23 @@
+import { requestFactory } from "./requester";
+import { HOST } from "../constants/host";
+
+const baseUrl = `${HOST}/bag`;
+
+export const bagServiceFactory = (token) => {
+  const request = requestFactory(token);
+
+  return {
+    create: (data, jewelryId) =>
+      request.post(`${baseUrl}/create/${jewelryId}`, data),
+
+    findAll: (userId) => request.get(`${baseUrl}/find-all/${userId}`),
+
+    findCount: () => request.get(`${baseUrl}/find-count`),
+
+    decrease: (bagId) => request.put(`${baseUrl}/decrease/${bagId}`),
+
+    increase: (bagId) => request.put(`${baseUrl}/increase/${bagId}`),
+
+    delete: (bagId) => request.delete(`${baseUrl}/delete/${bagId}`),
+  };
+};
