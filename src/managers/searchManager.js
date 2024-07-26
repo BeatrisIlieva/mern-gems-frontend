672f930d6fd0a864e7bdea56001cbf7c199550ac
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-26 17:03:01 +0300

    Updated search functionality

--- src/managers/searchManager.js
+++ src/managers/searchManager.js
@@ -1,116 +1,5 @@
 const Jewelry = require("../models/Jewelry");
 
-// exports.getAll = async (search) => {
-//   const searchTerms = search
-//     .split(" ")
-//     .map((term) => term.trim())
-//     .filter((term) => term.length > 0);
-//   const regexTerms = searchTerms.map((term) => new RegExp(term, "i"));
-
-//   const searchResult = await Jewelry.aggregate([
-//     {
-//       $lookup: {
-//         as: "inventories",
-//         from: "inventories",
-//         foreignField: "jewelry",
-//         localField: "_id",
-//       },
-//     },
-//     {
-//       $lookup: {
-//         as: "categories",
-//         from: "categories",
-//         foreignField: "_id",
-//         localField: "category",
-//       },
-//     },
-//     {
-//       $lookup: {
-//         as: "collections",
-//         from: "collections",
-//         foreignField: "_id",
-//         localField: "collection",
-//       },
-//     },
-//     {
-//       $lookup: {
-//         as: "jewelrystones",
-//         from: "jewelrystones",
-//         foreignField: "jewelry",
-//         localField: "_id",
-//       },
-//     },
-//     {
-//       $lookup: {
-//         as: "stonetypes",
-//         from: "stonetypes",
-//         foreignField: "_id",
-//         localField: "jewelrystones.stoneType",
-//       },
-//     },
-//     {
-//       $lookup: {
-//         as: "stonecolors",
-//         from: "stonecolors",
-//         foreignField: "_id",
-//         localField: "jewelrystones.stoneColor",
-//       },
-//     },
-
-//     {
-//       $match: {
-//         $and: [
-//           ...regexTerms.map((regex) => ({
-//             $or: [
-//               { title: { $regex: regex } },
-//               { "categories.title": { $regex: regex } },
-//               { "collections.title": { $regex: regex } },
-//               { "stonetypes.title": { $regex: regex } },
-//               { "stonecolors.title": { $regex: regex } },
-//             ],
-//           })),
-//         ],
-//       },
-//     },
-//     {
-//       $group: {
-//         _id: "$_id",
-//         firstImageUrl: {
-//           $addToSet: "$firstImageUrl",
-//         },
-//         jewelryIds: {
-//           $push: "$_id",
-//         },
-//         categoryTitle: {
-//           $addToSet: "$categories.title",
-//         },
-//         collectionTitle: {
-//           $addToSet: "$collections.title",
-//         },
-//         jewelryTitle: {
-//           $addToSet: "$title",
-//         },
-//       },
-//     },
-//     {
-//       $project: {
-//         firstImageUrl: 1,
-//         jewelryIds: 1,
-//         categoryTitle: 1,
-//         collectionTitle: 1,
-//         jewelryTitle: 1,
-//       },
-//     },
-//     {
-//       $sort: {
-//         _id: 1,
-//       },
-//     },
-//   ]);
-
-//   return searchResult;
-// };
-
 exports.getAll = async (search) => {
   const searchTerms = search
     .split(" ")
@@ -118,7 +7,6 @@ exports.getAll = async (search) => {
     .filter((term) => term.length > 0);
   const regexTerms = searchTerms.map((term) => new RegExp(term, "i"));
 
-  // Define the base aggregation pipeline
   const pipeline = [
     {
       $lookup: {
@@ -168,7 +56,6 @@ exports.getAll = async (search) => {
         localField: "jewelrystones.stoneColor",
       },
     },
-    // Conditionally add the $match stage if search terms are present
     ...(searchTerms.length > 0
       ? [
           {
@@ -185,7 +72,7 @@ exports.getAll = async (search) => {
             },
           },
         ]
-      : []), // No $match stage if search is empty
+      : []),
     {
       $group: {
         _id: "$_id",
