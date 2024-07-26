245f16beaedfd132152125e0e4aca023936d8769
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-26 18:39:28 +0300

    Updated user functionality

--- src/components/Account/ShippingAndOrders/ShippingDetails/initialFormValues.js
+++ src/components/Account/ShippingAndOrders/ShippingDetails/initialFormValues.js
@@ -7,6 +7,8 @@ import {
 } from "../../../../../constants/forms";
 
 const FORM_KEYS = {
+  FirstName: "firstName",
+  LastName: "lastName",
   PhoneNumber: "phoneNumber",
   Country: "country",
   City: "city",
@@ -16,6 +18,30 @@ const FORM_KEYS = {
 };
 
 export const INITIAL_FORM_VALUES = {
+  [FORM_KEYS.FirstName]: {
+    fieldLabel: "First Name",
+    fieldValue: "",
+    regexPattern: NAME_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+    fieldType: "text",
+    validTestData: "Test",
+    invalidTestData: "Test1",
+    emptyTestData: "",
+  },
+
+  [FORM_KEYS.LastName]: {
+    fieldLabel: "Last Name",
+    fieldValue: "",
+    regexPattern: NAME_PATTERN,
+    errorMessage: "",
+    isFocused: false,
+    fieldType: "text",
+    validTestData: "Test",
+    invalidTestData: "Test1",
+    emptyTestData: "",
+  },
+  
   [FORM_KEYS.PhoneNumber]: {
     fieldLabel: "Phone Number *",
     fieldValue: "",
