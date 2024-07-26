a49d6bb0a43f557383697f1f2202efa04738bcab
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-26 12:47:14 +0300

    Updated jewelry functionality

--- src/components/JewelryList/JewelryListItem/JewelryCard/JewelryCard.js
+++ src/components/JewelryList/JewelryListItem/JewelryCard/JewelryCard.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 
 import styles from "./JewelryCard.module.css";
 
-
 export const JewelryCard = ({
   firstImageUrl,
   jewelryId,
@@ -10,39 +9,18 @@ export const JewelryCard = ({
   slugifiedJewelryTitle,
   isSoldOut,
 }) => {
-
   return (
-<article className={styles["jewelry-card"]}>
-<Link
-      to={`/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}
-    >
-      <img
-        src={firstImageUrl}
-        alt={slugifiedJewelryTitle}
-        className={isSoldOut ? `${styles["sold-out"]}` : `${styles["image"]}`}
-      />
-      {isSoldOut && <span className={styles["sold-out-span"]}>SOLD OUT</span>}
-    </Link>
-</article>
+    <article className={styles["jewelry-card"]}>
+      <Link
+        to={`/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}
+      >
+        <img
+          src={firstImageUrl}
+          alt={slugifiedJewelryTitle}
+          className={isSoldOut ? `${styles["sold-out"]}` : `${styles["image"]}`}
+        />
+        {isSoldOut && <span className={styles["sold-out-span"]}>SOLD OUT</span>}
+      </Link>
+    </article>
   );
 };
-
-
-// export const JewelryCard = ({ firstImageUrl, jewelryTitle, isSoldOut }) => {
-//   return (
-//     <article className={styles["jewelry-card"]}>
-//       <div
-//         className={`${styles["jewelry-card-thumbnail"]} ${
-//           isSoldOut === true ? styles["sold-out"] : ""
-//         }`.trim()}
-//       >
-//         <img
-//           className={styles["jewelry-card-img"]}
-//           src={firstImageUrl}
-//           alt={jewelryTitle}
-//         />
-//       </div>
-//       {isSoldOut && <span className={styles["sold-out-span"]}>SOLD OUT</span>}
-//     </article>
-//   );
-// };
