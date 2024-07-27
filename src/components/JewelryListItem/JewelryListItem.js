38eb6c86be2c5c5823c5e4c6aea0d08817fc27ea
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-27 15:58:10 +0300

    Updated bag functionality

--- src/components/JewelryListItem/JewelryListItem.js
+++ src/components/JewelryListItem/JewelryListItem.js
@@ -1,25 +1,16 @@
 import { JewelryCard } from "../JewelryCard/JewelryCard";
 
-import { slugify } from "../../utils/slugify";
-
 export const JewelryListItem = ({
   _id,
   jewelryTitle,
   categoryTitle,
   firstImageUrl,
-  // jewelryTitle,
-  // categoryTitle,
   isSoldOut,
 }) => {
-  // const slugifiedCategoryTitle = slugify(categoryTitle);
-  // const slugifiedJewelryTitle = slugify(jewelryTitle);
-
   return (
     <JewelryCard
       jewelryId={_id}
       firstImageUrl={firstImageUrl}
-      // slugifiedCategoryTitle={slugifiedCategoryTitle}
-      // slugifiedJewelryTitle={slugifiedJewelryTitle}
       isSoldOut={isSoldOut}
       categoryTitle={categoryTitle}
       jewelryTitle={jewelryTitle}
