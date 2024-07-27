d71be5564fcfeb1e5f5a7debe76b86d134d253b4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-27 11:31:57 +0300

    Updated bag functionality

--- src/components/Bag/Bag.js
+++ src/components/Bag/Bag.js
@@ -1,13 +1,20 @@
 import { useService } from "../../hooks/useService";
+import { useBagContext } from "../../contexts/BagContext";
 
 import { bagServiceFactory } from "../../services/bagService";
 
 import { Popup } from "../Popup/Popup";
+import { LargeTitle } from "../LargeTitle/LargeTitle";
+import { SmallTitle } from "../SmallTitle/SmallTitle";
+import { Icon } from "../Icon/Icon";
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./Bag.module.css";
 
 export const Bag = ({ toggleDisplayBagPopup }) => {
-
-
   const bagService = useService(bagServiceFactory);
+
+  const { bagQuantity } = useBagContext();
 
   return (
     <Popup
@@ -15,7 +22,21 @@ export const Bag = ({ toggleDisplayBagPopup }) => {
       variant={"order"}
       popupCloseHandler={toggleDisplayBagPopup}
     >
-      <h1>Bag</h1>
+      <section className={styles["bag"]}>
+        <LargeTitle title={"Your Bag"} variant={"large-title"} />
+        <div className={styles["left-container"]}>
+          <div className={styles["delivery"]}>
+            <Icon icon={faTruck} variant={"icon"} />
+            <SmallTitle title={"Delivery"} />
+            <span className={styles["delivery-span"]}>
+              {bagQuantity === 1
+                ? `(${bagQuantity} item)`
+                : `(${bagQuantity} items)`}
+            </span>
+          </div>
+        </div>
+        <div className={styles["right-container"]}></div>
+      </section>
     </Popup>
   );
 };
