874f33ee795f0a2aa00d4e4d026400e1166ea2b5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-31 16:37:02 +0300

    Updated populate db

--- src/managers/orderManager.js
+++ src/managers/orderManager.js
@@ -1,19 +1,10 @@
 const Order = require("../models/Order");
-const bagManager = require("./bagManager");
 
 exports.create = async (userId) => {
-  const orderData = await bagManager.getAll(userId);
-
   const order = await Order.create({
     user: userId,
     status: "Pending",
   });
-
-  order.jewelries = orderData[0].documents;
-
-  order.subTotal = orderData[0].totalTotalPrice;
-
-  await order.save();
 
   return order;
 };
