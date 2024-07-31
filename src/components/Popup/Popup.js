c4d36dd57d3f221713003157935a7ea54ac26ab3
Beatris Ilieve <beatrisilieve@icloud.com>
2024-07-31 16:28:41 +0300

    Updated checkout functionality

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,32 +1,3 @@
-// import { useState } from "react";
-
-// import React from "react";
-// import styles from "./Popup.module.css";
-
-// import { faXmark } from "@fortawesome/free-solid-svg-icons";
-
-// import { Icon } from "../Icon/Icon";
-
-// export const Popup = ({ isVisible, children, variant, popupCloseHandler }) => {
-//   return (
-//     <section
-//       className={`${styles.overlay} ${isVisible ? styles.visible : ""}`.trim()}
-//     >
-//       <div className={styles[variant]}>
-//         <div className={styles["icon"]}>
-//           {variant !== "authentication" && (
-//             <Icon icon={faXmark} variant={"icon"} callBackFunction={popupCloseHandler} />
-//           )}
-//         </div>
-//         <div className={styles["content"]}>{children}</div>
-//       </div>
-//     </section>
-//   );
-// };
-
-import { useState } from "react";
-
-import React from "react";
 import styles from "./Popup.module.css";
 
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -34,17 +5,13 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Icon } from "../Icon/Icon";
 
 export const Popup = ({
-  isVisible,
   children,
   overlayVariant,
   modalVariant,
   popupCloseHandler,
 }) => {
   return (
-    <section
-      // className={`${styles[overlayVariant]} ${isVisible ? styles[modalVariant] : ""}`.trim()}
-      className={styles[overlayVariant]}
-    >
+    <section className={styles[overlayVariant]}>
       <div className={styles[modalVariant]}>
         <div className={styles["icon"]}>
           {modalVariant !== "authentication" && (
