5a2b8c42f306642188044114db8ed8abf69f2740
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-01 19:36:58 +0300

    Updated checkout functionality

--- src/components/MiniBag/MiniBag.js
+++ src/components/MiniBag/MiniBag.js
@@ -7,13 +7,14 @@ import { BagList } from "../BagList/BagList";
 import { NormalTitle } from "../NormalTitle/NormalTitle";
 
 import { LargeTitle } from "../LargeTitle/LargeTitle";
+import { SmallTitle } from "../SmallTitle/SmallTitle";
 
 import { PinkButton } from "../PinkButton/PinkButton";
 
 import styles from "./MiniBag.module.css";
 
 export const MiniBag = ({ toggleDisplayMiniBagPopup }) => {
-  const { totalPrice } = useBagContext();
+  const { totalPrice, bagTotalQuantityIntoState } = useBagContext();
 
   return (
     <Popup
@@ -23,7 +24,14 @@ export const MiniBag = ({ toggleDisplayMiniBagPopup }) => {
     >
       <section className={styles["mini-bag"]}>
         <div className={styles["title"]}>
-        <LargeTitle title={"My Bag"} />
+          <LargeTitle title={"My Bag"} />
+          <NormalTitle
+            title={
+              bagTotalQuantityIntoState > 1
+                ? `(${bagTotalQuantityIntoState}) items`
+                : `(${bagTotalQuantityIntoState}) item`
+            }
+          />
         </div>
         <BagList variant={"mini-bag"} />
         <div className={styles["total-price-wrapper"]}>
