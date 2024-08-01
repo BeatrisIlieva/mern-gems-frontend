7a43aa48b88b5a13c8bc9f16e3f608efebc171ee
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-01 18:54:29 +0300

    Updated checkout functionality

--- src/components/ShoppingProcessContainer/Checkout/Checkout.js
+++ src/components/ShoppingProcessContainer/Checkout/Checkout.js
@@ -2,25 +2,16 @@ import { ShoppingProcessContainer } from "../ShoppingProcessContainer";
 
 import { ShippingDetailsForm } from "./ShippingDetailsForm/ShippingDetailsForm";
 
-import { LargeTitle } from "../../LargeTitle/LargeTitle";
-
 import { LeftSide } from "../LeftSide/LeftSide";
 import { RightSide } from "../RightSide/RightSide";
 import { OrderSummary } from "../OrderSummary/OrderSummary";
 import { BagList } from "../../BagList/BagList";
 
-import styles from "./Checkout.module.css";
-
 export const Checkout = () => {
   return (
     <ShoppingProcessContainer title={"Checkout"}>
       <LeftSide>
-        {/* <section className={styles["shipping-details"]}>
-          <div className={styles["top-container"]}>
-            <LargeTitle title={"Shipping Information"} />
-          </div> */}
-          <ShippingDetailsForm />
-        {/* </section> */}
+        <ShippingDetailsForm />
       </LeftSide>
       <RightSide>
         <OrderSummary />
