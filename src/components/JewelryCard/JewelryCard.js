275f755fe9179dc72f249f2a0a6578ecb0d5c753
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-03 19:17:10 +0300

    Updated jewelry functionality

--- src/components/JewelryCard/JewelryCard.js
+++ src/components/JewelryCard/JewelryCard.js
@@ -1,4 +1,43 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+// import { slugify } from "../../utils/slugify";
+
+// import styles from "./JewelryCard.module.css";
+
+// export const JewelryCard = ({
+//   firstImageUrl,
+//   jewelryId,
+//   categoryTitle,
+//   collectionTitle,
+//   jewelryTitle,
+//   isSoldOut,
+//   variant,
+// }) => {
+//   const slugifiedCollectionTitle = slugify(collectionTitle);
+//   const slugifiedCategoryTitle = slugify(categoryTitle);
+//   const slugifiedJewelryTitle = slugify(jewelryTitle);
+
+//   return (
+//     <article className={styles["jewelry-card"]}>
+//       <Link
+//         to={`/${slugifiedCollectionTitle}/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}
+//       >
+//         <div className={styles[variant]}>
+//           <img
+//             src={firstImageUrl}
+//             alt={slugifiedJewelryTitle}
+//             className={
+//               isSoldOut ? `${styles["sold-out"]}` : `${styles["image"]}`
+//             }
+//           />
+//         </div>
+//         {isSoldOut && <span className={styles["sold-out-span"]}>SOLD OUT</span>}
+//       </Link>
+//     </article>
+//   );
+// };
+
+import { NavLink } from "react-router-dom";
 
 import { slugify } from "../../utils/slugify";
 
@@ -7,21 +46,15 @@ import styles from "./JewelryCard.module.css";
 export const JewelryCard = ({
   firstImageUrl,
   jewelryId,
-  categoryTitle,
-  collectionTitle,
   jewelryTitle,
   isSoldOut,
   variant,
 }) => {
-  const slugifiedCollectionTitle = slugify(collectionTitle);
-  const slugifiedCategoryTitle = slugify(categoryTitle);
   const slugifiedJewelryTitle = slugify(jewelryTitle);
 
   return (
     <article className={styles["jewelry-card"]}>
-      <Link
-        to={`/${slugifiedCollectionTitle}/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}
-      >
+      <NavLink to={`${slugifiedJewelryTitle}/${jewelryId}`}>
         <div className={styles[variant]}>
           <img
             src={firstImageUrl}
@@ -32,7 +65,7 @@ export const JewelryCard = ({
           />
         </div>
         {isSoldOut && <span className={styles["sold-out-span"]}>SOLD OUT</span>}
-      </Link>
+      </NavLink>
     </article>
   );
 };
--- src/components/JewelryList/JewelryList.js
+++ src/components/JewelryList/JewelryList.js
@@ -1,3 +1,118 @@
+// import { Routes, Route } from "react-router-dom";
+
+// import { useLocation } from "react-router-dom";
+// import { useJewelryList } from "../../hooks/useJewelryList";
+
+// import { JewelryListItem } from "../JewelryListItem/JewelryListItem";
+// import { LoadingSpinner } from "../LoadingSpinner/LoadingSpinner";
+// import { AnimatedButton } from "../AnimatedButton/AnimatedButton";
+// import { CardSkeleton } from "../CardSkeleton/CardSkeleton";
+
+// import { COLLECTIONS_BY_ID } from "../../mappers/collectionsById";
+// import { CATEGORIES_BY_ID } from "../../mappers/categoriesById";
+
+// import { NavLinkItem } from "../NavLinkItem/NavLinkItem";
+
+// import { transformUrlSegment } from "../../utils/transformUrlSegment";
+
+// import { faFilter } from "@fortawesome/free-solid-svg-icons";
+// import { faFilterCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+// import styles from "./JewelryList.module.css";
+
+// export const JewelryList = () => {
+//   const location = useLocation();
+
+//   const pathname = location.pathname.substring(1);
+//   const [collectionName, categoryName] = pathname.split("/");
+
+//   const collectionId = COLLECTIONS_BY_ID[collectionName];
+//   const categoryId = CATEGORIES_BY_ID[categoryName];
+
+//   const transformedCollectionName = transformUrlSegment(collectionName);
+
+//   const navItems = [
+//     {
+//       to: "bracelets",
+//       label: "Bracelets",
+//       icon: faFilter,
+//       iconVariant: "filter",
+//     },
+//     {
+//       to: "earrings",
+//       label: "Earrings",
+//       icon: faFilter,
+//       iconVariant: "filter",
+//     },
+//     {
+//       to: "necklaces-pendants",
+//       label: "Necklaces & Pendants",
+//       icon: faFilter,
+//       iconVariant: "filter",
+//     },
+//     { to: "rings", label: "Rings", icon: faFilter, iconVariant: "filter" },
+//   ];
+
+//   const navCollection = [
+//     {
+//       to: `/${collectionName}`,
+//       label: `${transformedCollectionName} Collection`,
+//       icon: faFilterCircleXmark,
+//       iconVariant: "filter",
+//     },
+//   ];
+
+//   const {
+//     loading,
+//     jewelries,
+//     totalCount,
+//     loadMore,
+//     handleLoadMore,
+//     showLoadMore,
+//   } = useJewelryList(collectionId, categoryId);
+
+//   return (
+//     <section className={styles["jewelries"]}>
+//       <div className={styles["nav-wrapper"]}>
+//         <div className={styles["nav-filter"]}>
+//           <NavLinkItem items={navItems} />
+//         </div>
+//         <NavLinkItem items={navCollection} />
+//       </div>
+//       <Routes>
+//         <Route path="/bracelets" />
+//         <Route path="/earrings" />
+//         <Route path="/necklaces-pendants" />
+//         <Route path="/rings" />
+//         <Route path={`/${collectionName}`} />
+//       </Routes>
+//       <div className={styles["jewelries-count"]}>
+//         Showing 1 -{" "}
+//         {totalCount >= jewelries.length ? jewelries.length : totalCount} of{" "}
+//         {totalCount}
+//       </div>
+//       {loading ? (
+//         <>
+//           <LoadingSpinner />
+//           <CardSkeleton />
+//         </>
+//       ) : (
+//         <div className={styles["jewelry-grid"]}>
+//           {jewelries.map((j) => (
+//             <JewelryListItem key={j._id} {...j} />
+//           ))}
+//         </div>
+//       )}
+//       {loadMore && showLoadMore && (
+//         <div className={styles["button"]} onClick={handleLoadMore}>
+//           <AnimatedButton title={"Load More"} />
+//         </div>
+//       )}
+//     </section>
+//   );
+// };
+
+
 import { Routes, Route } from "react-router-dom";
 
 import { useLocation } from "react-router-dom";
@@ -17,6 +132,8 @@ import { transformUrlSegment } from "../../utils/transformUrlSegment";
 
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import { faFilterCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+import { JewelryItem } from "../JewelryItem/JewelryItem";
 
 import styles from "./JewelryList.module.css";
 
@@ -80,11 +197,12 @@ export const JewelryList = () => {
         <NavLinkItem items={navCollection} />
       </div>
       <Routes>
-        <Route path="/bracelets" />
-        <Route path="/earrings" />
-        <Route path="/necklaces-pendants" />
-        <Route path="/rings" />
+        <Route path="/bracelets*" />
+        <Route path="/earrings*" />
+        <Route path="/necklaces-pendants*" />
+        <Route path="/rings*" />
         <Route path={`/${collectionName}`} />
+        <Route path="/:slugifiedJewelryTitle/:jewelryId" element={<JewelryItem />} />
       </Routes>
       <div className={styles["jewelries-count"]}>
         Showing 1 -{" "}
