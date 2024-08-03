6abb9c954f3d1dafac9f5cece0c0fd3257cf6f15
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-03 12:46:15 +0300

    Updated bag functionality

--- src/components/ShoppingProcessContainer/Bag/Bag.js
+++ src/components/ShoppingProcessContainer/Bag/Bag.js
@@ -1,19 +1,4 @@
-import { Link } from "react-router-dom";
-
 import { useBagContext } from "../../../contexts/BagContext";
-
-import { ShoppingProcessContainer } from "../ShoppingProcessContainer";
-
-import { BagList } from "../../BagList/BagList";
-
-import { OrderSummary } from "../OrderSummary/OrderSummary";
-
-import { Button } from "../../Button/Button";
-
-import styles from "./Bag.module.css";
-
-import { LeftSide } from "../LeftSide/LeftSide";
-import { RightSide } from "../RightSide/RightSide";
 
 import { NonEmptyBag } from "./NonEmptyBag/NonEmptyBag";
 
