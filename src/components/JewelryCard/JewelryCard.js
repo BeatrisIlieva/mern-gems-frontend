69d6b3c6f33a2d34c82d5480ee87d8070dc080d7
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-03 20:41:38 +0300

    Updated jewelry functionality

--- src/components/JewelryCard/JewelryCard.js
+++ src/components/JewelryCard/JewelryCard.js
@@ -1,42 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// import { slugify } from "../../utils/slugify";
-
-// import styles from "./JewelryCard.module.css";
-
-// export const JewelryCard = ({
-//   firstImageUrl,
-//   jewelryId,
-//   categoryTitle,
-//   collectionTitle,
-//   jewelryTitle,
-//   isSoldOut,
-//   variant,
-// }) => {
-//   const slugifiedCollectionTitle = slugify(collectionTitle);
-//   const slugifiedCategoryTitle = slugify(categoryTitle);
-//   const slugifiedJewelryTitle = slugify(jewelryTitle);
-
-//   return (
-//     <article className={styles["jewelry-card"]}>
-//       <Link
-//         to={`/${slugifiedCollectionTitle}/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}
-//       >
-//         <div className={styles[variant]}>
-//           <img
-//             src={firstImageUrl}
-//             alt={slugifiedJewelryTitle}
-//             className={
-//               isSoldOut ? `${styles["sold-out"]}` : `${styles["image"]}`
-//             }
-//           />
-//         </div>
-//         {isSoldOut && <span className={styles["sold-out-span"]}>SOLD OUT</span>}
-//       </Link>
-//     </article>
-//   );
-// };
-
 import { NavLink } from "react-router-dom";
 
 import { slugify } from "../../utils/slugify";
@@ -52,13 +13,15 @@ export const JewelryCard = ({
   collectionTitle,
   categoryTitle,
 }) => {
-    const slugifiedCollectionTitle = slugify(collectionTitle);
+  const slugifiedCollectionTitle = slugify(collectionTitle);
   const slugifiedCategoryTitle = slugify(categoryTitle);
   const slugifiedJewelryTitle = slugify(jewelryTitle);
 
   return (
     <article className={styles["jewelry-card"]}>
-      <NavLink to={`/${slugifiedCollectionTitle}/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}>
+      <NavLink
+        to={`/${slugifiedCollectionTitle}/${slugifiedCategoryTitle}/${slugifiedJewelryTitle}/${jewelryId}`}
+      >
         <div className={styles[variant]}>
           <img
             src={firstImageUrl}
