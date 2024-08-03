06ee23ab8913a052852f5580d14d8a928f7798ed
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-03 17:11:37 +0300

    Updated user functionality

--- src/components/OrderConfirmation/ShippingInformation/ShippingInformation.js
+++ src/components/OrderConfirmation/ShippingInformation/ShippingInformation.js
@@ -1,35 +1,13 @@
-import { useState, useEffect } from "react";
-
-import { useAuthenticationContext } from "../../../contexts/AuthenticationContext";
-
-import { useService } from "../../../hooks/useService";
-
-import { userServiceFactory } from "../../../services/userService";
-
 import { useUserShippingDetails } from "../../../hooks/useUserShippingDetails";
 
 import { XLargeTitle } from "../../XLargeTitle/XLargeTitle";
 
 export const ShippingInformation = () => {
-  const {userShippingDetails} = useUserShippingDetails();
-  // const { userId } = useAuthenticationContext();
-  // const userService = useService(userServiceFactory);
-  // const [userInformation, setUserInformation] = useState([]);
-
-  // useEffect(() => {
-  //   userService
-  //     .getUserShippingDetails(userId)
-  //     .then((data) => {
-  //       setUserInformation(data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  // }, [userService, userId]);
+  const { userShippingDetails } = useUserShippingDetails();
 
   return (
     <XLargeTitle
-      title={`Thank you for your purchase, ${userShippingDetails  .firstName}!`}
+      title={`Thank you for your purchase, ${userShippingDetails.firstName}!`}
     />
   );
 };
