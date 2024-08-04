aee2d4d978eeeb6233061007c88e341b3572210c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-04 15:21:42 +0300

    Updated user functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,22 +24,7 @@ router.get("/:userId", async (req, res) => {
   }
 });
 
-router.put("/password/:userId", async (req, res) => {
-  const userId = req.params.userId;
 
-  const data = { ...req.body };
-
-  try {
-    const result = await userManager.updatePassword(userId, data);
-
-    res.status(200).json(result);
-  } catch (err) {
-    console.log(err.message);
-    res.status(401).json({
-      message: err.message,
-    });
-  }
-});
 
 router.get("/shipping-details/:userId", async (req, res) => {
   const userId = req.params.userId;
--- src/controllers/userLoginDetailsController.js
+++ src/controllers/userLoginDetailsController.js
@@ -56,6 +56,23 @@ router.put("/email/:userId", async (req, res) => {
   }
 });
 
+router.put("/password/:userId", async (req, res) => {
+  const userId = req.params.userId;
+
+  const data = { ...req.body };
+
+  try {
+    const result = await userManager.updatePassword(userId, data);
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err.message);
+    res.status(401).json({
+      message: err.message,
+    });
+  }
+});
+
 router.get("/logout", (req, res) => {
   res.status(204).json();
 });
