69e74c8693af7c24e86aba8702d903c79882e81e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-04 15:13:46 +0300

    Updated user functionality

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
 const router = require("express").Router();
 
-const userController = require("./controllers/userController");
+const userLoginDetailsController = require("./controllers/userLoginDetailsController");
+const userShippingDetailsController = require("./controllers/userShippingDetailsController");
+const userCardDetailsController = require("./controllers/userCardDetailsController");
 const jewelryController = require("./controllers/jewelryController");
 const bagController = require("./controllers/bagController");
-const userCardDetailsController = require("./controllers/userCardDetailsController");
 const paymentController = require("./controllers/paymentController");
 const orderController = require("./controllers/orderController");
 
-router.use("/users", userController);
+router.use("/users-login-details", userLoginDetailsController);
+router.use("/users-shipping-details", userShippingDetailsController);
+router.use("/users-card-details", userCardDetailsController);
 router.use("/jewelries", jewelryController);
 router.use("/bags", bagController);
-router.use("/users-card-details", userCardDetailsController);
 router.use("/payments", paymentController);
 router.use("/orders", orderController);
 
