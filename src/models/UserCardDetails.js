64a4d27374d0a3949e3b516194382e2a480cd452
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-04 11:55:08 +0300

    Updated user functionality

--- src/models/UserCardDetails.js
+++ src/models/UserCardDetails.js
@@ -25,6 +25,7 @@ const userCardDetailsSchema = new mongoose.Schema({
   _id: {
     type: String,
   },
+
   cardHolder: {
     type: String,
     match: [CARD_HOLDER_NAME_PATTERN, CARD_HOLDER_NAME_PATTERN_ERROR_MESSAGE],
@@ -39,15 +40,34 @@ const userCardDetailsSchema = new mongoose.Schema({
     type: String,
     match: [CVV_CODE_PATTERN, CVV_CODE_PATTERN_ERROR_MESSAGE],
   },
+
   expiryDate: {
     type: String,
-    match: [
-      EXPIRY_DATE_PATTERN,
-      EXPIRY_DATE_PATTERN_ERROR_MESSAGE,
-      CARD_HAS_EXPIRED_ERROR_MESSAGE,
-    ],
+    match: [EXPIRY_DATE_PATTERN, EXPIRY_DATE_PATTERN_ERROR_MESSAGE],
+    validate: {
+      validator: function (value) {
+        return checkIfCardHasExpired(value);
+      },
+      message: CARD_HAS_EXPIRED_ERROR_MESSAGE,
+    },
   },
 });
+
+function checkIfCardHasExpired(expirationDate) {
+  const [month, year] = expirationDate
+    .split("/")
+    .map((val) => parseInt(val, 10));
+    
+  const expiration = new Date("20" + year + "-" + month + "-01");
+
+  const currentDate = new Date();
+
+  if (expiration < currentDate) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 userCardDetailsSchema.pre("save", async function () {
   this._id = this._id;
