1d135ec6733742e8c905aba0ab49bb61d3f07a3f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-05 10:23:39 +0300

    Updated user functionality

--- src/components/OrderConfirmation/OrderInformation/OrderInformation.js
+++ src/components/OrderConfirmation/OrderInformation/OrderInformation.js
@@ -1,34 +1,8 @@
-import { useState, useEffect } from "react";
-
-import { useAuthenticationContext } from "../../../contexts/AuthenticationContext";
-
-import { useService } from "../../../hooks/useService";
-
-import { orderServiceFactory } from "../../../services/orderService";
-
 import { NormalTitle } from "../../NormalTitle/NormalTitle";
 
 import { convertToReadableDate } from "../../../utils/convertToReadableDate";
 
 export const OrderInformation = ({ orderInformation }) => {
-  // const { userId } = useAuthenticationContext();
-  // const orderService = useService(orderServiceFactory);
-  // const [orderInformation, setOrderInformation] = useState([]);
-
-  // useEffect(() => {
-  //   // toggleIsLoading();
-  //   orderService
-  //     .confirm(userId)
-  //     .then((data) => {
-  //       setOrderInformation(data);
-
-  //       // toggleIsLoading();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  // }, [orderService, userId]);
-
   const readableDate = convertToReadableDate(orderInformation.createdAt);
 
   return (
