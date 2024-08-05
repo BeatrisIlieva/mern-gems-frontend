e6e3f24123fece0ea4b43a82e6ebb2ca6343b32b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-05 10:24:10 +0300

    Updated user functionality

--- src/components/OrderConfirmation/ShippingInformation/ShippingInformation.js
+++ src/components/OrderConfirmation/ShippingInformation/ShippingInformation.js
@@ -1,35 +1,6 @@
-import { useState, useEffect } from "react";
-
-import { useAuthenticationContext } from "../../../contexts/AuthenticationContext";
-
-import { useService } from "../../../hooks/useService";
-import { userShippingDetailsServiceFactory } from "../../../services/userShippingDetailsService";
 import { XLargeTitle } from "../../XLargeTitle/XLargeTitle";
 
 export const ShippingInformation = ({ userShippingDetails }) => {
-  // const [userShippingDetails, setUserShippingDetails] = useState([]);
-
-  // const { userId } = useAuthenticationContext();
-
-  // const userShippingDetailsService = useService(
-  //   userShippingDetailsServiceFactory
-  // );
-
-  // useEffect(() => {
-  //   // toggleIsLoading();
-
-  //   userShippingDetailsService
-  //     .getOne(userId)
-  //     .then((data) => {
-  //       setUserShippingDetails(data);
-
-  //       // toggleIsLoading();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  // }, [userShippingDetailsService, userId]);
-
   return (
     <XLargeTitle
       title={`Thank you for your purchase, ${userShippingDetails.firstName}!`}
