033e718b470c40cb1b423c28c244bca786a1b28e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-05 10:23:08 +0300

    Updated forms

--- src/components/OrderConfirmation/LoginInformation/LoginInformation.js
+++ src/components/OrderConfirmation/LoginInformation/LoginInformation.js
@@ -1,33 +1,6 @@
-import { useState, useEffect } from "react";
-
-import { useService } from "../../../hooks/useService";
-import { userLoginDetailsServiceFactory } from "../../../services/userLoginDetailsService";
-
-import { useAuthenticationContext } from "../../../contexts/AuthenticationContext";
 import { NormalTitle } from "../../NormalTitle/NormalTitle";
 
-export const LoginInformation = ({ userLoginDetails}) => {
-  // const [userLoginDetails, setUserLoginDetails] = useState([]);
-
-  // const { userId } = useAuthenticationContext();
-
-  // const userLoginDetailsService = useService(userLoginDetailsServiceFactory);
-
-  // useEffect(() => {
-  //   toggleIsLoading();
-  //   userLoginDetailsService
-  //     .getOne(userId)
-  //     .then((data) => {
-  //       setUserLoginDetails(data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     })
-  //     .finally(() => {
-  //       toggleIsLoading();
-  //     });
-  // }, [userLoginDetailsService, userId, toggleIsLoading]);
-
+export const LoginInformation = ({ userLoginDetails }) => {
   return (
     <NormalTitle
       title={`A confirmation email has been sent to: ${userLoginDetails.email}`}
