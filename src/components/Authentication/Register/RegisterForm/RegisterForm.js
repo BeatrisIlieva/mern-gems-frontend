5d731594a8e4fd55dd40946c7eaf69f751b15bdc
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-06 14:05:11 +0300

    Updated forms

--- src/components/Authentication/Register/RegisterForm/RegisterForm.js
+++ src/components/Authentication/Register/RegisterForm/RegisterForm.js
@@ -1,5 +1,3 @@
-
-
 import { useService } from "../../../../hooks/useService";
 import { useAuthenticationContext } from "../../../../contexts/AuthenticationContext";
 
@@ -8,13 +6,14 @@ import { DynamicForm } from "../../../DynamicForm/DynamicForm";
 import { checkIfFormErrorHasOccurred } from "../../../../utils/checkIfFormErrorHasOccurred";
 
 import { INITIAL_FORM_VALUES, FORM_KEYS } from "./initialFormValues";
-import { EMAIL_ALREADY_EXISTS_ERROR_MESSAGE } from "../../../../constants/email";
 
 import { userLoginDetailsServiceFactory } from "../../../../services/userLoginDetailsService";
 
 import { clearInitialFormValuesMessages } from "../../../../utils/clearInitialFormValuesMessages";
 
 import { getData } from "./helpers/getData";
+
+import { setEmailAlreadyExistsErrorMessage } from "./helpers/setEmailAlreadyExistsErrorMessage";
 
 export const RegisterForm = () => {
   const { updateAuthentication } = useAuthenticationContext();
@@ -33,14 +32,11 @@ export const RegisterForm = () => {
   const onSubmit = async (e) => {
     submitHandler(e);
 
-    const updatedValues = { ...values };
-
-    setValues(updatedValues);
-
-    const errorOccurred = checkIfFormErrorHasOccurred(updatedValues);
+    const errorOccurred = checkIfFormErrorHasOccurred(values);
 
     if (!errorOccurred) {
       const data = getData(values);
+
       try {
         const result = await userLoginDetailsService.register(data);
 
@@ -48,15 +44,14 @@ export const RegisterForm = () => {
 
         clearInitialFormValuesMessages(FORM_KEYS, INITIAL_FORM_VALUES);
       } catch (err) {
-        if (err.message === EMAIL_ALREADY_EXISTS_ERROR_MESSAGE) {
-          setValues((prevValues) => ({
-            ...prevValues,
-            [FORM_KEYS.Email]: {
-              ...prevValues[FORM_KEYS.Email],
-              errorMessage: err.message,
-            },
-          }));
-        }
+        let spreadValues = { ...values };
+
+        spreadValues = setEmailAlreadyExistsErrorMessage(
+          spreadValues,
+          FORM_KEYS
+        );
+
+        setValues(spreadValues);
       }
     }
   };
