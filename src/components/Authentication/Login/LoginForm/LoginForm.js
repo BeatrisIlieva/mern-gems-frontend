bbc411c6fbd4496650c4b32e767890c5a661fd9a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-06 13:56:29 +0300

    Updated forms

--- src/components/Authentication/Login/LoginForm/LoginForm.js
+++ src/components/Authentication/Login/LoginForm/LoginForm.js
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import { useService } from "../../../../hooks/useService";
 import { useAuthenticationContext } from "../../../../contexts/AuthenticationContext";
 
@@ -9,14 +7,15 @@ import { useForm } from "../../../../hooks/useForm";
 
 import { checkIfFormErrorHasOccurred } from "../../../../utils/checkIfFormErrorHasOccurred";
 
-import { INVALID_CREDENTIALS_ERROR_MESSAGE } from "../../../../constants/email";
 import { INITIAL_FORM_VALUES, FORM_KEYS } from "./initialFormValues";
 
 import { userLoginDetailsServiceFactory } from "../../../../services/userLoginDetailsService";
 
 import { clearInitialFormValuesMessages } from "../../../../utils/clearInitialFormValuesMessages";
 
-import { getData } from "./getData";
+import { getData } from "./helpers/getData";
+
+import { setInvalidCredentialsErrorMessage } from "./helpers/setInvalidCredentialsErrorMessage";
 
 export const LoginForm = () => {
   const { updateAuthentication } = useAuthenticationContext();
@@ -26,24 +25,14 @@ export const LoginForm = () => {
   let {
     values,
     setValues,
-    updateForm,
     clickHandler,
     blurHandler,
     changeHandler,
     submitHandler,
   } = useForm(INITIAL_FORM_VALUES);
 
-  useEffect(() => {
-    updateForm();
-  }, [updateForm]);
-
   const onSubmit = async (e) => {
     submitHandler(e);
-
-    const updatedValues = { ...values };
-    setValues(updatedValues);
-
-    updateForm();
 
     const errorOccurred = checkIfFormErrorHasOccurred(values);
 
@@ -57,17 +46,14 @@ export const LoginForm = () => {
 
         clearInitialFormValuesMessages(FORM_KEYS, INITIAL_FORM_VALUES);
       } catch (err) {
-        if (err.message === INVALID_CREDENTIALS_ERROR_MESSAGE) {
-          setValues((prevValues) => ({
-            ...prevValues,
-            [FORM_KEYS.Email]: {
-              ...prevValues[FORM_KEYS.Email],
-              errorMessage: err.message,
-            },
-          }));
+        let spreadValues = { ...values };
 
-          updateForm();
-        }
+        spreadValues = setInvalidCredentialsErrorMessage(
+          spreadValues,
+          FORM_KEYS
+        );
+
+        setValues(spreadValues);
       }
     }
   };
