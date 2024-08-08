35fffaa19e0c3f4af7e53343b0c39bac424a76af
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-08 11:05:02 +0300

    Updated user functionality

--- src/components/Authentication/Register/Register.js
+++ src/components/Authentication/Register/Register.js
@@ -4,17 +4,15 @@ import { RegisterForm } from "./RegisterForm/RegisterForm";
 
 export const Register = ({ switchPopupHandler, switchOptions }) => {
   return (
-    // <Popup title={"Become A Member"}>
-<>
-<Header title={"Become A Member"}/>
-<RegisterForm />
+    <>
+      <Header title={"Become A Member"} />
+      <RegisterForm />
       <SwitchButton
         text={"Already a member?"}
         title={"Sign In"}
         switchPopupHandler={switchPopupHandler}
         option={switchOptions.Login}
       />
-</>
-    // </Popup>
+    </>
   );
 };
