00c6b4ddcdf9dfebc84605f9d7c54da6aa8227aa
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-08 11:05:16 +0300

    Updated user functionality

--- src/components/Authentication/Login/Login.js
+++ src/components/Authentication/Login/Login.js
@@ -4,17 +4,15 @@ import { LoginForm } from "./LoginForm/LoginForm";
 
 export const Login = ({ switchPopupHandler, switchOptions }) => {
   return (
-    // <Popup title={"Sign In to Shop"}>
-<>
-<Header title={"Sign In to Shop"}/>
-<LoginForm />
+    <>
+      <Header title={"Sign In to Shop"} />
+      <LoginForm />
       <SwitchButton
         text={"Not a member?"}
         title={"Sign Up"}
         switchPopupHandler={switchPopupHandler}
         option={switchOptions.Register}
       />
-</>
-    // </Popup>
+    </>
   );
 };
