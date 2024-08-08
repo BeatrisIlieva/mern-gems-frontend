7c59c6bcaa53f095ffa3e4f172688e018c9aea0a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-08 13:10:10 +0300

    Updated user functionality

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,30 +1,9 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faXmark } from "@fortawesome/free-solid-svg-icons";
-
-// import styles from "./Popup.module.css";
-
-// export const Popup = ({ popupCloseHandler, title, children, variant}) => {
-//   return (
-//     <section className={styles["overlay"]}>
-//       <div className={styles["modal"]}>
-//         <FontAwesomeIcon
-//           icon={faXmark}
-//           className={styles["x-mark"]}
-//           onClick={popupCloseHandler}
-//         />
-//         <h2 className={styles["title"]}>{title}</h2>
-//         {children}
-//       </div>
-//     </section>
-//   );
-// };
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Popup.module.css";
 
-export const Popup = ({ popupCloseHandler, title, children, variant}) => {
+export const Popup = ({ popupCloseHandler, title, children, variant }) => {
   return (
     <section className={styles["overlay"]}>
       <div className={`${styles["modal"]} ${styles[variant]}`}>
