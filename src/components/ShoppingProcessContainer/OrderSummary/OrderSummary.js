2c5e5c00357c2e711b64604571cda46273cf0f59
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-08 13:59:55 +0300

    Updated checkout functionality

--- src/components/ShoppingProcessContainer/OrderSummary/OrderSummary.js
+++ src/components/ShoppingProcessContainer/OrderSummary/OrderSummary.js
@@ -1,7 +1,6 @@
 import { useBagContext } from "../../../contexts/BagContext";
 
 import { HorizontalLine } from "../../HorizontalLine/HorizontalLine";
-import { NormalTitle } from "../../NormalTitle/NormalTitle";
 import { ContainerTitle } from "../../ContainerTitle/ContainerTitle";
 import { SectionContainer } from "./SectionContainer/SectionContainer";
 import styles from "./OrderSummary.module.css";
@@ -12,22 +11,22 @@ export const OrderSummary = () => {
   return (
     <section className={styles["order-summary"]}>
       <ContainerTitle title={"Order Summary"} />
-      <SectionContainer firstTitle={"Subtotal"} secondTitle={`$ ${totalPrice}`} variant={"bolded"}/>
-      <SectionContainer firstTitle={"Shipping"} secondTitle={"Complimentary"} variant={"regular"}/>
-      <SectionContainer firstTitle={"Total"} secondTitle={`$ ${totalPrice}`} variant={"bolded"}/>
-      {/* <div className={styles["sub-container"]}>
-        <NormalTitle title={"Subtotal"} variant={"bolded"} />
-        <NormalTitle title={`$ ${totalPrice}`} variant={"bolded"} />
-      </div>
-      <div className={styles["sub-container"]}>
-        <NormalTitle title={"Shipping"} variant={"regular"} />
-        <NormalTitle title={"Complimentary"} variant={"regular"} />
-      </div>
+      <SectionContainer
+        firstTitle={"Subtotal"}
+        secondTitle={`$ ${totalPrice}`}
+        variant={"bolded"}
+      />
+      <SectionContainer
+        firstTitle={"Shipping"}
+        secondTitle={"Complimentary"}
+        variant={"regular"}
+      />
       <HorizontalLine variant={"large"} />
-      <div className={styles["bottom-sub-container"]}>
-        <NormalTitle title={"Total"} variant={"bolded"} />
-        <NormalTitle title={`$ ${totalPrice}`} variant={"bolded"} />
-      </div> */}
+      <SectionContainer
+        firstTitle={"Total"}
+        secondTitle={`$ ${totalPrice}`}
+        variant={"bolded"}
+      />
     </section>
   );
 };
