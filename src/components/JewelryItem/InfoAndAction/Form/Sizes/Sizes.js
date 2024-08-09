b402c051a87e6a2ede2df409ea64755541771263
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-09 14:30:29 +0300

    Updated jewelry functionality

--- src/components/JewelryItem/InfoAndAction/Form/Sizes/Sizes.js
+++ src/components/JewelryItem/InfoAndAction/Form/Sizes/Sizes.js
@@ -2,35 +2,30 @@ import { useJewelryItemContext } from "../../../../../contexts/JewelryItemContex
 import { SIZE_FORM_KEY } from "../../../../../constants/sizeFormKey";
 import styles from "./Sizes.module.css";
 
-import { NormalTitle } from "../../../../NormalTitle/NormalTitle";
-
 export const Sizes = ({ errorMessage, changeHandler }) => {
   const { sizes, selectedSize } = useJewelryItemContext();
 
   return (
-    <>
-      <NormalTitle title={"Size:"} variant={"bolded"} />
-      <div className={styles["size-wrapper"]}>
-        <div className={styles["radio-container"]}>
-          {sizes.map((item) => (
-            <div key={item._id}>
-              <input
-                type="radio"
-                name={SIZE_FORM_KEY.Size}
-                id={item._id}
-                value={item._id}
-                onChange={changeHandler}
-                checked={Number(selectedSize[SIZE_FORM_KEY.Size]) === item._id}
-                disabled={!Number(item.quantity) > 0}
-              />
-              <label className={styles["label"]} htmlFor={item._id}>
-                {item.measurement}
-              </label>
-            </div>
-          ))}
-        </div>
-        <div className={styles["error-message"]}>{errorMessage}</div>
+    <div className={styles["size-wrapper"]}>
+      <div className={styles["radio-container"]}>
+        {sizes.map((item) => (
+          <div key={item._id}>
+            <input
+              type="radio"
+              name={SIZE_FORM_KEY.Size}
+              id={item._id}
+              value={item._id}
+              onChange={changeHandler}
+              checked={Number(selectedSize[SIZE_FORM_KEY.Size]) === item._id}
+              disabled={!Number(item.quantity) > 0}
+            />
+            <label className={styles["label"]} htmlFor={item._id}>
+              {item.measurement}
+            </label>
+          </div>
+        ))}
       </div>
-    </>
+      <div className={styles["error-message"]}>{errorMessage}</div>
+    </div>
   );
 };
