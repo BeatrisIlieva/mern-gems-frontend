ef837f47fa55fd20c34f58068d3163a6bebedc51
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-09 15:35:21 +0300

    Updated OrderHistory

--- src/components/Account/OrderHistory/EmptyOrderHistory/EmptyOrderHistory.js
+++ src/components/Account/OrderHistory/EmptyOrderHistory/EmptyOrderHistory.js
@@ -3,12 +3,12 @@ import { Collection } from "../../../Collection/Collection";
 
 export const EmptyOrderHistory = () => {
   return (
-    <div>
+    <>
       <InfoMessage
         title={"You have no orders"}
         subtitle={"Explore and add something you love."}
       />
       <Collection />
-    </div>
+    </>
   );
 };
