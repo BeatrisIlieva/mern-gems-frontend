e60288417300c55f6356a3b5bac7bd5aaa585d1b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-10 19:08:48 +0300

    Updated ShippingInformation

--- src/components/ShoppingProcessContainer/Payment/ShippingInformation/ShippingInformation.js
+++ src/components/ShoppingProcessContainer/Payment/ShippingInformation/ShippingInformation.js
@@ -4,42 +4,26 @@ import { NormalTitle } from "../../../NormalTitle/NormalTitle";
 import { Popup } from "../../../Popup/Popup";
 import { ShippingDetailsForm } from "../../../ShippingDetailsForm/ShippingDetailsForm";
 import { Button } from "../../../Button/Button";
+import { UserShippingDetails } from "./UserShippingDetails/UserShippingDetails";
 
 import { useAuthenticationContext } from "../../../../contexts/AuthenticationContext";
 
 import { useService } from "../../../../hooks/useService";
 
-import { userShippingDetailsServiceFactory } from "../../../../services/userShippingDetailsService";
 import { userLoginDetailsServiceFactory } from "../../../../services/userLoginDetailsService";
 
 import styles from "./ShippingInformation.module.css";
 
 export const ShippingInformation = () => {
-  const [userShippingDetails, setUserShippingDetails] = useState([]);
-
   const [userLoginDetails, setUserLoginDetails] = useState([]);
 
   const [displayShippingDetailsPopup, setDisplayShippingDetailsPopup] =
     useState(false);
 
-  const { userId } = useAuthenticationContext();
-
-  const userShippingDetailsService = useService(
-    userShippingDetailsServiceFactory
-  );
-
   const userLoginDetailsService = useService(userLoginDetailsServiceFactory);
 
-  useEffect(() => {
-    userShippingDetailsService
-      .getOne(userId)
-      .then((data) => {
-        setUserShippingDetails(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, [userShippingDetailsService, userId]);
+
+  const { userId } = useAuthenticationContext();
 
   useEffect(() => {
     userLoginDetailsService
@@ -72,49 +56,7 @@ export const ShippingInformation = () => {
         <NormalTitle title={"Email Address"} variant={"bolded"} />
         <NormalTitle title={userLoginDetails.email} variant={"regular"} />
       </div>
-      <ul role="list">
-        <li className={styles["list-item"]}>
-          <NormalTitle title={"Shipping Address"} variant={"bolded"} />
-        </li>
-        <li className={styles["list-item"]}>
-          <NormalTitle
-            title={`${userShippingDetails.firstName} ${userShippingDetails.lastName}`}
-            variant={"regular"}
-          />
-        </li>
-        <li className={styles["list-item"]}>
-          <NormalTitle
-            title={userShippingDetails.phoneNumber}
-            variant={"regular"}
-          />
-        </li>
-        <li className={styles["list-item"]}>
-          <NormalTitle
-            title={userShippingDetails.country}
-            variant={"regular"}
-          />
-        </li>
-        <li className={styles["list-item"]}>
-          <NormalTitle
-            title={`${userShippingDetails.city} ${userShippingDetails.zipCode}`}
-            variant={"regular"}
-          />
-        </li>
-        <li className={styles["list-item"]}>
-          <NormalTitle
-            title={`${userShippingDetails.street} St.`}
-            variant={"regular"}
-          />
-        </li>
-        {userShippingDetails.apartment && (
-          <li className={styles["list-item"]}>
-            <NormalTitle
-              title={`Apt. ${userShippingDetails.apartment}`}
-              variant={"regular"}
-            />
-          </li>
-        )}
-      </ul>
+      <UserShippingDetails />
       {displayShippingDetailsPopup && (
         <Popup
           popupCloseHandler={toggleDisplayShippingDetailsPopup}
---
+++ src/components/ShoppingProcessContainer/Payment/ShippingInformation/UserShippingDetails/UserShippingDetails.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+
+import { NormalTitle } from "../../../../NormalTitle/NormalTitle";
+
+import { useAuthenticationContext } from "../../../../../contexts/AuthenticationContext";
+
+import { useService } from "../../../../../hooks/useService";
+
+import { userShippingDetailsServiceFactory } from "../../../../../services/userShippingDetailsService";
+
+import styles from "./UserShippingDetails.module.css";
+
+export const UserShippingDetails = () => {
+  const [userShippingDetails, setUserShippingDetails] = useState([]);
+
+  const { userId } = useAuthenticationContext();
+
+  const userShippingDetailsService = useService(
+    userShippingDetailsServiceFactory
+  );
+
+  useEffect(() => {
+    userShippingDetailsService
+      .getOne(userId)
+      .then((data) => {
+        setUserShippingDetails(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [userShippingDetailsService, userId]);
+
+  return (
+    <ul role="list">
+      <li className={styles["list-item"]}>
+        <NormalTitle title={"Shipping Address"} variant={"bolded"} />
+      </li>
+      <li className={styles["list-item"]}>
+        <NormalTitle
+          title={`${userShippingDetails.firstName} ${userShippingDetails.lastName}`}
+          variant={"regular"}
+        />
+      </li>
+      <li className={styles["list-item"]}>
+        <NormalTitle
+          title={userShippingDetails.phoneNumber}
+          variant={"regular"}
+        />
+      </li>
+      <li className={styles["list-item"]}>
+        <NormalTitle title={userShippingDetails.country} variant={"regular"} />
+      </li>
+      <li className={styles["list-item"]}>
+        <NormalTitle
+          title={`${userShippingDetails.city} ${userShippingDetails.zipCode}`}
+          variant={"regular"}
+        />
+      </li>
+      <li className={styles["list-item"]}>
+        <NormalTitle
+          title={`${userShippingDetails.street} St.`}
+          variant={"regular"}
+        />
+      </li>
+      {userShippingDetails.apartment && (
+        <li className={styles["list-item"]}>
+          <NormalTitle
+            title={`Apt. ${userShippingDetails.apartment}`}
+            variant={"regular"}
+          />
+        </li>
+      )}
+    </ul>
+  );
+};
