5fbbecb68572509121159401bcdbbb642542baba
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-10 18:34:54 +0300

    Updated Popup

--- src/components/MiniBag/Popup/Popup.js
+++ src/components/MiniBag/Popup/Popup.js
@@ -1,24 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faXmark } from "@fortawesome/free-solid-svg-icons";
-
-// import styles from "./Popup.module.css";
-
-// export const Popup = ({ popupCloseHandler, title, children, variant}) => {
-//   return (
-//     <section className={styles["overlay"]}>
-//       <div className={styles["modal"]}>
-//         <FontAwesomeIcon
-//           icon={faXmark}
-//           className={styles["x-mark"]}
-//           onClick={popupCloseHandler}
-//         />
-//         <h2 className={styles["title"]}>{title}</h2>
-//         {children}
-//       </div>
-//     </section>
-//   );
-// };
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
