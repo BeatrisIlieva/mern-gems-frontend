523785020a78c59d2a7e4db979d751445f8c9146
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-10 19:15:44 +0300

    Updated Payment

--- src/components/ShoppingProcessContainer/Payment/Payment.js
+++ src/components/ShoppingProcessContainer/Payment/Payment.js
@@ -1,11 +1,7 @@
 import { ShoppingProcessContainer } from "../ShoppingProcessContainer";
-
 import { ShippingInformation } from "./ShippingInformation/ShippingInformation";
-
 import { CardDetailsForm } from "../../CardDetailsForm/CardDetailsForm";
-
 import { ContainerTitle } from "../ContainerTitle/ContainerTitle";
-
 import { LeftSide } from "../LeftSide/LeftSide";
 import { RightSide } from "../RightSide/RightSide";
 import { OrderSummary } from "../OrderSummary/OrderSummary";
