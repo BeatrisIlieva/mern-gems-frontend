e6b4cf67e38c76549b502d487191625d9ea1f9c1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-10 18:35:18 +0300

    Updated MiniBag

--- src/components/MiniBag/MiniBag.js
+++ src/components/MiniBag/MiniBag.js
@@ -1,17 +1,14 @@
-import { useBagContext } from "../../contexts/BagContext";
-
 import { NonEmptyMiniBag } from "./NonEmptyMiniBag/NonEmptyMiniBag";
-
 import { EmptyMiniBag } from "./EmptyMiniBag/EmptyMiniBag";
-import { Popup } from "./Popup/Popup"
+import { Popup } from "./Popup/Popup";
+
+import { useBagContext } from "../../contexts/BagContext";
 
 export const MiniBag = ({ toggleDisplayMiniBagPopup }) => {
   const { bagTotalQuantity } = useBagContext();
 
   return (
-    <Popup
-      popupCloseHandler={toggleDisplayMiniBagPopup}
-    >
+    <Popup popupCloseHandler={toggleDisplayMiniBagPopup}>
       {bagTotalQuantity > 0 ? (
         <NonEmptyMiniBag
           toggleDisplayMiniBagPopup={toggleDisplayMiniBagPopup}
