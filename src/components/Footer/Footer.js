48ba865fb3dc029e6b511684e869db8907b3ed2e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-10 18:02:03 +0300

    Updated Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import styles from "./Footer.module.css";
 export const Footer = () => {
   return (
     <>
-      <HorizontalLine variant={"large"} />
+      <HorizontalLine variantHr={"large"} />
       <footer className={styles["footer"]}>
         <div className={styles["footer-container"]}>
           © 2024 MERN Gems | Beatris Ilieve | beatrisilieve@icloud.com
