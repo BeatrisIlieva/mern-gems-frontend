4b5d9225cbdeb6e93df32610a3c79096ab847b8a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-12 17:09:34 +0300

    Organized folders

--- src/components/Account/OrderHistory/EmptyOrderHistory/EmptyOrderHistory.js
+++ src/components/Account/OrderHistory/EmptyOrderHistory/EmptyOrderHistory.js
@@ -1,5 +1,5 @@
 import { InfoMessage } from "../../../InfoMessage/InfoMessage";
-import { Collection } from "../../../Collection/Collection";
+import { Collection } from "../../../reusable/Collection/Collection";
 
 export const EmptyOrderHistory = () => {
   return (
--- src/components/ShoppingProcessContainer/Bag/EmptyBag/EmptyBag.js
+++ src/components/ShoppingProcessContainer/Bag/EmptyBag/EmptyBag.js
@@ -1,4 +1,4 @@
-import { Collection } from "../../../Collection/Collection";
+import { Collection } from "../../../reusable/Collection/Collection";
 
 import { InfoMessage } from "../../../InfoMessage/InfoMessage";
 
