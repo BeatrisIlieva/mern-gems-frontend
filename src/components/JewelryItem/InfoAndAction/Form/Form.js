c929ba80b80ee3e53ac2ff2e05373e7e0cf8ae03
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-12 17:03:08 +0300

    Organized folders

--- src/components/JewelryItem/InfoAndAction/Form/Form.js
+++ src/components/JewelryItem/InfoAndAction/Form/Form.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { Button } from "../../../Button/Button";
+import { Button } from "../../../reusable/Button/Button";
 import { Sizes } from "./Sizes/Sizes";
 
 import { useJewelryItemContext } from "../../../../contexts/JewelryItemContext";
