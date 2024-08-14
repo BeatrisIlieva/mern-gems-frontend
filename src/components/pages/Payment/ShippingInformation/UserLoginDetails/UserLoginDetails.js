6c5070a93813d54a9caa328f6dce54cf78f0c280
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-14 14:04:16 +0300

    UserLogindetails

--- src/components/pages/Payment/ShippingInformation/UserLoginDetails/UserLoginDetails.js
+++ src/components/pages/Payment/ShippingInformation/UserLoginDetails/UserLoginDetails.js
@@ -5,23 +5,6 @@ import { useUserLoginDetails } from "../../../../../hooks/useUserLoginDetails";
 import styles from "./UserLoginDetails.module.css";
 
 export const UserLoginDetails = () => {
-  // const [userLoginDetails, setUserLoginDetails] = useState([]);
-
-  // const { userId } = useAuthenticationContext();
-
-  // const userLoginDetailsService = useService(userLoginDetailsServiceFactory);
-
-  // useEffect(() => {
-  //   userLoginDetailsService
-  //     .getOne(userId)
-  //     .then((data) => {
-  //       setUserLoginDetails(data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  // }, [userLoginDetailsService, userId]);
-
   const { email } = useUserLoginDetails();
 
   return (
