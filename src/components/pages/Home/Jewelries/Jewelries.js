f06c9b8bf2934907ad74630b6550df863520fc40
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-16 17:21:21 +0300

    Updated jewelry functionality

--- src/components/pages/Home/Jewelries/Jewelries.js
+++ src/components/pages/Home/Jewelries/Jewelries.js
@@ -1,23 +1,11 @@
-import { useState, useEffect } from "react";
-
-import { useService } from "../../../../hooks/useService";
-
-import { jewelryServiceFactory } from "../../../../services/jewelryService";
-
-import { filterJewelriesByCategoryId } from "./helpers";
-
-import { LoadingSpinner } from "../../JewelryList/LoadingSpinner/LoadingSpinner";
-
-import {Bracelets} from "./Bracelets/Bracelets"
+import { Bracelets } from "./Bracelets/Bracelets";
 
 import styles from "./Jewelries.module.css";
 
 export const Jewelries = () => {
-
-
   return (
-  <section className={styles["jewelries"]}>
-    <Bracelets/>
+    <section className={styles["jewelries"]}>
+      <Bracelets />
     </section>
-    );
+  );
 };
