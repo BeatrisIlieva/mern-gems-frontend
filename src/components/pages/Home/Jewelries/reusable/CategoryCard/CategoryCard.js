789ac5fe009c2088e9fb706ebd0a5c89a697d618
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-16 16:34:12 +0300

    Updated jewelry functionality

--- src/components/pages/Home/Jewelries/reusable/CategoryCard/CategoryCard.js
+++ src/components/pages/Home/Jewelries/reusable/CategoryCard/CategoryCard.js
@@ -1,24 +1,36 @@
+import { useState } from "react";
+
 import { MiniImages } from "./MiniImages/MiniImages";
 
 import styles from "./CategoryCard.module.css";
 
 export const CategoryCard = ({ entity }) => {
+  const [imageIsHovered, setImageIsHovered] = useState(false);
+
   return (
     <article className={styles["category-card"]}>
-      {/* <div className={styles["thumbnail"]}> */}
+      <div
+        className={`${styles["category-card"]} ${styles["thumbnail"]}`}
+        onMouseEnter={() => setImageIsHovered(true)}
+        onMouseLeave={() => setImageIsHovered(false)}
+      >
         <img
-          className={`${styles["category-card"]} ${styles["image"]}`}
-          src={entity[0].firstImageUrl}
+          src={
+            imageIsHovered ? entity[0].secondImageUrl : entity[0].firstImageUrl
+          }
           alt={entity[0].title}
         />
-        <ul className={`${styles["category-card"]} ${styles["image"]} ${styles["mini-images-list"]}`} role="list">
-          {entity.map((item) => (
-            <li key={item._id} >
-              <MiniImages imageObject={item.miniImage} />
-            </li>
-          ))}
-        </ul>
-      {/* </div> */}
+      </div>
+      <ul
+        className={`${styles["category-card"]} ${styles["image"]} ${styles["mini-images-list"]}`}
+        role="list"
+      >
+        {entity.map((item) => (
+          <li key={item._id}>
+            <MiniImages imageObject={item.miniImage} />
+          </li>
+        ))}
+      </ul>
     </article>
   );
 };
