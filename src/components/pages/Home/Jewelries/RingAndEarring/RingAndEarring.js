ddaead4c1763f97249fcc2b9a034e4656d59f5bc
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-17 16:26:13 +0300

    Updated jewelry functionality

--- src/components/pages/Home/Jewelries/RingAndEarring/RingAndEarring.js
+++ src/components/pages/Home/Jewelries/RingAndEarring/RingAndEarring.js
@@ -6,17 +6,8 @@ import styles from "./RingAndEarring.module.css";
 export const RingAndEarring = () => {
   return (
     <section id={styles["ring-and-earring"]}>
-      <div className={styles["cards-wrapper"]}>
-        <Rings />
-        <Earrings />
-      </div>
-      <div className={styles["thumbnail"]}>
-        <img
-          className={styles["image"]}
-          src="https://res.cloudinary.com/deztgvefu/image/upload/v1723886288/discover/sbs_lp_forgetmenot_h1qc12.jpg"
-          alt="Forget-Me-Not-Collection"
-        />
-      </div>
+      <Rings />
+      <Earrings />
     </section>
   );
 };
