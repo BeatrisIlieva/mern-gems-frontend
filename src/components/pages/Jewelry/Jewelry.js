8536f3d53d007ecfe1aac060c3429d54805ced70
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-19 17:01:03 +0300

    Updated jewelry functionality

--- src/components/pages/Jewelry/Jewelry.js
+++ src/components/pages/Jewelry/Jewelry.js
@@ -1,8 +1,30 @@
+import { LargeImages } from "../../common/LargeImages/LargeImages";
+import { MiniImages } from "../../common/MiniImages/MiniImages";
+import { StockStatus } from "../../common/StockStatus/StockStatus";
+
 import { useJewelryContext } from "../../../contexts/JewelryContext";
 
 import styles from "./Jewelry.module.css";
 
 export const Jewelry = () => {
-  const { selectedEntity, selectedColor } = useJewelryContext();
-  return <img src={selectedEntity[selectedColor].firstImageUrl} alt="" />;
+  const { selectedEntity, selectedColor, updateSelectedColor } =
+    useJewelryContext();
+
+  return (
+    <section id={styles["jewelry"]}>
+      <div className={styles["thumbnail"]}>
+        <StockStatus selectedEntityColor={selectedEntity[selectedColor]} />
+        <LargeImages entity={selectedEntity} colorIndex={selectedColor} />
+      </div>
+      <div className={styles["info-and-action"]}>
+        <h1 className={styles["title"]}></h1>
+        <p className={styles["description"]}></p>
+        <MiniImages
+          colorIndex={selectedColor}
+          entity={selectedEntity}
+          updateColorIndex={updateSelectedColor}
+        />
+      </div>
+    </section>
+  );
 };
