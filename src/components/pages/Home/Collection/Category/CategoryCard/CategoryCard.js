038e50f3338abe5aafdb09abd84e34c33cf66572
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-19 13:36:50 +0300

    Updated jewelry functionality

--- src/components/pages/Home/Collection/Category/CategoryCard/CategoryCard.js
+++ src/components/pages/Home/Collection/Category/CategoryCard/CategoryCard.js
@@ -1,19 +1,9 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import { DualTitleSection } from "../../../../../reusable/DualTitleSection/DualTitleSection";
 import { LargeImages } from "../../common/LargeImages/LargeImages";
 import { MiniImages } from "../../common/MiniImages/MiniImages";
 import { StockStatus } from "../../common/StockStatus/StockStatus";
-
-import { useJewelryContext } from "../../../../../../contexts/JewelryContext";
-
-import { slugify } from "../../../../../../utils/slugify";
-
-import { checkIfItemsHasBeenSoldOut } from "./helpers/checkIfItemsHasBeenSoldOut";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./CategoryCard.module.css";
 
@@ -22,27 +12,9 @@ export const CategoryCard = ({ entity, colorIndex, updateColorIndex }) => {
 
   const [activeMiniImage, setActiveMiniImage] = useState(colorIndex);
 
-
-
-  // const { updateSelectedEntity, updateSelectedColor } = useJewelryContext();
-
-  // const navigate = useNavigate();
-
-
-
   const updateActiveMiniImage = (index) => {
     setActiveMiniImage(index);
   };
-
-  // const slugifiedJewelryTitle = slugify(entity[colorIndex].title);
-
-  // const clickHandler = () => {
-  //   updateSelectedEntity(entity);
-
-  //   updateSelectedColor(colorIndex);
-
-  //   navigate(`/${slugifiedJewelryTitle}`);
-  // };
 
   const selectedEntity = entity[colorIndex];
 
@@ -56,26 +28,11 @@ export const CategoryCard = ({ entity, colorIndex, updateColorIndex }) => {
           : styles["category-card"]
       }
     >
-      {/* <DualTitleSection
+      <DualTitleSection
         firstTitle={`$${entity[0].inventories[0].price} - $${entity[0].inventories[2].price}`}
-        secondTitle={
-          <span className={styles["stock"]}>
-            <FontAwesomeIcon
-              icon={faCircle}
-              className={`${styles["icon"]} ${
-                isSoldOut ? styles["sold-out"] : styles["in-stock"]
-              }`}
-            />¸¸¸
-            {isSoldOut ? "Sold Out" : "In Stock"}
-          </span>
-        }
+        secondTitle={<StockStatus entity={selectedEntity} />}
         variant={"regular"}
-      /> */}
-          <DualTitleSection
-      firstTitle={`$${entity[0].inventories[0].price} - $${entity[0].inventories[2].price}`}
-      secondTitle={<StockStatus entity={selectedEntity} />}
-      variant={"regular"}
-    />
+      />
       <LargeImages entity={entity} colorIndex={colorIndex} />
       <MiniImages
         colorIndex={colorIndex}
