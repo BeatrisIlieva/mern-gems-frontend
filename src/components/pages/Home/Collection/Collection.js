d18e0f43ddde3ec9e181bea03ad2031ca080855a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-21 13:35:02 +0300

    Updated jewelry functionality

--- src/components/pages/Home/Collection/Collection.js
+++ src/components/pages/Home/Collection/Collection.js
@@ -1,26 +1,3 @@
-// import { HeroBanner } from "./HeroBanner/HeroBanner";
-// import { Category } from "./Category/Category";
-
-// import { CATEGORIES_BY_ID_AND_INITIAL_COLOR_ID } from "./constants/categoriesByIdAndInitialColorId";
-
-// import styles from "./Collection.module.css";
-
-// export const Collection = () => {
-//   return (
-//     <section id={styles["collection"]}>
-//       <HeroBanner />
-//       <div className={styles["wrapper"]}>
-//         {Object.entries(CATEGORIES_BY_ID_AND_INITIAL_COLOR_ID).map(
-//           ([category, { id, initialColorId }]) => (
-//             <Category key={id} category={category} categoryId={id} colorId={initialColorId} />
-//           )
-//         )}
-//       </div>
-//     </section>
-//   );
-// };
-
-
 import { HeroBanner } from "./HeroBanner/HeroBanner";
 import { Category } from "./Category/Category";
 
@@ -35,7 +12,11 @@ export const Collection = () => {
       <div className={styles["wrapper"]}>
         {Object.entries(CATEGORIES_BY_ID_AND_INITIAL_COLOR_ID).map(
           ([categoryTitle, colorTitle]) => (
-            <Category key={categoryTitle} categoryTitle={categoryTitle} colorTitle={colorTitle} />
+            <Category
+              key={categoryTitle}
+              categoryTitle={categoryTitle}
+              colorTitle={colorTitle}
+            />
           )
         )}
       </div>
