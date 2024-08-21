3d04bc9c172270d832a9ce4bf01fc9a5ebb03195
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-21 15:04:25 +0300

    Updated bag functionality

---
+++ src/hooks/useBag.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+import { useService } from "./useService";
+import { bagServiceFactory } from "../services/bagService";
+
+export const useBag = ({ userId }) => {
+  const [bagItems, setBagItems] = useState([]);
+
+  const bagService = useService(bagServiceFactory);
+
+  useEffect(() => {
+    bagService
+      .getAll(userId)
+      .then((data) => {
+        setBagItems(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return { bagItems };
+};
