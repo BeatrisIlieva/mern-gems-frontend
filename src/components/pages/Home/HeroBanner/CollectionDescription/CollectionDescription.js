4d2a069c23fe3c5f4f38fd7be9f058d8e24d3385
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-23 11:18:01 +0300

    Updated jewelry functionality

--- src/components/pages/Home/HeroBanner/CollectionDescription/CollectionDescription.js
+++ src/components/pages/Home/HeroBanner/CollectionDescription/CollectionDescription.js
@@ -11,16 +11,6 @@ export const CollectionDescription = () => {
   return (
     <div className={styles["description-wrapper"]}>
       <div className={styles["collection-description"]}>
-        {/* <MiniImage
-          imageUrl={IMAGE_BY_URL_AND_VARIANT.pink.imageUrl}
-          variant={IMAGE_BY_URL_AND_VARIANT.pink.variant}
-          waveEffect={IMAGE_BY_URL_AND_VARIANT.pink.waveEffect}
-        />
-        <MiniImage
-          imageUrl={IMAGE_BY_URL_AND_VARIANT.blue.imageUrl}
-          variant={IMAGE_BY_URL_AND_VARIANT.blue.variant}
-          waveEffect={IMAGE_BY_URL_AND_VARIANT.blue.waveEffect}
-        /> */}
         <div className={styles["wrapper"]}>
           <div className={styles["inner-wrapper"]}>
             <MiniImage
@@ -29,19 +19,18 @@ export const CollectionDescription = () => {
               waveEffect={IMAGE_BY_URL_AND_VARIANT.butterfly.waveEffect}
             />
             <XLargeTitle title={"Forget-Me-Not Collection"} />
-
-          <MiniImage
-            imageUrl={IMAGE_BY_URL_AND_VARIANT.white.imageUrl}
-            variant={IMAGE_BY_URL_AND_VARIANT.white.variant}
-            waveEffect={IMAGE_BY_URL_AND_VARIANT.white.waveEffect}
-          />
-          <Paragraph
-            text={
-              "The enchanting and delicate beauty of a Forget-Me-Not flower in bloom is captured in a series of fine jewelry designs that celebrate the endless beauty of nature’s greatest gifts – rare gemstones and flowers in bloom."
-            }
-            textAlign={"center"}
-          />
-          <Button variant={"animated"} title={"Discover"} />
+            <MiniImage
+              imageUrl={IMAGE_BY_URL_AND_VARIANT.white.imageUrl}
+              variant={IMAGE_BY_URL_AND_VARIANT.white.variant}
+              waveEffect={IMAGE_BY_URL_AND_VARIANT.white.waveEffect}
+            />
+            <Paragraph
+              text={
+                "The enchanting and delicate beauty of a Forget-Me-Not flower in bloom is captured in a series of fine jewelry designs that celebrate the endless beauty of nature’s greatest gifts – rare gemstones and flowers in bloom."
+              }
+              textAlign={"center"}
+            />
+            <Button variant={"animated"} title={"Discover"} />
           </div>
         </div>
       </div>
