36040478de9268cf7ee95ee4aaef314d58611903
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-23 11:21:04 +0300

    Updated jewelry functionality

--- src/components/pages/Home/HeroBanner/HeroBanner.js
+++ src/components/pages/Home/HeroBanner/HeroBanner.js
@@ -1,4 +1,3 @@
-import { CollectionDescription } from "./CollectionDescription/CollectionDescription";
 import { CollectionImage } from "./CollectionImage/CollectionImage";
 
 import styles from "./HeroBanner.module.css";
@@ -6,7 +5,6 @@ import styles from "./HeroBanner.module.css";
 export const HeroBanner = () => {
   return (
     <article id={styles["hero-banner"]}>
-      {/* <CollectionDescription /> */}
       <CollectionImage />
     </article>
   );
