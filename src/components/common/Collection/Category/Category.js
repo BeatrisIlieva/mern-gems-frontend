1f96a27ae785dcce383ec72a27c365702bae476a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-24 17:39:21 +0300

    Updated jewelry functionality

--- src/components/common/Collection/Category/Category.js
+++ src/components/common/Collection/Category/Category.js
@@ -1,56 +1,20 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 import { CategoryCard } from "./CategoryCard/CategoryCard";
 import { CardSkeleton } from "../CardSkeleton/CardSkeleton";
-
-import { useService } from "../../../../hooks/useService";
-
-import { jewelryServiceFactory } from "../../../../services/jewelryService";
 
 import { useJewelry } from "../../../../hooks/useJewelry";
 
 export const Category = ({ categoryTitle, colorTitle }) => {
-  const navigate = useNavigate();
-
-  // const [selectedColorTitle, setSelectedColorTitle] = useState(colorTitle);
-
-  // const updateColorTitle = (title) => {
-  //   navigate(`/collection/${colorTitle}`)
-  //   setSelectedColorTitle(title);
-
-  // };
-
-  // const [jewelriesByCategory, setJewelriesByCategory] = useState([]);
-
-  // const jewelryService = useService(jewelryServiceFactory);
-
   const { jewelriesByCategory } = useJewelry({
     categoryTitle,
     colorTitle,
   });
-
-  // useEffect(() => {
-  //   jewelryService
-  //     .getOne(categoryTitle, colorTitle)
-  //     .then((data) => {
-  //       setJewelriesByCategory(data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     })
-  //     .finally(() => {});
-  // }, [categoryTitle, colorTitle, jewelryService]);
 
   return (
     <>
       {jewelriesByCategory.length < 1 ? (
         <CardSkeleton />
       ) : (
-        <CategoryCard
-          jewelriesByCategory={jewelriesByCategory}
-          // updateColorTitle={updateColorTitle}
-        />
+        <CategoryCard jewelriesByCategory={jewelriesByCategory} />
       )}
     </>
   );
