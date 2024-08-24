b1214e18f633e3282ff32de615b6197b2080c718
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-24 18:05:35 +0300

    Updated jewelry functionality

--- src/components/pages/CollectionList/CollectionList.js
+++ src/components/pages/CollectionList/CollectionList.js
@@ -1,5 +1,11 @@
 import { Collection } from "../../common/Collection/Collection";
 
+import styles from "./CollectionList.module.css";
+
 export const CollectionList = () => {
-  return <Collection />;
+  return (
+    <section className={styles["collection-list"]}>
+      <Collection />
+    </section>
+  );
 };
