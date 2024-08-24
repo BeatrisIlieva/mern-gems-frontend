0cb2c07dcdfc575cfb22498190cd272e6a95025f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-24 17:38:48 +0300

    Updated jewelry functionality

--- src/components/common/Collection/Category/CategoryCard/CategoryCard.js
+++ src/components/common/Collection/Category/CategoryCard/CategoryCard.js
@@ -5,13 +5,9 @@ import { LargeImages } from "../../../LargeImages/LargeImages";
 import { MiniImages } from "../../../MiniImages/MiniImages";
 import { StockStatus } from "../../../StockStatus/StockStatus";
 
-import { useIsTransitioning } from "../../../../../hooks/useIsTransitioning";
-
 import styles from "./CategoryCard.module.css";
 
-export const CategoryCard = ({ jewelriesByCategory}) => {
-  const {isTransitioning} = useIsTransitioning();
-
+export const CategoryCard = ({ jewelriesByCategory }) => {
   const [articleIsHovered, setArticleIsHovered] = useState(false);
 
   return (
@@ -23,10 +19,6 @@ export const CategoryCard = ({ jewelriesByCategory}) => {
           ? `${styles["category-card"]} ${styles["hovered"]}`
           : styles["category-card"]
       }
-      // className={
-      //     `${styles["category-card"]} ${
-      //   isTransitioning ? styles["slide-out"] : styles["slide-in"]
-      // }` }
     >
       <DualTitleSection
         firstTitle={`$${jewelriesByCategory[0].inventories[0].price} - $${jewelriesByCategory[0].inventories[2].price}`}
@@ -34,10 +26,7 @@ export const CategoryCard = ({ jewelriesByCategory}) => {
         variant={"regular"}
       />
       <LargeImages jewelriesByCategory={jewelriesByCategory} />
-      <MiniImages
-        jewelriesByCategory={jewelriesByCategory}
-        // updateColorTitle={updateColorTitle}
-      />
+      <MiniImages jewelriesByCategory={jewelriesByCategory} />
     </article>
   );
 };
