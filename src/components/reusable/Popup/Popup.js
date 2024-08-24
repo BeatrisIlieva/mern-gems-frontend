e93b6b3c65b9c8135e0f7ad262babf6b95e85a27
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-24 19:05:54 +0300

    Updated jewelry functionality

--- src/components/reusable/Popup/Popup.js
+++ src/components/reusable/Popup/Popup.js
@@ -1,55 +1,8 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
-// import styles from "./Popup.module.css";
-
-// export const Popup = ({
-//   popupCloseHandler,
-//   children,
-//   modalVariant,
-//   overlayVariant,
-// }) => {
-//   const showXMark = modalVariant !== "authentication";
-
-//   document.body.style.overflow = "hidden";
-
-//   const header = document.querySelector(".header");
-
-//   header.style.zIndex = "0";
-
-
-
-//   const closeHandler = () => {
-//     document.body.style.overflow = "visible";
-
-//     header.style.zIndex = "100";
-
-
-//     popupCloseHandler();
-//   };
-
-//   return (
-//     <section
-//       className={`${styles["overlay"]} ${styles["slide-in"]} ${styles[overlayVariant]}`}
-//     >
-//       <div className={`${styles["modal"]} ${styles[modalVariant]}`}>
-//         {showXMark && (
-//           <FontAwesomeIcon
-//             icon={faXmark}
-//             className={styles["x-mark"]}
-//             onClick={closeHandler}
-//           />
-//         )}
-//         {children}
-//       </div>
-//     </section>
-//   );
-// };
-
-
-import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Popup.module.css";
 
 export const Popup = ({
@@ -58,43 +11,45 @@ export const Popup = ({
   modalVariant,
   overlayVariant,
 }) => {
-  const [isOpen, setIsOpen] = useState(true); 
+  const showXMark = modalVariant !== "authentication";
+
+  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
-
-    const headerElement = document.getElementById('header');
+    const headerElement = document.getElementById("header");
 
     if (isOpen) {
       document.body.style.overflow = "hidden";
-      headerElement.style.zIndex = "0"; 
+      headerElement.style.zIndex = "0";
     } else {
       document.body.style.overflow = "visible";
-      headerElement.style.zIndex = "100"; 
+      headerElement.style.zIndex = "100";
     }
-
 
     return () => {
       headerElement.style.zIndex = "100";
       document.body.style.overflow = "visible";
     };
-  }, [isOpen]); 
+  }, [isOpen]);
 
   const closeHandler = () => {
-    setIsOpen(false); 
-    popupCloseHandler(); 
+    setIsOpen(false);
+    popupCloseHandler();
   };
 
   return (
     <section
       className={`${styles["overlay"]} ${styles["slide-in"]} ${styles[overlayVariant]}`}
-      onAnimationEnd={() => !isOpen && setIsOpen(false)} 
+      onAnimationEnd={() => !isOpen && setIsOpen(false)}
     >
       <div className={`${styles["modal"]} ${styles[modalVariant]}`}>
-        <FontAwesomeIcon
-          icon={faXmark}
-          className={styles["x-mark"]}
-          onClick={closeHandler}
-        />
+        {showXMark && (
+          <FontAwesomeIcon
+            icon={faXmark}
+            className={styles["x-mark"]}
+            onClick={closeHandler}
+          />
+        )}
         {children}
       </div>
     </section>
