2ce301d11bc6fffe667ef1cef30197115971f4db
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-24 15:15:29 +0300

    Updated jewelry functionality

--- src/components/pages/Home/HeroBanner/HeroBanner.js
+++ src/components/pages/Home/HeroBanner/HeroBanner.js
@@ -1,12 +1,8 @@
 import { Collection } from "./Collection/Collection";
 
-import { useIsTransitioning } from "../../../../hooks/useIsTransitioning";
-
 import styles from "./HeroBanner.module.css";
 
 export const HeroBanner = () => {
-  const { isTransitioning } = useIsTransitioning();
-
   return (
     <section className={styles["hero-banner"]}>
       <Collection />
