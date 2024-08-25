04a66c7a6c89bd549e891d0a60bd0455dae9ebeb
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-25 18:21:06 +0300

    Updated jewelry functuonality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { ScrollToTop } from "./components/utils/ScrollToTop/ScrollToTop";
 import { Header } from "./components/layout/Header/Header";
 import { Main } from "./components/layout/Main/Main";
 import { Footer } from "./components/layout/Footer/Footer";
@@ -9,7 +8,6 @@ import styles from "./App.css";
 function App() {
   return (
     <div className={styles["app"]}>
-      <ScrollToTop />
       <Header />
       <Main />
       <Footer />
