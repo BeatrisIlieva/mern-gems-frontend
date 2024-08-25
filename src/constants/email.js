9a4143705bbaa784829747d3b7ac2864cb59b966
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-25 15:16:07 +0300

    Updated jewelry functuonality

--- src/constants/email.js
+++ src/constants/email.js
@@ -3,10 +3,10 @@ export const EMAIL_PATTERN = /^[A-Za-z0-9]+@+[a-z]+\.[a-z]{2,4}$/;
 export const EMAIL_ERROR_MESSAGE = "* Ensure you enter a valid email address";
 
 export const EMAIL_ALREADY_EXISTS_ERROR_MESSAGE =
-  "This email address is already registered. Please log in or use a different one";
+  "This email address is already registered";
 
 export const INVALID_CREDENTIALS_ERROR_MESSAGE =
   "We couldn't find an account matching the email and password you entered";
 
 export const EMAIL_QUESTION_MARK =
-  "Please enter your email address so that we can contact you with important information regarding your order.";
+  "Please enter your email address so that we can contact you with important information regarding your order";
