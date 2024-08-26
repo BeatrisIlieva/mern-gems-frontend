46418bd025f6efe3305447e5c85b2385fc299e7b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-26 09:32:53 +0300

    Updated files

--- src/components/layout/Header/MainHeader/MainHeader.js
+++ src/components/layout/Header/MainHeader/MainHeader.js
@@ -1,5 +1,5 @@
 import { HorizontalLine } from "../../../reusable/HorizontalLine/HorizontalLine";
-import { NavLinkItem } from "../../../reusable/NavLinkItem/NavLinkItem";
+import { NavLinkItem } from "./NavLinkItem/NavLinkItem";
 import { Link } from "react-router-dom";
 
 import { LEFT_NAV_ITEMS, RIGHT_NAV_ITEMS } from "./navItems";
