57b5fc60939972a6e7f9cde65e3946cdfbf4324e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-26 11:44:54 +0300

    Updated files

--- src/components/reusable/ShoppingProcessContainer/ShoppingProcessContainer.js
+++ src/components/reusable/ShoppingProcessContainer/ShoppingProcessContainer.js
@@ -3,7 +3,6 @@ import styles from "./ShoppingProcessContainer.module.css";
 export const ShoppingProcessContainer = ({ children }) => {
   return (
     <section className={styles["shopping-process-container"]}>
-      <div className={styles["top"]}></div>
       <div className={styles["children"]}>{children}</div>
     </section>
   );
