0adcf0ab47a478300676fc0a29659551a017aec6
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-27 11:37:49 +0300

    Updated responsive design

--- src/components/pages/Bag/Bag.js
+++ src/components/pages/Bag/Bag.js
@@ -1,15 +1,9 @@
-import { Link } from "react-router-dom";
-
 import { ShoppingProcessContainer } from "../../reusable/ShoppingProcessContainer/ShoppingProcessContainer";
-import { ChildWrapper } from "../../reusable/ChildWrapper/ChildWrapper";
-import { BagList } from "../../common/BagList/BagList";
-import { OrderSummary } from "../../common/OrderSummary/OrderSummary";
-import { Button } from "../../reusable/Button/Button";
 import { Collection } from "../../common/Collection/Collection";
 import { InfoMessage } from "../../reusable/InfoMessage/InfoMessage";
-import { BagHeader } from "../../common/BagHeader/BagHeader";
 import { BagContent } from "./BagContent/BagContent";
 import { OrderSummaryContent } from "./OrderSummaryContent/OrderSummaryContent";
+
 import { useBagContext } from "../../../contexts/BagContext";
 
 import { useIsMobile } from "../../../hooks/useIsMobile";
