78a362256987b7a3606b92c519e2fb7e468637f9
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-27 16:29:23 +0300

    Updated responsive design

--- src/components/reusable/Button/Button.js
+++ src/components/reusable/Button/Button.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import styles from "./Button.module.css";
 
 export const Button = ({
@@ -8,16 +10,28 @@ export const Button = ({
   type,
   isLoading,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const toggleIsHovered = () => {
+    setIsHovered((isHovered) => !isHovered);
+  };
+
   return (
     <button
       type={type}
       className={
         !buttonIsDisabled && !isLoading
-          ? `${styles["button"]} ${styles[variant]}`
+          ? `${styles["button"]} ${styles[variant]} ${
+              isHovered ? styles["hovered"] : ""
+            }`.trim()
           : `${styles["button"]} ${styles[variant]} ${styles["disabled"]}`
       }
       disabled={buttonIsDisabled}
       onClick={callBackFunction}
+      onMouseEnter={toggleIsHovered}
+      onMouseLeave={toggleIsHovered}
+      onTouchStart={toggleIsHovered}
+      onTouchEnd={toggleIsHovered}
     >
       {isLoading ? "Processing..." : title}
     </button>
