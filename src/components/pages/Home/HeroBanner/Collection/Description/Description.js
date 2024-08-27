cb272176447fa07e882f03015ee804d695af269f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-27 12:21:48 +0300

    Updated responsive design

--- src/components/pages/Home/HeroBanner/Collection/Description/Description.js
+++ src/components/pages/Home/HeroBanner/Collection/Description/Description.js
@@ -25,7 +25,7 @@ export const Description = () => {
             variant={IMAGE_BY_URL_AND_VARIANT.butterfly.variant}
             waveEffect={IMAGE_BY_URL_AND_VARIANT.butterfly.waveEffect}
           />
-          <LargeTitle title={"Forget-Me-Not Collection"} />
+          <LargeTitle title={"Forget-Me-Not Collection"} textAlign={"align-center"}/>
           <div className={styles["inner-wrapper"]}>
             <MiniImage
               imageUrl={IMAGE_BY_URL_AND_VARIANT.white.imageUrl}
