03a886bf5cf45539c971fae5b9dc4f865b303b02
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-27 14:48:12 +0300

    Updated responsive design

--- src/components/common/BagHeader/BagHeader.js
+++ src/components/common/BagHeader/BagHeader.js
@@ -1,18 +1,12 @@
 import { DualTitleSection } from "../../reusable/DualTitleSection/DualTitleSection";
-import { LargeTitle } from "../../reusable/LargeTitle/LargeTitle";
 import { Delivery } from "./Delivery/Delivery";
-import { BagCount } from "./Delivery/BagCount/BagCount";
+import { BagCount } from "./BagCount/BagCount";
 
 export const BagHeader = () => {
   return (
-    // <DualTitleSection
-    //   firstTitle={<LargeTitle title={"My Bag"} />}
-    //   secondTitle={<Delivery />}
-    //   variant={"bolded"}
-    // />
     <DualTitleSection
       firstTitle={<Delivery />}
-      secondTitle={<BagCount/>}
+      secondTitle={<BagCount />}
       variant={"bolded"}
     />
   );
