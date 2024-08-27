3dd1d0704271c6402b6d7fca7af922986f3afd6e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-27 18:25:27 +0300

    Updated responsive design

--- src/components/pages/Payment/ShippingInformation/ShippingInformation.js
+++ src/components/pages/Payment/ShippingInformation/ShippingInformation.js
@@ -9,24 +9,18 @@ import styles from "./ShippingInformation.module.css";
 export const ShippingInformation = ({ toggleDisplayShippingDetailsPopup }) => {
   return (
     <section className={styles["shipping-information"]}>
-      {/* <div className={styles["header-wrapper"]}> */}
-        <DualTitleSection
-          firstTitle={
-            <LargeTitle
-              title={"Shipping Information"}
-              textAlign={"align-left"}
-            />
-          }
-          secondTitle={
-            <Button
-              title={"Edit"}
-              callBackFunction={toggleDisplayShippingDetailsPopup}
-              variant={"underlined"}
-            />
-          }
-        />
-        {/* <h2 className={styles["title"]}>Shipping Information</h2> */}
-      {/* </div> */}
+      <DualTitleSection
+        firstTitle={
+          <LargeTitle title={"Shipping Information"} textAlign={"align-left"} />
+        }
+        secondTitle={
+          <Button
+            title={"Edit"}
+            callBackFunction={toggleDisplayShippingDetailsPopup}
+            variant={"underlined"}
+          />
+        }
+      />
       <UserLoginDetails />
       <UserShippingDetails />
     </section>
