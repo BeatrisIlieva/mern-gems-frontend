3256113cba083ce20f0669d99e81cde7f993832a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-29 13:15:37 +0300

    Updated responsive design

--- src/hooks/useIsMobile.js
+++ src/hooks/useIsMobile.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 
 export const useIsMobile = () => {
-  const [isReversed, setIsReversed] = useState(window.innerWidth <= 1280);
+  const [isReversed, setIsReversed] = useState(window.innerWidth <= 1023);
 
   const handleResize = () => {
-    setIsReversed(window.innerWidth <= 1280);
+    setIsReversed(window.innerWidth <= 1023);
   };
 
   useEffect(() => {
