e5ccfde2e4054d8f52af3e9bdb5f6c9787a81a9a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-30 17:58:22 +0300

    Updated jewelry functionality

--- src/components/common/Collection/CategoryCard/CategoryCard.js
+++ src/components/common/Collection/CategoryCard/CategoryCard.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
-import { DualTitleSection } from "../../../reusable/DualTitleSection/DualTitleSection";
-import { LargeImages } from "../../LargeImages/LargeImages";
-import { MiniImages } from "../../MiniImages/MiniImages";
-import { StockStatus } from "../../StockStatus/StockStatus";
-import { LoadingSpinner } from "../../LoadingSpinner/LoadingSpinner";
+
+
+import { Card } from "./Card/Card";
 
 import { useJewelry } from "../../../../hooks/useJewelry";
 
-import styles from "./CategoryCard.module.css";
+
 
 export const CategoryCard = ({ categoryTitle, colorTitle }) => {
   const [selectedColor, setSelectedColor] = useState(colorTitle);
@@ -30,44 +28,14 @@ export const CategoryCard = ({ categoryTitle, colorTitle }) => {
     colorTitle: selectedColor,
   });
 
-  const [articleIsHovered, setArticleIsHovered] = useState(false);
-
   return (
     <>
       {jewelriesByCategory.length > 0 && (
-        <div className={styles["outer-wrapper"]}>
-          {isTransitioning && (
-            <LoadingSpinner isTransitioning={isTransitioning} />
-          )}
-          <div
-            className={`${styles["spinner-wrapper"]} ${
-              isTransitioning ? `${styles["transitioning"]}` : ""
-            }`.trim()}
-          >
-            <article
-              onMouseEnter={() => setArticleIsHovered(true)}
-              onMouseLeave={() => setArticleIsHovered(false)}
-              className={
-                articleIsHovered
-                  ? `${styles["category-card"]} ${styles["hovered"]}`
-                  : styles["category-card"]
-              }
-            >
-              <DualTitleSection
-                firstTitle={`$${jewelriesByCategory[0].inventories[0].price} - $${jewelriesByCategory[0].inventories[2].price}`}
-                secondTitle={
-                  <StockStatus jewelriesByCategory={jewelriesByCategory} />
-                }
-                variant={"regular"}
-              />
-              <LargeImages jewelriesByCategory={jewelriesByCategory} />
-              <MiniImages
-                jewelriesByCategory={jewelriesByCategory}
-                updateSelectedColor={updateSelectedColor}
-              />
-            </article>
-          </div>
-        </div>
+        <Card
+          jewelriesByCategory={jewelriesByCategory}
+          isTransitioning={isTransitioning}
+          updateSelectedColor={updateSelectedColor}
+        />
       )}
     </>
   );
