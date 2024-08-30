38841abdfad4d0a486ee647c4c7cd930ab3bfa34
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-30 18:48:34 +0300

    Updated jewelry functionality

--- src/components/common/LargeImages/LargeImage/LargeImage.js
+++ src/components/common/LargeImages/LargeImage/LargeImage.js
@@ -1,12 +1,42 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { slugify } from "../../../../utils/slugify";
+
 import styles from "./LargeImage.module.css";
 
 export const LargeImage = ({
   jewelriesByCategory,
   firstImageUrlIsActive,
   variant,
+  toggleFirstImageUrlIsActive,
 }) => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const locationIsCollectionPage = location.pathname === "/collection";
+
+  const clickHandler = () => {
+    if (locationIsCollectionPage) {
+      const categoryTitle = jewelriesByCategory[0].categories[0].title;
+
+      const colorTitle = jewelriesByCategory[0].colors[0].title;
+
+      const slugifiedCategoryTitle = slugify(categoryTitle);
+
+      const slugifiedColorTitle = slugify(colorTitle);
+
+      navigate(`/collection/${slugifiedCategoryTitle}/${slugifiedColorTitle}`);
+    } else {
+      toggleFirstImageUrlIsActive();
+    }
+  };
+
   return (
-    <div className={`${styles["thumbnail"]} ${styles[variant]}`}>
+    <div
+      className={`${styles["thumbnail"]} ${styles[variant]}`}
+      onClick={clickHandler}
+    >
       <img
         className={`${styles["image"]} ${
           firstImageUrlIsActive
--- src/components/common/LargeImages/LargeImages.js
+++ src/components/common/LargeImages/LargeImages.js
@@ -1,12 +1,9 @@
 import { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 
 import { CircleIcons } from "./CircleIcons/CircleIcons";
 import { LargeImage } from "./LargeImage/LargeImage";
 
 import { useIsTransitioning } from "../../../hooks/useIsTransitioning";
-
-import { slugify } from "../../../utils/slugify";
 
 import styles from "./LargeImages.module.css";
 
@@ -19,34 +16,12 @@ export const LargeImages = ({
 
   const [firstImageUrlIsActive, setFirstImageUrlIsActive] = useState(true);
 
-  const location = useLocation();
-
-  const locationIsCollectionPage = location.pathname === "/collection";
-
   useEffect(() => {
     setFirstImageUrlIsActive(true);
   }, [jewelriesByCategory[0].color]);
 
   const toggleFirstImageUrlIsActive = () => {
     setFirstImageUrlIsActive((firstImageUrlIsActive) => !firstImageUrlIsActive);
-  };
-
-  const navigate = useNavigate();
-
-  const clickHandler = () => {
-    if (locationIsCollectionPage) {
-      const categoryTitle = jewelriesByCategory[0].categories[0].title;
-
-      const colorTitle = jewelriesByCategory[0].colors[0].title;
-
-      const slugifiedCategoryTitle = slugify(categoryTitle);
-
-      const slugifiedColorTitle = slugify(colorTitle);
-
-      navigate(`/collection/${slugifiedCategoryTitle}/${slugifiedColorTitle}`);
-    } else {
-      toggleFirstImageUrlIsActive();
-    }
   };
 
   return (
@@ -62,13 +37,12 @@ export const LargeImages = ({
         secondImageUrl={jewelriesByCategory[0].secondImageUrl}
         position={circleIconsPosition}
       />
-      <div onClick={clickHandler}>
-        <LargeImage
-          firstImageUrlIsActive={firstImageUrlIsActive}
-          jewelriesByCategory={jewelriesByCategory}
-          variant={variant}
-        />
-      </div>
+      <LargeImage
+        firstImageUrlIsActive={firstImageUrlIsActive}
+        jewelriesByCategory={jewelriesByCategory}
+        variant={variant}
+        toggleFirstImageUrlIsActive={toggleFirstImageUrlIsActive}
+      />
     </div>
   );
 };
