7cf0927b0e8490687f6a8e093493e7c5c27b8da6
Beatris Ilieve <beatrisilieve@icloud.com>
2024-08-31 18:56:40 +0300

    Added test

--- test/controllers/orderController.test.js
+++ test/controllers/orderController.test.js
@@ -61,8 +61,37 @@ describe("orderController", () => {
       size,
     });
 
-    const res3 = await request.get(`/orders/${userId}`);
+    const res3 = await request.get(`/orders/create/${userId}`);
 
     expect(res3.status).toBe(201);
+  });
+
+  test("Test get all orders; Expect success", async () => {
+    await request
+      .post("/users-login-details/register")
+      .send({ email, password });
+
+    const createdUserLoginDetails = await UserLoginDetails.findOne({
+      email,
+    });
+
+    const userId = createdUserLoginDetails._id;
+
+    await request.post(`/bags/add/${jewelryId}/${userId}`).send({
+      size,
+    });
+
+    const res3 = await request.get(`/orders/${userId}`);
+
+    expect(res3.status).toBe(200);
+
+    const responseBody = res3.body;
+    expect(responseBody).toBeInstanceOf(Array);
+
+    responseBody.forEach((item) => {
+      expect(item).toHaveProperty("jewelries");
+      expect(item).toHaveProperty("totalPrice");
+      expect(item).toHaveProperty("createdAt");
+      expect(item).toHaveProperty("status");
   });
 });
