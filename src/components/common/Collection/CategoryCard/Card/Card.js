aa7d114f5538a24267ecc07bd7816d418948849e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-02 16:02:24 +0300

    Updated CSS

--- src/components/common/Collection/CategoryCard/Card/Card.js
+++ src/components/common/Collection/CategoryCard/Card/Card.js
@@ -1,33 +1,3 @@
-// import { LoadingSpinner } from "../../../LoadingSpinner/LoadingSpinner";
-// import { Content } from "./Content/Content";
-
-// import styles from "./Card.module.css";
-
-// export const Card = ({
-//   jewelriesByCategory,
-//   isTransitioning,
-//   updateSelectedColor,
-// }) => {
-//   return (
-//     <div className={styles["outer-wrapper"]}>
-//       {isTransitioning ? (
-//         <LoadingSpinner isTransitioning={isTransitioning} />
-//       ) : (
-//         <div
-//           className={`${styles["spinner-wrapper"]} ${
-//             isTransitioning ? `${styles["transitioning"]}` : ""
-//           }`.trim()}
-//         >
-//           <Content
-//             jewelriesByCategory={jewelriesByCategory}
-//             updateSelectedColor={updateSelectedColor}
-//           />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
 import { Content } from "./Content/Content";
 
 import styles from "./Card.module.css";
