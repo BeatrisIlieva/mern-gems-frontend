674ccc21ad8dcd386077d1ff566198e1617a094c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-02 16:02:06 +0300

    Updated CSS

--- src/components/common/Collection/CategoryCard/CategoryCard.js
+++ src/components/common/Collection/CategoryCard/CategoryCard.js
@@ -7,19 +7,11 @@ import { useJewelry } from "../../../../hooks/useJewelry";
 export const CategoryCard = ({ categoryTitle, colorTitle }) => {
   const [selectedColor, setSelectedColor] = useState(colorTitle);
 
-  // const [isTransitioning, setIsTransitioning] = useState(false);
-
   const updateSelectedColor = (color) => {
     setSelectedColor(color);
-
-    // setIsTransitioning(true);
-
-    // setTimeout(() => {
-    //   setIsTransitioning(false);
-    // }, 2000);
   };
 
-  const { jewelriesByCategory,     isLoading } = useJewelry({
+  const { jewelriesByCategory, isLoading } = useJewelry({
     categoryTitle,
     colorTitle: selectedColor,
   });
