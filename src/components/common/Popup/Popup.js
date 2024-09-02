910585eed0d5b67846afaf56ffb282ab61a1728a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-02 11:44:39 +0300

    Updated Popup

--- src/components/common/Popup/Popup.js
+++ src/components/common/Popup/Popup.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
@@ -13,36 +11,12 @@ export const Popup = ({
 }) => {
   const showXMark = modalVariant !== "authentication";
 
-  // const [isOpen, setIsOpen] = useState(true);
-
-  // useEffect(() => {
-  //   const headerElement = document.getElementById("header");
-
-  //   if (isOpen) {
-  //     document.body.style.overflow = "hidden";
-  //     headerElement.style.zIndex = "0";
-  //   } else {
-  //     document.body.style.overflow = "visible";
-  //     headerElement.style.zIndex = "100";
-  //   }
-
-  //   return () => {
-  //     headerElement.style.zIndex = "100";
-  //     document.body.style.overflow = "visible";
-  //   };
-  // }, [isOpen]);
-
   const closeHandler = () => {
-    // setIsOpen(false);
     popupCloseHandler();
   };
 
   return (
-    <section
-    className={`${styles["overlay"]}  ${styles[overlayVariant]}`}
-      // className={`isOpen ? ${styles["overlay"]} ${styles["slide-in"]} ${styles[overlayVariant]} : ${styles["overlay"]} ${styles["slide-out"]} ${styles[overlayVariant]}`}
-      // onAnimationEnd={() => !isOpen && setIsOpen(false)}
-    >
+    <section className={`${styles["overlay"]}  ${styles[overlayVariant]}`}>
       <div className={`${styles["modal"]} ${styles[modalVariant]}`}>
         {showXMark && (
           <FontAwesomeIcon
