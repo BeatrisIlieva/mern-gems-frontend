d6e6a84756391edbba69f015562735aee7546e94
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-03 20:05:21 +0300

    Updated Responsive design

--- src/components/pages/Bag/NonEmptyBag/NonEmptyBag.js
+++ src/components/pages/Bag/NonEmptyBag/NonEmptyBag.js
@@ -1,4 +1,3 @@
-import { ShoppingProcessContainer } from "../../../reusable/ShoppingProcessContainer/ShoppingProcessContainer";
 import { OrderSummaryContent } from "./OrderSummaryContent/OrderSummaryContent";
 import { BagContent } from "./BagContent/BagContent";
 
@@ -7,37 +6,20 @@ import { useIsMobile } from "../../../../hooks/useIsMobile";
 import styles from "./NonEmptyBag.module.css";
 
 export const NonEmptyBag = () => {
-  const { isReversed } = useIsMobile();
-
   return (
     <section id={styles["non-empty-bag"]}>
-      {/* {isReversed ? (
-        <div className={styles["outer-wrapper"]}>
-          <div className={styles["order-summary-content"]}>
-            <div className={styles["inner-wrapper"]}>
-              <OrderSummaryContent />
-            </div>
-          </div>
-          <div className={styles["bag-content"]}>
-            <div className={styles["inner-wrapper"]}>
-              <BagContent />
-            </div>
+      <div className={styles["outer-wrapper"]}>
+        <div className={styles["bag-content"]}>
+          <div className={styles["inner-wrapper"]}>
+            <BagContent />
           </div>
         </div>
-      ) : ( */}
-        <div className={styles["outer-wrapper"]}>
-          <div className={styles["bag-content"]}>
-            <div className={styles["inner-wrapper"]}>
-              <BagContent />
-            </div>
-          </div>
-          <div className={styles["order-summary-content"]}>
-            <div className={styles["inner-wrapper"]}>
-              <OrderSummaryContent />
-            </div>
+        <div className={styles["order-summary-content"]}>
+          <div className={styles["inner-wrapper"]}>
+            <OrderSummaryContent />
           </div>
         </div>
-      {/* )} */}
+      </div>
     </section>
   );
 };
