c71c4aaa5062d1321a3c034b51f212b1034af0a7
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-04 14:10:37 +0300

    Added wishlist functionality

--- src/components/common/Heart/Heart.js
+++ src/components/common/Heart/Heart.js
@@ -1,19 +1,29 @@
 import { useWishlistContext } from "../../../contexts/WishlistContext";
+import { useAuthenticationContext } from "../../../contexts/AuthenticationContext";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Heart.module.css";
-import { useBagContext } from "../../../contexts/BagContext";
 
 export const Heart = ({ jewelryId }) => {
+  const { userId } = useAuthenticationContext();
+
   const { wishlistItems, add, remove } = useWishlistContext();
 
   return (
     <>
-      <FontAwesomeIcon icon={regularHeart} className={styles["heart-icon"]} />
-      <FontAwesomeIcon icon={solidHeart} className={styles["heart-icon"]} />
+      <FontAwesomeIcon
+        icon={regularHeart}
+        className={styles["heart-icon"]}
+        onClick={() => add(jewelryId, userId)}
+      />
+      <FontAwesomeIcon
+        icon={solidHeart}
+        className={styles["heart-icon"]}
+        onClick={() => remove(jewelryId, userId)}
+      />
     </>
   );
 };
