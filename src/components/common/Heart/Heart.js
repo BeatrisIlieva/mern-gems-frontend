04ed6c2c23257ad14a34652a06f7d1331bc643a3
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-04 14:04:10 +0300

    Added wishlist functionality

--- src/components/common/Heart/Heart.js
+++ src/components/common/Heart/Heart.js
@@ -1,9 +1,16 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Heart.module.css";
 
 export const Heart = () => {
-  return <FontAwesomeIcon icon={faHeart} className={styles["heart-icon"]} />;
+  return (
+    <>
+      <FontAwesomeIcon icon={regularHeart} className={styles["heart-icon"]} />
+      <FontAwesomeIcon icon={solidHeart} className={styles["heart-icon"]} />
+    </>
+  );
 };
