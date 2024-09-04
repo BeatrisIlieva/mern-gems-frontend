9f4c7007a816d91e388b8fb8e0c49ed7262d6af1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-04 12:31:10 +0300

    Updated responsive design

--- src/components/pages/Bag/Bag.js
+++ src/components/pages/Bag/Bag.js
@@ -4,7 +4,7 @@ import { EmptyBag } from "./EmptyBag/EmptyBag";
 import { useBagContext } from "../../../contexts/BagContext";
 
 export const Bag = () => {
-  const { bagTotalQuantity } = useBagContext();
+  const { bagTotalQuantity, totalPrice} = useBagContext();
 
-  return <>{bagTotalQuantity < 1 ? <EmptyBag /> : <NonEmptyBag />}</>;
+  return <>{(bagTotalQuantity < 1 && totalPrice < 1) ? <EmptyBag /> : <NonEmptyBag />}</>;
 };
