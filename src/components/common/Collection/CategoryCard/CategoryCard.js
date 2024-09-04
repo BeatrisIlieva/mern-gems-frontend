878f7265fe45d948d789b7a194cbc7344aa83851
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-04 20:27:42 +0300

    Updated collection list

--- src/components/common/Collection/CategoryCard/CategoryCard.js
+++ src/components/common/Collection/CategoryCard/CategoryCard.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Card } from "./Card/Card";
+import { Button } from "../../../reusable/Button/Button";
 
 import { useJewelry } from "../../../../hooks/useJewelry";
 
+import { slugify } from "../../../../utils/slugify";
+
+import styles from "./CategoryCard.module.css";
+
 export const CategoryCard = ({ categoryTitle, colorTitle }) => {
   const [selectedColor, setSelectedColor] = useState(colorTitle);
+
+  const navigate = useNavigate();
 
   const updateSelectedColor = (color) => {
     setSelectedColor(color);
@@ -16,10 +24,25 @@ export const CategoryCard = ({ categoryTitle, colorTitle }) => {
     colorTitle: selectedColor,
   });
 
+  const buttonClickHandler = () => {
+    const slugifiedCategoryTitle = slugify(categoryTitle);
+
+    const slugifiedColorTitle = selectedColor;
+
+    navigate(`/collection/${slugifiedCategoryTitle}/${slugifiedColorTitle}`);
+  };
+
   return (
-    <Card
-      jewelriesByCategory={jewelriesByCategory}
-      updateSelectedColor={updateSelectedColor}
-    />
+    <section className={styles["category-card"]}>
+      <Button
+        title={`Shop ${categoryTitle}`}
+        variant={"underlined"}
+        callBackFunction={buttonClickHandler}
+      />
+      <Card
+        jewelriesByCategory={jewelriesByCategory}
+        updateSelectedColor={updateSelectedColor}
+      />
+    </section>
   );
 };
