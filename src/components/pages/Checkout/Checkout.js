40e21f950466ddf811d3f09f63396f25d7734d93
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-04 18:27:38 +0300

    Updated responsive design

--- src/components/pages/Checkout/Checkout.js
+++ src/components/pages/Checkout/Checkout.js
@@ -1,13 +1,9 @@
-
-import { useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import { ShoppingProcessContainer } from "../../reusable/ShoppingProcessContainer/ShoppingProcessContainer";
 import { OrderSummary } from "../../common/OrderSummary/OrderSummary";
 import { BagList } from "../../common/BagList/BagList";
 import { ChildWrapper } from "../../reusable/ChildWrapper/ChildWrapper";
-
-
-
 import { ShippingContent } from "./ShippingContent/ShippingContent";
 import { CardDetailsContent } from "./CardDetailsContent/CardDetailsContent";
 
@@ -15,8 +11,6 @@ export const Checkout = () => {
   const location = useLocation();
 
   const locationIsCheckout = location.pathname === "/checkout";
-
-
 
   return (
     <>
