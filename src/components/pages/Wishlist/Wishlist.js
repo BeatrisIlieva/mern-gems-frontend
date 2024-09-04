f87a64f11c0946a4d7306e6a5f8820f9fca8012a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-04 16:56:59 +0300

    Added wishlist functionality

--- src/components/pages/Wishlist/Wishlist.js
+++ src/components/pages/Wishlist/Wishlist.js
@@ -2,29 +2,30 @@ import { CategoryCard } from "../../common/Collection/CategoryCard/CategoryCard"
 
 import { useWishlistContext } from "../../../contexts/WishlistContext";
 
-import styles from "./Wishlist.module.css"
+import styles from "./Wishlist.module.css";
 
 export const Wishlist = () => {
   const { wishlistItems } = useWishlistContext();
 
   const itemsArray = wishlistItems?.result;
 
-    if (!Array.isArray(itemsArray)) {
-      // If itemsArray is not an array (e.g., undefined or null), return an empty state or loading indicator
-      return <div>Loading...</div>;
-    }
+  const displayContent = Array.isArray(itemsArray);
 
   return (
-    <section id={styles["wishlist"]}>
-      <div className={styles["wrapper"]}>
-        {itemsArray.map((item) => (
-          <CategoryCard
-            key={item._id}
-            categoryTitle={item.category.title}
-            colorTitle={item.color.title}
-          />
-        ))}
-      </div>
-    </section>
+    <>
+      {displayContent && (
+        <section id={styles["wishlist"]}>
+          <div className={styles["wrapper"]}>
+            {itemsArray.map((item) => (
+              <CategoryCard
+                key={item._id}
+                categoryTitle={item.category.title}
+                colorTitle={item.color.title}
+              />
+            ))}
+          </div>
+        </section>
+      )}
+    </>
   );
 };
