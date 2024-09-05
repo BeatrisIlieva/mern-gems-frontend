ea81c0610e5d902d44a5c6f9a1f2d96b2887caca
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-05 14:44:34 +0300

    Updated jewelry functionality

--- src/components/pages/CollectionList/CollectionList.js
+++ src/components/pages/CollectionList/CollectionList.js
@@ -7,19 +7,15 @@ import styles from "./CollectionList.module.css";
 export const CollectionList = () => {
   return (
     <section id={styles["collection-list"]}>
-      {/* <div className={styles["outer-wrapper"]}> */}
-        {/* <div className={styles["wrapper"]}> */}
-          {Object.entries(CATEGORIES_BY_ID_AND_INITIAL_COLOR_ID).map(
-            ([categoryTitle, colorTitle]) => (
-              <CategoryCard
-                key={categoryTitle}
-                categoryTitle={categoryTitle}
-                colorTitle={colorTitle}
-              />
-            )
-          )}
-        {/* </div> */}
-      {/* </div> */}
+      {Object.entries(CATEGORIES_BY_ID_AND_INITIAL_COLOR_ID).map(
+        ([categoryTitle, colorTitle]) => (
+          <CategoryCard
+            key={categoryTitle}
+            categoryTitle={categoryTitle}
+            colorTitle={colorTitle}
+          />
+        )
+      )}
     </section>
   );
 };
