30d5ebc90bc695873ec84c501a205fa338605ec9
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-05 15:17:26 +0300

    Updated jewelry functionality

--- src/components/pages/CollectionList/CategoryCard/CategoryCard.js
+++ src/components/pages/CollectionList/CategoryCard/CategoryCard.js
@@ -4,6 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { Content } from "./Content/Content";
 import { ShopBy } from "../../../common/ShopBy/ShopBy";
 
+import { DualTitleSection } from "../../../reusable/DualTitleSection/DualTitleSection";
+import { LargeImages } from "./LargeImages/LargeImages";
+import { MiniImages } from "../../../common/MiniImages/MiniImages";
+import { StockStatus } from "../../../common/StockStatus/StockStatus";
+import { PriceRange } from "../../../common/PriceRange/PriceRange";
+
 import { useJewelry } from "../../../../hooks/useJewelry";
 
 import { slugify } from "../../../../utils/slugify";
@@ -12,6 +18,8 @@ import styles from "./CategoryCard.module.css";
 
 export const CategoryCard = ({ categoryTitle, colorTitle }) => {
   const [selectedColor, setSelectedColor] = useState(colorTitle);
+
+  const [articleIsHovered, setArticleIsHovered] = useState(false);
 
   const navigate = useNavigate();
 
@@ -33,15 +41,49 @@ export const CategoryCard = ({ categoryTitle, colorTitle }) => {
   };
 
   return (
-    <section className={styles["category-card"]}>
-      <ShopBy
-        categoryTitle={categoryTitle}
-        buttonClickHandler={buttonClickHandler}
-      />
-      <Content
-        jewelriesByCategory={jewelriesByCategory}
-        updateSelectedColor={updateSelectedColor}
-      />
-    </section>
+    <>
+      {jewelriesByCategory.length > 0 && (
+        <section className={styles["category-card"]}>
+          <ShopBy
+            categoryTitle={categoryTitle}
+            buttonClickHandler={buttonClickHandler}
+          />
+          {/* <div
+            onMouseEnter={() => setArticleIsHovered(true)}
+            onMouseLeave={() => setArticleIsHovered(false)}
+            onTouchStart={() => setArticleIsHovered(true)}
+            onTouchEnd={() => setArticleIsHovered(false)}
+            className={
+              articleIsHovered
+                ? `${styles["content"]} ${styles["hovered"]}`
+                : styles["content"]
+            }
+          >
+            <DualTitleSection
+              firstTitle={
+                <PriceRange jewelriesByCategory={jewelriesByCategory} />
+              }
+              secondTitle={
+                <StockStatus jewelriesByCategory={jewelriesByCategory} />
+              }
+              variant={"regular"}
+            />
+            <LargeImages
+              jewelriesByCategory={jewelriesByCategory}
+              updateSelectedColor={updateSelectedColor}
+              circleIconsPosition={"top"}
+            />
+            <MiniImages
+              jewelriesByCategory={jewelriesByCategory}
+              clickHandler={updateSelectedColor}
+            />
+          </div> */}
+          <Content
+            jewelriesByCategory={jewelriesByCategory}
+            updateSelectedColor={updateSelectedColor}
+          />
+        </section>
+      )}
+    </>
   );
 };
