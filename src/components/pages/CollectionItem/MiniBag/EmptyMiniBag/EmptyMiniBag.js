6467645a5368031254f97694acf842bbf368324d
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-05 19:45:36 +0300

    Updated jewelry functionality

--- src/components/pages/CollectionItem/MiniBag/EmptyMiniBag/EmptyMiniBag.js
+++ src/components/pages/CollectionItem/MiniBag/EmptyMiniBag/EmptyMiniBag.js
@@ -1,9 +1,11 @@
 import { InfoMessage } from "../../../../reusable/InfoMessage/InfoMessage";
+import { CardSlider } from "../../../../common/CardSlider/CardSlider";
 
 export const EmptyMiniBag = () => {
   return (
     <>
       <InfoMessage title={"Your Shopping Bag Is Empty"} />
+      <CardSlider/>
     </>
   );
 };
