e02c7f6a92043605f189e09f87f3bbf0a1910048
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-07 15:08:17 +0300

    Added optimization

--- src/contexts/WishlistContext.js
+++ src/contexts/WishlistContext.js
@@ -1,82 +1,3 @@
-// import {
-//   useState,
-//   useEffect,
-//   createContext,
-//   useContext,
-//   useCallback,
-//   useMemo,
-// } from "react";
-
-// import { useAuthenticationContext } from "../contexts/AuthenticationContext";
-
-// import { useService } from "../hooks/useService";
-
-// import { wishlistServiceFactory } from "../services/wishlistService";
-
-// export const WishlistContext = createContext();
-
-// export const WishlistProvider = ({ children }) => {
-//   const { userId } = useAuthenticationContext();
-
-//   const [wishlistItems, setWishlistItems] = useState([]);
-
-//   const wishlistService = useService(wishlistServiceFactory);
-
-//   useEffect(() => {
-//     wishlistService
-//       .getAll(userId)
-//       .then((data) => {
-//         setWishlistItems(data.result);
-//       })
-//       .catch((err) => {
-//         console.log(err.message);
-//       });
-//   }, [userId, wishlistService]);
-
-//   const wishlistTotalQuantity = useMemo(() => {
-//     return wishlistItems.length;
-//   }, [wishlistItems.length]);
-
-//   const add = useCallback(
-//     async (categoryId, colorId, userId) => {
-//       await wishlistService.add(categoryId, colorId, userId);
-//     },
-//     [wishlistService]
-//   );
-
-//   const remove = useCallback(
-//     async (categoryId, colorId, userId) => {
-//       await wishlistService.delete(categoryId, colorId, userId);
-//     },
-//     [wishlistService]
-//   );
-
-//   // const context = {
-//   //   wishlistItems,
-//   //   wishlistTotalQuantity,
-//   //   add,
-//   //   remove,
-//   // };
-
-//   const context = useMemo(
-//     () => ({ wishlistItems, wishlistTotalQuantity, add, remove }),
-//     [wishlistItems, wishlistTotalQuantity, add, remove]
-//   );
-
-//   return (
-//     <WishlistContext.Provider value={context}>
-//       {children}
-//     </WishlistContext.Provider>
-//   );
-// };
-
-// export const useWishlistContext = () => {
-//   const context = useContext(WishlistContext);
-
-//   return context;
-// };
-
-
 import {
   useState,
   useEffect,
@@ -87,53 +8,51 @@ import {
 } from "react";
 
 import { useAuthenticationContext } from "../contexts/AuthenticationContext";
+
 import { useService } from "../hooks/useService";
+
 import { wishlistServiceFactory } from "../services/wishlistService";
 
 export const WishlistContext = createContext();
 
 export const WishlistProvider = ({ children }) => {
   const { userId } = useAuthenticationContext();
-  const wishlistService = useService(wishlistServiceFactory);
 
   const [wishlistItems, setWishlistItems] = useState([]);
 
-  useEffect(() => {
-    const fetchWishlistItems = async () => {
-      try {
-        const data = await wishlistService.getAll(userId);
-        setWishlistItems(data.result);
-      } catch (err) {
-        console.log(err.message);
-      }
-    };
+  const wishlistService = useService(wishlistServiceFactory);
 
-    fetchWishlistItems();
+  useEffect(() => {
+    wishlistService
+      .getAll(userId)
+      .then((data) => {
+        setWishlistItems(data.result);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, [userId, wishlistService]);
 
-  const wishlistTotalQuantity = useMemo(() => wishlistItems.length, [wishlistItems]);
+  const wishlistTotalQuantity = useMemo(() => {
+    return wishlistItems.length;
+  }, [wishlistItems.length]);
 
   const add = useCallback(
-    async (categoryId, colorId) => {
+    async (categoryId, colorId, userId) => {
       await wishlistService.add(categoryId, colorId, userId);
     },
-    [wishlistService, userId]
+    [wishlistService]
   );
 
   const remove = useCallback(
-    async (categoryId, colorId) => {
+    async (categoryId, colorId, userId) => {
       await wishlistService.delete(categoryId, colorId, userId);
     },
-    [wishlistService, userId]
+    [wishlistService]
   );
 
   const context = useMemo(
-    () => ({
-      wishlistItems,
-      wishlistTotalQuantity,
-      add,
-      remove
-    }),
+    () => ({ wishlistItems, wishlistTotalQuantity, add, remove }),
     [wishlistItems, wishlistTotalQuantity, add, remove]
   );
 
@@ -146,8 +65,6 @@ export const WishlistProvider = ({ children }) => {
 
 export const useWishlistContext = () => {
   const context = useContext(WishlistContext);
-  if (!context) {
-    throw new Error("useWishlistContext must be used within a WishlistProvider");
-  }
+
   return context;
 };
