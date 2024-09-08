8b48129d4a8d1901a1a787ce14449f8137d3bda9
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 14:26:47 +0300

    Updated export

--- src/components/common/MiniImages/Image/Image.js
+++ src/components/common/MiniImages/Image/Image.js
@@ -4,34 +4,28 @@ import { COLORS_BY_TITLE } from "../../../../constants/colorsByTitle";
 
 import styles from "./Image.module.css";
 
-const Image = ({
-  imageUrl,
-  title,
-  colorName,
-  updateActiveMiniImage,
-  isActive,
-}) => {
-  const color = COLORS_BY_TITLE[colorName];
+export const Image = memo(
+  ({ imageUrl, title, colorName, updateActiveMiniImage, isActive }) => {
+    const color = COLORS_BY_TITLE[colorName];
 
-  const clickHandler = () => {
-    updateActiveMiniImage(colorName);
-  };
+    const clickHandler = () => {
+      updateActiveMiniImage(colorName);
+    };
 
-  return (
-    <div className={styles["image-object"]}>
-      <div className={styles["thumbnail"]}>
-        <img
-          className={styles["image"]}
-          src={imageUrl}
-          alt={title}
-          onClick={clickHandler}
-        />
+    return (
+      <div className={styles["image-object"]}>
+        <div className={styles["thumbnail"]}>
+          <img
+            className={styles["image"]}
+            src={imageUrl}
+            alt={title}
+            onClick={clickHandler}
+          />
+        </div>
+        {isActive && (
+          <span className={`${styles["title"]} ${styles[color]}`}>{title}</span>
+        )}
       </div>
-      {isActive && (
-        <span className={`${styles["title"]} ${styles[color]}`}>{title}</span>
-      )}
-    </div>
-  );
-};
-
-export default memo(Image);
+    );
+  }
+);
