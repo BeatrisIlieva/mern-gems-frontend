03a04f8ece6ecdc71b8ddce8443a3e804aacfd31
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 20:05:32 +0300

    Added tests

--- src/components/pages/Bag/EmptyBag/EmptyBag.test.js
+++ src/components/pages/Bag/EmptyBag/EmptyBag.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { EmptyBag } from "./EmptyBag";
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
 
-// Mock the child components
+import { EmptyBag } from "./EmptyBag";
+
 jest.mock("../../../reusable/InfoMessage/InfoMessage", () => ({
   InfoMessage: ({ title, subtitle }) => (
     <div>
@@ -20,11 +20,11 @@ describe("EmptyBag Component", () => {
   test("renders InfoMessage and CardSlider", () => {
     render(<EmptyBag />);
 
-    // Check if InfoMessage with the correct title and subtitle is rendered
     expect(screen.getByText("Your Shopping Bag Is Empty.")).toBeInTheDocument();
-    expect(screen.getByText("Explore and add something you love.")).toBeInTheDocument();
-    
-    // Check if CardSlider is rendered
+    expect(
+      screen.getByText("Explore and add something you love.")
+    ).toBeInTheDocument();
+
     expect(screen.getByText("CardSlider")).toBeInTheDocument();
   });
 });
