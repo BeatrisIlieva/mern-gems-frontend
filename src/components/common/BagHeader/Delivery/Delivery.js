5b829e4778b7fdf4de13acc5aed060b86e982477
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 11:32:51 +0300

    Added optimization

--- src/components/common/BagHeader/Delivery/Delivery.js
+++ src/components/common/BagHeader/Delivery/Delivery.js
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 import { LargeTitle } from "../../../reusable/LargeTitle/LargeTitle";
 
 import { faTruck } from "@fortawesome/free-solid-svg-icons";
@@ -5,11 +7,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./Delivery.module.css";
 
-export const Delivery = () => {
+export const Delivery = memo(() => {
   return (
     <div className={styles["delivery"]}>
       <FontAwesomeIcon icon={faTruck} className={styles["icon"]} />
       <LargeTitle title={"Delivery"} variant={"italic"} />
     </div>
   );
-};
+});
