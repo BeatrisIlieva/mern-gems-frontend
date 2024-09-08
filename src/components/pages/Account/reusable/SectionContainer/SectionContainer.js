1614260af0a91a2963a50b948fff3e69dbc0befe
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 14:28:06 +0300

    Updated export

--- src/components/pages/Account/reusable/SectionContainer/SectionContainer.js
+++ src/components/pages/Account/reusable/SectionContainer/SectionContainer.js
@@ -4,21 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./SectionContainer.module.css";
 
-const SectionContainer = ({
-  sectionTitle,
-  callBackFunction,
-  icon,
-  buttonTitle,
-}) => {
-  return (
-    <section className={styles["section-container"]}>
-      <h2 className={styles["title"]}>{sectionTitle}</h2>
-      <button onClick={callBackFunction} className={styles["button"]}>
-        <FontAwesomeIcon icon={icon} className={styles["icon"]} />
-        {buttonTitle}
-      </button>
-    </section>
-  );
-};
-
-export default memo(SectionContainer);
+export const SectionContainer = memo(
+  ({ sectionTitle, callBackFunction, icon, buttonTitle }) => {
+    return (
+      <section className={styles["section-container"]}>
+        <h2 className={styles["title"]}>{sectionTitle}</h2>
+        <button onClick={callBackFunction} className={styles["button"]}>
+          <FontAwesomeIcon icon={icon} className={styles["icon"]} />
+          {buttonTitle}
+        </button>
+      </section>
+    );
+  }
+);
