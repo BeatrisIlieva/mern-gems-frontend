a8b5bfd95d4eb906d873715da575c901dd826680
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 19:59:34 +0300

    Added tests

--- src/components/pages/Wishlist/NonEmptyWishList/NonEmptyWishlist.test.js
+++ src/components/pages/Wishlist/NonEmptyWishList/NonEmptyWishlist.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+
 import { NonEmptyWishlist } from "./NonEmptyWishlist";
+
 import { useWishlistContext } from "../../../../contexts/WishlistContext";
 
-// Mock the child components
 jest.mock("../../../reusable/InfoMessage/InfoMessage", () => ({
   InfoMessage: ({ title, subtitle }) => (
     <div>
@@ -21,17 +22,19 @@ jest.mock("./Content/Content", () => ({
   ),
 }));
 
-// Mock the useWishlistContext hook
 jest.mock("../../../../contexts/WishlistContext", () => ({
   useWishlistContext: jest.fn(),
 }));
 
 describe("NonEmptyWishlist Component", () => {
   test("renders InfoMessage and Content components when wishlist has items", () => {
-    // Mock data for wishlist context
     const mockWishlistItems = [
       { _id: "1", category: { title: "Jewelry" }, color: { title: "Red" } },
-      { _id: "2", category: { title: "Accessories" }, color: { title: "Blue" } },
+      {
+        _id: "2",
+        category: { title: "Accessories" },
+        color: { title: "Blue" },
+      },
     ];
     const mockWishlistTotalQuantity = mockWishlistItems.length;
 
@@ -42,11 +45,15 @@ describe("NonEmptyWishlist Component", () => {
 
     render(<NonEmptyWishlist />);
 
-    // Check InfoMessage rendering
-    expect(screen.getByText(`Your Wish List (${mockWishlistTotalQuantity})`)).toBeInTheDocument();
-    expect(screen.getByText("Your favorite item(s) are below. Wishes can come true, especially when you dream.")).toBeInTheDocument();
+    expect(
+      screen.getByText(`Your Wish List (${mockWishlistTotalQuantity})`)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Your favorite item(s) are below. Wishes can come true, especially when you dream."
+      )
+    ).toBeInTheDocument();
 
-    // Check Content components rendering
     mockWishlistItems.forEach((item) => {
       expect(screen.getByText(item.category.title)).toBeInTheDocument();
       expect(screen.getByText(item.color.title)).toBeInTheDocument();
@@ -61,9 +68,12 @@ describe("NonEmptyWishlist Component", () => {
 
     render(<NonEmptyWishlist />);
 
-    // Check that InfoMessage and Content components are not rendered
     expect(screen.queryByText("Your Wish List")).not.toBeInTheDocument();
-    expect(screen.queryByText("This list is empty. Explore and add something you love.")).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(
+        "This list is empty. Explore and add something you love."
+      )
+    ).not.toBeInTheDocument();
     expect(screen.queryByText("Jewelry")).not.toBeInTheDocument();
     expect(screen.queryByText("Red")).not.toBeInTheDocument();
   });
