f3050d317626ce113245d526c85df1ed16e2c3e6
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 20:28:39 +0300

    Added tests

---
+++ src/components/pages/Checkout/CardDetailsContent/ShippingInformation/ShippingInformation.test.js
@@ -0,0 +1,54 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import { ShippingInformation } from "./ShippingInformation";
+
+jest.mock("../../../../reusable/DualTitleSection/DualTitleSection", () => ({
+  DualTitleSection: ({ firstTitle, secondTitle }) => (
+    <div>
+      <div>{firstTitle}</div>
+      <div>{secondTitle}</div>
+    </div>
+  ),
+}));
+
+jest.mock("../../../../reusable/LargeTitle/LargeTitle", () => ({
+  LargeTitle: ({ title }) => <h1>{title}</h1>,
+}));
+
+jest.mock("../../../../reusable/Button/Button", () => ({
+  Button: ({ title, callBackFunction }) => (
+    <button onClick={callBackFunction}>{title}</button>
+  ),
+}));
+
+jest.mock("./UserLoginDetails/UserLoginDetails", () => ({
+  UserLoginDetails: () => <div>User Login Details</div>,
+}));
+
+jest.mock("./UserShippingDetails/UserShippingDetails", () => ({
+  UserShippingDetails: () => <div>User Shipping Details</div>,
+}));
+
+describe("ShippingInformation Component", () => {
+  test("renders components correctly and handles button click", () => {
+    const toggleDisplayShippingDetailsPopup = jest.fn();
+
+    render(
+      <ShippingInformation
+        toggleDisplayShippingDetailsPopup={toggleDisplayShippingDetailsPopup}
+      />
+    );
+
+    expect(screen.getByText("Shipping Information")).toBeInTheDocument();
+    expect(screen.getByText("User Login Details")).toBeInTheDocument();
+    expect(screen.getByText("User Shipping Details")).toBeInTheDocument();
+
+    const editButton = screen.getByText("Edit");
+    expect(editButton).toBeInTheDocument();
+
+    fireEvent.click(editButton);
+
+    expect(toggleDisplayShippingDetailsPopup).toHaveBeenCalled();
+  });
+});
