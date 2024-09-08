9423890b1bc0fc4fcadf5f856dfcc27621e9a697
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 14:30:38 +0300

    Updated export

--- src/components/reusable/DynamicForm/FieldBox/FieldContainer/FieldContainer.js
+++ src/components/reusable/DynamicForm/FieldBox/FieldContainer/FieldContainer.js
@@ -5,39 +5,39 @@ import Label from "./Label/Label";
 
 import styles from "./FieldContainer.module.css";
 
-const FieldContainer = ({
-  clickHandler,
-  blurHandler,
-  changeHandler,
-  userInformation,
-  initialFormValues,
-  values,
-  value,
-  currentKey,
-}) => {
-  return (
-    <div
-      className={`${styles["field-container"]} ${
-        values[value].errorMessage !== "" ? styles["error"] : ""
-      }`.trim()}
-      onClick={() => clickHandler(value)}
-      onBlur={() => blurHandler(value)}
-    >
-      <Input
-        changeHandler={changeHandler}
-        clickHandler={clickHandler}
-        userInformation={userInformation}
-        values={values}
-        value={value}
-        currentKey={currentKey}
-      />
-      <Label
-        initialFormValues={initialFormValues}
-        values={values}
-        value={value}
-      />
-    </div>
-  );
-};
-
-export default memo(FieldContainer);
+export const FieldContainer = memo(
+  ({
+    clickHandler,
+    blurHandler,
+    changeHandler,
+    userInformation,
+    initialFormValues,
+    values,
+    value,
+    currentKey,
+  }) => {
+    return (
+      <div
+        className={`${styles["field-container"]} ${
+          values[value].errorMessage !== "" ? styles["error"] : ""
+        }`.trim()}
+        onClick={() => clickHandler(value)}
+        onBlur={() => blurHandler(value)}
+      >
+        <Input
+          changeHandler={changeHandler}
+          clickHandler={clickHandler}
+          userInformation={userInformation}
+          values={values}
+          value={value}
+          currentKey={currentKey}
+        />
+        <Label
+          initialFormValues={initialFormValues}
+          values={values}
+          value={value}
+        />
+      </div>
+    );
+  }
+);
