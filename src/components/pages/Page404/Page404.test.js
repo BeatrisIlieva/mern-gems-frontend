05cc348a372893bd02be1583fa32148a64db3fc9
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 20:45:12 +0300

    Added tests

---
+++ src/components/pages/Page404/Page404.test.js
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react";
+
+import { Page404 } from "./Page404";
+
+import { InfoMessage } from "../../reusable/InfoMessage/InfoMessage";
+
+import { CardSlider } from "../../common/CardSlider/CardSlider";
+
+jest.mock("../../reusable/InfoMessage/InfoMessage");
+jest.mock("../../common/CardSlider/CardSlider");
+
+describe("Page404 Component", () => {
+  beforeEach(() => {
+    InfoMessage.mockReturnValue(<div data-testid="info-message" />);
+    CardSlider.mockReturnValue(<div data-testid="card-slider" />);
+  });
+
+  test("renders Page404 component correctly", () => {
+    render(<Page404 />);
+
+    const infoMessage = screen.getByTestId("info-message");
+    expect(infoMessage).toBeInTheDocument();
+
+    const cardSlider = screen.getByTestId("card-slider");
+    expect(cardSlider).toBeInTheDocument();
+  });
+
+  test("renders InfoMessage with correct title and subtitle", () => {
+    render(<Page404 />);
+
+    expect(InfoMessage).toHaveBeenCalledWith(
+      {
+        title: "Sorry, we can’t locate that page.",
+        subtitle: "You can continue shopping by exploring the collection.",
+      },
+      {}
+    );
+  });
+
+  test("renders CardSlider component", () => {
+    render(<Page404 />);
+
+    expect(CardSlider).toHaveBeenCalled();
+  });
+});
