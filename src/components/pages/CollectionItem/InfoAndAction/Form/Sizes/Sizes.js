b6395f385fd1d2258e0e6aa4d73ba984a286b46a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 14:29:21 +0300

    Updated export

--- src/components/pages/CollectionItem/InfoAndAction/Form/Sizes/Sizes.js
+++ src/components/pages/CollectionItem/InfoAndAction/Form/Sizes/Sizes.js
@@ -7,50 +7,50 @@ import { SIZE_FORM_KEY } from "../../../../../../constants/sizeFormKey";
 
 import styles from "./Sizes.module.css";
 
-const Sizes = ({ inventories, errorMessage, changeHandler, selectedSize }) => {
-  const [hoveredLabel, setHoveredLabel] = useState("");
+export const Sizes = memo(
+  ({ inventories, errorMessage, changeHandler, selectedSize }) => {
+    const [hoveredLabel, setHoveredLabel] = useState("");
 
-  const location = useLocation();
+    const location = useLocation();
 
-  useEffect(() => {
-    setHoveredLabel("");
-  }, [location.pathname, inventories]);
+    useEffect(() => {
+      setHoveredLabel("");
+    }, [location.pathname, inventories]);
 
-  return (
-    <div className={styles["size-wrapper"]}>
-      <NormalTitle title={"Size"} variant={"bolded"} />
-      <div className={styles["radio-container"]}>
-        {inventories.map((item) => (
-          <div key={item.size} className={styles["wrapper"]}>
-            <input
-              type="radio"
-              name={SIZE_FORM_KEY.Size}
-              id={item.size}
-              value={item.size}
-              onChange={changeHandler}
-              onClick={changeHandler}
-              checked={item.size === selectedSize}
-              disabled={item.quantity === 0}
-            />
-            <label
-              onMouseEnter={() => setHoveredLabel(item.size)}
-              onMouseLeave={() => setHoveredLabel(item.size)}
-              onTouchStart={() => setHoveredLabel(item.size)}
-              onTouchEnd={() => setHoveredLabel(item.size)}
-              className={`${styles["label"]} ${
-                hoveredLabel === item.size ? styles["hovered"] : ""
-              }`.trim()}
-              htmlFor={item.size}
-            >
-              {item.size}
-            </label>
-            <NormalTitle title={`$${item.price}`} variant={"bolded"} />
-          </div>
-        ))}
+    return (
+      <div className={styles["size-wrapper"]}>
+        <NormalTitle title={"Size"} variant={"bolded"} />
+        <div className={styles["radio-container"]}>
+          {inventories.map((item) => (
+            <div key={item.size} className={styles["wrapper"]}>
+              <input
+                type="radio"
+                name={SIZE_FORM_KEY.Size}
+                id={item.size}
+                value={item.size}
+                onChange={changeHandler}
+                onClick={changeHandler}
+                checked={item.size === selectedSize}
+                disabled={item.quantity === 0}
+              />
+              <label
+                onMouseEnter={() => setHoveredLabel(item.size)}
+                onMouseLeave={() => setHoveredLabel(item.size)}
+                onTouchStart={() => setHoveredLabel(item.size)}
+                onTouchEnd={() => setHoveredLabel(item.size)}
+                className={`${styles["label"]} ${
+                  hoveredLabel === item.size ? styles["hovered"] : ""
+                }`.trim()}
+                htmlFor={item.size}
+              >
+                {item.size}
+              </label>
+              <NormalTitle title={`$${item.price}`} variant={"bolded"} />
+            </div>
+          ))}
+        </div>
+        <div className={styles["error-message"]}>{errorMessage}</div>
       </div>
-      <div className={styles["error-message"]}>{errorMessage}</div>
-    </div>
-  );
-};
-
-export default memo(Sizes);
+    );
+  }
+);
