371d81f6c60fb433b7c0e99411a7989b3fe2e69b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 14:30:55 +0300

    Updated export

--- src/components/reusable/DynamicForm/FieldBox/FieldContainer/Input/Input.js
+++ src/components/reusable/DynamicForm/FieldBox/FieldContainer/Input/Input.js
@@ -1,29 +1,29 @@
 import { memo } from "react";
 
-const Input = ({
-  changeHandler,
-  clickHandler,
-  userInformation,
-  values,
-  value,
-  currentKey,
-}) => {
-  return (
-    <input
-      type={values[value].fieldType}
-      name={value}
-      id={value}
-      defaultValue={
-        currentKey !== "Password"
-          ? userInformation
-            ? userInformation[value]
-            : values[currentKey]
-          : ""
-      }
-      onChange={(e) => changeHandler(value, e.target.value)}
-      onFocus={() => clickHandler(value)}
-    />
-  );
-};
-
-export default memo(Input);
+export const Input = memo(
+  ({
+    changeHandler,
+    clickHandler,
+    userInformation,
+    values,
+    value,
+    currentKey,
+  }) => {
+    return (
+      <input
+        type={values[value].fieldType}
+        name={value}
+        id={value}
+        defaultValue={
+          currentKey !== "Password"
+            ? userInformation
+              ? userInformation[value]
+              : values[currentKey]
+            : ""
+        }
+        onChange={(e) => changeHandler(value, e.target.value)}
+        onFocus={() => clickHandler(value)}
+      />
+    );
+  }
+);
