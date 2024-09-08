c97f4d7c3540d42393f98d57e124f83ff5e79ae1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 11:32:28 +0300

    Added optimization

--- src/components/common/BagHeader/BagCount/BagCount.js
+++ src/components/common/BagHeader/BagCount/BagCount.js
@@ -1,15 +1,17 @@
+import { memo } from "react";
+
 import { useBagContext } from "../../../../contexts/BagContext";
 
 import styles from "./BagCount.module.css";
 
-export const BagCount = () => {
+export const BagCount = memo(() => {
   const { bagTotalQuantity } = useBagContext();
 
   return (
-      <h4 className={styles["title"]}>
-        {bagTotalQuantity > 1
-          ? `(${bagTotalQuantity} items)`
-          : `(${bagTotalQuantity} item)`}
-      </h4>
+    <h4 className={styles["title"]}>
+      {bagTotalQuantity > 1
+        ? `(${bagTotalQuantity} items)`
+        : `(${bagTotalQuantity} item)`}
+    </h4>
   );
-};
+});
