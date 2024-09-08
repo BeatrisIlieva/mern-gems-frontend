744925ad3094ea26d6215f9423ecb36721dc9dd5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-08 11:32:05 +0300

    Added optimization

--- src/components/common/BagList/UpdateQuantity/UpdateQuantity.js
+++ src/components/common/BagList/UpdateQuantity/UpdateQuantity.js
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 import { useBagContext } from "../../../../contexts/BagContext";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,26 +8,28 @@ import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./UpdateQuantity.module.css";
 
-export const UpdateQuantity = ({ bagId, bagQuantity, inventoryQuantity }) => {
-  const { increase, decrease } = useBagContext();
+export const UpdateQuantity = memo(
+  ({ bagId, bagQuantity, inventoryQuantity }) => {
+    const { increase, decrease } = useBagContext();
 
-  return (
-    <div className={styles["update-quantity"]}>
-      <button disabled={inventoryQuantity < 1} className={styles["button"]}>
+    return (
+      <div className={styles["update-quantity"]}>
+        <button disabled={inventoryQuantity < 1} className={styles["button"]}>
+          <FontAwesomeIcon
+            icon={faPlus}
+            className={
+              inventoryQuantity >= 1 ? styles["enabled"] : styles["disabled"]
+            }
+            onClick={() => increase(bagId)}
+          />
+        </button>
+        {bagQuantity}
         <FontAwesomeIcon
-          icon={faPlus}
-          className={
-            inventoryQuantity >= 1 ? styles["enabled"] : styles["disabled"]
-          }
-          onClick={() => increase(bagId)}
+          icon={faMinus}
+          className={styles["enabled"]}
+          onClick={() => decrease(bagId)}
         />
-      </button>
-      {bagQuantity}
-      <FontAwesomeIcon
-        icon={faMinus}
-        className={styles["enabled"]}
-        onClick={() => decrease(bagId)}
-      />
-    </div>
-  );
-};
+      </div>
+    );
+  }
+);
