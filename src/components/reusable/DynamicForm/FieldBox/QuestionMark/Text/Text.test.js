f6378970ee9b0018560f626271a64ba0ade63cee
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-09 19:44:48 +0300

    Added tests

--- src/components/reusable/DynamicForm/FieldBox/QuestionMark/Text/Text.test.js
+++ src/components/reusable/DynamicForm/FieldBox/QuestionMark/Text/Text.test.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { Text } from './Text';
-import styles from './Text.module.css';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-describe('Text Component', () => {
-  test('renders the text prop correctly', () => {
-    const textContent = 'Hello, World!';
-    
+import { Text } from "./Text";
+
+import styles from "./Text.module.css";
+
+describe("Text Component", () => {
+  test("renders the text prop correctly", () => {
+    const textContent = "Hello, World!";
+
     render(<Text text={textContent} />);
-    
+
     const textElement = screen.getByText(textContent);
-    
-    // Check if the text is rendered correctly
+
     expect(textElement).toBeInTheDocument();
     expect(textElement).toHaveTextContent(textContent);
   });
 
-  test('applies the correct CSS class', () => {
-    const textContent = 'Styled Text';
-    
+  test("applies the correct CSS class", () => {
+    const textContent = "Styled Text";
+
     render(<Text text={textContent} />);
-    
+
     const textElement = screen.getByText(textContent);
-    
-    // Check if the correct CSS class is applied
+
     expect(textElement).toHaveClass(styles["text"]);
   });
 });
