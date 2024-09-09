7052ab834c499073fbeb746accdb01e35fe2c24f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-09 15:54:02 +0300

    Added test

--- src/components/common/OrderSummary/OrderSummary.test.js
+++ src/components/common/OrderSummary/OrderSummary.test.js
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import { OrderSummary } from "./OrderSummary";
-import { useBagContext } from "../../../contexts/BagContext";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
-// Mock the useBagContext hook
+import { OrderSummary } from "./OrderSummary";
+
+import { useBagContext } from "../../../contexts/BagContext";
+
 jest.mock("../../../contexts/BagContext");
 
 describe("OrderSummary Component", () => {
   beforeEach(() => {
-    // Mock the useBagContext hook to return a specific total price
     useBagContext.mockReturnValue({
-      totalPrice: 150.0,
+      totalPrice: 0,
     });
   });
 
@@ -31,18 +31,13 @@ describe("OrderSummary Component", () => {
   it("should render the Order Summary with correct titles and prices", () => {
     renderOrderSummaryWithRoute("/users/shopping-bag");
 
-    // Check if the "Order Summary" title is displayed
     expect(screen.getByText("Order Summary")).toBeInTheDocument();
 
-    // Check if "Subtotal" and the correct total price are displayed
     expect(screen.getByText("Subtotal")).toBeInTheDocument();
 
-    // Check if "Shipping" is displayed correctly
     expect(screen.getByText("Shipping")).toBeInTheDocument();
     expect(screen.getByText("Complimentary")).toBeInTheDocument();
 
-    // Check if "Total" and the correct total price are displayed
     expect(screen.getByText("Total")).toBeInTheDocument();
   });
-
 });
