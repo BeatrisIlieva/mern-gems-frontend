3dab723497f13f52b663ff7bdd3be26df660650c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-09 18:22:52 +0300

    Added tests

--- src/components/layout/Header/MainHeader/MainHeader.test.js
+++ src/components/layout/Header/MainHeader/MainHeader.test.js
@@ -1,58 +1,54 @@
-// MainHeader.test.js
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { useBagContext } from '../../../../contexts/BagContext';
-import { useWishlistContext } from '../../../../contexts/WishlistContext';
-import { MainHeader } from './MainHeader';
-import { HorizontalLine } from '../../../reusable/HorizontalLine/HorizontalLine';
-import { NavLinkItem } from './NavLinkItem/NavLinkItem';
+import { MainHeader } from "./MainHeader";
 
-// Mock the contexts
-jest.mock('../../../../contexts/BagContext', () => ({
-  useBagContext: jest.fn()
+import { useBagContext } from "../../../../contexts/BagContext";
+import { useWishlistContext } from "../../../../contexts/WishlistContext";
+
+jest.mock("../../../../contexts/BagContext", () => ({
+  useBagContext: jest.fn(),
 }));
 
-jest.mock('../../../../contexts/WishlistContext', () => ({
-  useWishlistContext: jest.fn()
+jest.mock("../../../../contexts/WishlistContext", () => ({
+  useWishlistContext: jest.fn(),
 }));
 
-describe('MainHeader Component', () => {
+describe("MainHeader Component", () => {
   beforeEach(() => {
     useBagContext.mockReturnValue({ bagTotalQuantity: 3 });
     useWishlistContext.mockReturnValue({ wishlistTotalQuantity: 5 });
   });
 
-  test('renders with the correct number of NavLinkItem components', () => {
+  test("renders with the correct number of NavLinkItem components", () => {
     render(
       <Router>
         <MainHeader />
       </Router>
     );
 
-    // Check if NavLinkItems are rendered with the correct props
-    expect(screen.getByText('Collection')).toBeInTheDocument();
-    expect(screen.getByText('Wishlist')).toBeInTheDocument();
-    expect(screen.getByText('My Bag')).toBeInTheDocument();
-    expect(screen.getByText('Account')).toBeInTheDocument();
+    expect(screen.getByText("Collection")).toBeInTheDocument();
+    expect(screen.getByText("Wishlist")).toBeInTheDocument();
+    expect(screen.getByText("My Bag")).toBeInTheDocument();
+    expect(screen.getByText("Account")).toBeInTheDocument();
 
-    // Check for counts
-    expect(screen.getByText('(5)')).toBeInTheDocument(); // Wishlist count
-    expect(screen.getByText('(3)')).toBeInTheDocument(); // Bag count
+    expect(screen.getByText("(5)")).toBeInTheDocument();
+    expect(screen.getByText("(3)")).toBeInTheDocument();
   });
 
-  test('renders the logo image', () => {
+  test("renders the logo image", () => {
     render(
       <Router>
         <MainHeader />
       </Router>
     );
 
-    const logoImage = screen.getByAltText('logo-image');
+    const logoImage = screen.getByAltText("logo-image");
     expect(logoImage).toBeInTheDocument();
-    expect(logoImage).toHaveAttribute('src', 'https://res.cloudinary.com/deztgvefu/image/upload/v1724933359/forget-me-not-collection/miniImages/Screenshot_2024-08-29_at_15.08.13_ycwzhl.png');
+    expect(logoImage).toHaveAttribute(
+      "src",
+      "https://res.cloudinary.com/deztgvefu/image/upload/v1724933359/forget-me-not-collection/miniImages/Screenshot_2024-08-29_at_15.08.13_ycwzhl.png"
+    );
   });
-
 });
