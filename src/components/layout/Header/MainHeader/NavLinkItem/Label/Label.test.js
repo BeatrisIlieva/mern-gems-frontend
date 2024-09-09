318ca87de9050fb6c6a2c7045b46e1c64c41e2c5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-09 16:14:08 +0300

    Added test

--- src/components/layout/Header/MainHeader/NavLinkItem/Label/Label.test.js
+++ src/components/layout/Header/MainHeader/NavLinkItem/Label/Label.test.js
@@ -1,15 +1,13 @@
 import { render, screen } from "@testing-library/react";
+
 import { Label } from "./Label";
 
-// Test suite for the Label component
 describe("Label Component", () => {
   it("renders the correct title", () => {
     const mockTitle = "Test Title";
 
-    // Render the Label component with the mock title prop
     render(<Label title={mockTitle} />);
 
-    // Check if the title is rendered correctly
     const labelElement = screen.getByRole("heading", { level: 3 });
     expect(labelElement).toHaveTextContent(mockTitle);
   });
@@ -17,10 +15,8 @@ describe("Label Component", () => {
   it("applies the correct CSS class", () => {
     const mockTitle = "Test Title";
 
-    // Render the Label component
     render(<Label title={mockTitle} />);
 
-    // Check if the correct CSS class is applied
     const labelElement = screen.getByRole("heading", { level: 3 });
     expect(labelElement).toHaveClass("label");
   });
