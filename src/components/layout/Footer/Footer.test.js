ac574ae6ea094d4feb0a847276ca2241183116dd
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-11 20:05:00 +0300

    Updated tests

--- src/components/layout/Footer/Footer.test.js
+++ src/components/layout/Footer/Footer.test.js
@@ -5,10 +5,6 @@ import { Footer } from "./Footer";
 describe("Footer Component", () => {
   test("renders footer with correct content", () => {
     render(<Footer />);
-    expect(
-      screen.getByText(
-        "© 2024 MERN Gems"
-      )
-    ).toBeInTheDocument();
+    expect(screen.getByText("© 2024 MERN Gems")).toBeInTheDocument();
   });
 });
