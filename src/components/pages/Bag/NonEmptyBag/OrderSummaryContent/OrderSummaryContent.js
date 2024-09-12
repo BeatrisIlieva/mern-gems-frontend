3c2ec0aab605fc62487d9790998d4ee840df8d70
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 21:01:21 +0300

    Updated Switch Language functionality

--- src/components/pages/Bag/NonEmptyBag/OrderSummaryContent/OrderSummaryContent.js
+++ src/components/pages/Bag/NonEmptyBag/OrderSummaryContent/OrderSummaryContent.js
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 import { OrderSummary } from "../../../../common/OrderSummary/OrderSummary";
 import { Button } from "../../../../reusable/Button/Button";
 
+import { useLanguageContext } from "../../../../../contexts/LanguageContext";
+
+import { CONTINUE_CHECKOUT_BUTTON_NAMING } from "../../../../../constants/languageRelated";
+
 import styles from "./OrderSummaryContent.module.css";
 
 export const OrderSummaryContent = () => {
+  const { language } = useLanguageContext();
+
   return (
     <>
       <OrderSummary />
       <Link to={"/checkout"} className={styles["no-decoration"]}>
         <Button
-          title={"Continue Checkout"}
+          title={CONTINUE_CHECKOUT_BUTTON_NAMING[language]}
           buttonIsDisabled={false}
           variant={"gray"}
         />
