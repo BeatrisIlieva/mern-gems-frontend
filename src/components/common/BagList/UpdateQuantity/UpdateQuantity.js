7600fcb99e12528590785bcd323769076625bd4c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 19:51:47 +0300

    Updated Switch Language functionality

--- src/components/common/BagList/UpdateQuantity/UpdateQuantity.js
+++ src/components/common/BagList/UpdateQuantity/UpdateQuantity.js
@@ -1,6 +1,9 @@
 import { memo } from "react";
 
 import { useBagContext } from "../../../../contexts/BagContext";
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
+
+import { UPDATING_NAMING } from "./constants/languageRelated";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -16,35 +19,44 @@ export const UpdateQuantity = memo(
     isProcessing,
     processingBagId,
   }) => {
+    const { language } = useLanguageContext();
+
     const { increase, decrease } = useBagContext();
 
     return (
-      <div className={styles["update-quantity"]} data-testid="increase-button">
-        <button
-          disabled={inventoryQuantity < 1 || isProcessing}
-          className={styles["button"]}
+      <div className={styles["wrapper"]}>
+        {isProcessing && processingBagId === bagId && (
+          <span className={styles["updating"]}>
+            {UPDATING_NAMING[language]}
+          </span>
+        )}
+        <div
+          className={styles["update-quantity"]}
+          data-testid="increase-button"
         >
+          <button
+            disabled={inventoryQuantity < 1 || isProcessing}
+            className={styles["button"]}
+          >
+            <FontAwesomeIcon
+              data-testid="increase-icon"
+              icon={faPlus}
+              className={
+                inventoryQuantity < 1 || isProcessing
+                  ? styles["disabled"]
+                  : styles["enabled"]
+              }
+              onClick={() => increase(bagId)}
+            />
+          </button>
+          {bagQuantity}
           <FontAwesomeIcon
-            data-testid="increase-icon"
-            icon={faPlus}
-            className={
-              inventoryQuantity < 1 || isProcessing
-                ? styles["disabled"]
-                : styles["enabled"]
-            }
-            onClick={() => increase(bagId)}
+            data-testid="decrease-icon"
+            icon={faMinus}
+            onClick={() => decrease(bagId)}
+            className={isProcessing ? styles["disabled"] : styles["enabled"]}
           />
-          {isProcessing && processingBagId === bagId && (
-            <span className={styles["updating"]}>Updating...</span>
-          )}
-        </button>
-        {bagQuantity}
-        <FontAwesomeIcon
-          data-testid="decrease-icon"
-          icon={faMinus}
-          onClick={() => decrease(bagId)}
-          className={isProcessing ? styles["disabled"] : styles["enabled"]}
-        />
+        </div>
       </div>
     );
   }
---
+++ src/components/common/BagList/UpdateQuantity/constants/languageRelated.js
@@ -0,0 +1,5 @@
+export const UPDATING_NAMING = {
+  English: "Updating...",
+  Bulgarian: "Обновяване...",
+  Chinese: "更新中...",
+};
