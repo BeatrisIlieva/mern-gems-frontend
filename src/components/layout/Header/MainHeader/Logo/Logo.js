ee6352467b5f8b42638aa16dca2fe105352e866f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 17:05:14 +0300

    Updated Header

---
+++ src/components/layout/Header/MainHeader/Logo/Logo.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+import styles from "./Logo.module.css";
+
+export const Logo = () => {
+  return (
+    <>
+      <div className={styles["text-wrapper"]}>
+        <div className={styles["text-thumbnail"]}>
+          <img
+            className={styles["logo-text"]}
+            src="https://res.cloudinary.com/deztgvefu/image/upload/v1725291440/forget-me-not-collection/miniImages/Screenshot_2024-08-29_at_15.08.13_ycwzhl_1_qybyju.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <Link to="/" className={styles["logo-wrapper"]}>
+        <div className={styles["thumbnail"]}>
+          <img
+            className={styles["logo-image"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1726147711/forget-me-not-collection/miniImages/logo2_zfmuo1.png"
+            }
+            alt="logo-image"
+          />
+        </div>
+      </Link>
+    </>
+  );
+};
--- src/components/layout/Header/MainHeader/MainHeader.js
+++ src/components/layout/Header/MainHeader/MainHeader.js
@@ -1,13 +1,14 @@
 import { useMemo } from "react";
-import { Link } from "react-router-dom";
 
 import { HorizontalLine } from "../../../reusable/HorizontalLine/HorizontalLine";
 import { NavLinkItem } from "./NavLinkItem/NavLinkItem";
 import { SwitchLanguage } from "../../../common/SwitchLanguage/SwitchLanguage";
+import { Logo } from "./Logo/Logo";
 
 import { useLanguageContext } from "../../../../contexts/LanguageContext";
 import { useBagContext } from "../../../../contexts/BagContext";
 import { useWishlistContext } from "../../../../contexts/WishlistContext";
+import { useAuthenticationContext } from "../../../../contexts/AuthenticationContext";
 
 import { LABEL_TITLES } from "./constants/labelTitles";
 
@@ -19,7 +20,6 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./MainHeader.module.css";
-import { useAuthenticationContext } from "../../../../contexts/AuthenticationContext";
 
 export const MainHeader = () => {
   const { language } = useLanguageContext();
@@ -41,27 +41,8 @@ export const MainHeader = () => {
 
   return (
     <header className={styles["header"]}>
-      <div className={styles["text-wrapper"]}>
-        <div className={styles["text-thumbnail"]}>
-          <img
-            className={styles["logo-text"]}
-            src="https://res.cloudinary.com/deztgvefu/image/upload/v1725291440/forget-me-not-collection/miniImages/Screenshot_2024-08-29_at_15.08.13_ycwzhl_1_qybyju.png"
-            alt=""
-          />
-        </div>
-      </div>
-      <Link to="/" className={styles["logo-wrapper"]}>
-        <div className={styles["thumbnail"]}>
-          <img
-            className={styles["logo-image"]}
-            src={
-              "https://res.cloudinary.com/deztgvefu/image/upload/v1726147711/forget-me-not-collection/miniImages/logo2_zfmuo1.png"
-            }
-            alt="logo-image"
-          />
-        </div>
-      </Link>
       {isAuthenticated && <SwitchLanguage />}
+      <Logo />
       <nav className={styles["nav"]}>
         <ul role="list" className={styles["list"]}>
           <li className={styles["list-item"]}>
