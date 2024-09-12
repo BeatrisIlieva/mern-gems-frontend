b0b6308a0b4e3ce07f9895bccf86de15239579c8
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 20:39:55 +0300

    Updated Switch Language functionality

--- src/components/common/BagHeader/Delivery/Delivery.js
+++ src/components/common/BagHeader/Delivery/Delivery.js
@@ -2,16 +2,22 @@ import { memo } from "react";
 
 import { LargeTitle } from "../../../reusable/LargeTitle/LargeTitle";
 
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
+
+import { DELIVERY_NAMING } from "./constants/languageRelated";
+
 import { faTruck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./Delivery.module.css";
 
 export const Delivery = memo(() => {
+  const { language } = useLanguageContext();
+
   return (
     <div className={styles["delivery"]}>
       <FontAwesomeIcon icon={faTruck} className={styles["icon"]} />
-      <LargeTitle title={"Delivery"} variant={"italic"} />
+      <LargeTitle title={DELIVERY_NAMING[language]} variant={"italic"} />
     </div>
   );
 });
---
+++ src/components/common/BagHeader/Delivery/constants/languageRelated.js
@@ -0,0 +1,5 @@
+export const DELIVERY_NAMING = {
+    English: "Delivery",
+    Chinese: "交付",
+    Bulgarian: "Доставка"
+}
