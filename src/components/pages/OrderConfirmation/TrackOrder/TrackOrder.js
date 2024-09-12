d9e3426dab3147ef270be6a6a449c473298d8a09
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 13:24:01 +0300

    Updated CSS

--- src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.js
+++ src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
-import { NormalTitle } from "../../../reusable/NormalTitle/NormalTitle";
-
 import styles from "./TrackOrder.module.css";
 
 export const TrackOrder = () => {
   return (
     <Link to={"/users/account"} className={styles["link"]}>
-      <span className={styles["gray"]}>You can track your order status in your </span>
+      <span className={styles["gray"]}>
+        You can track your order status in your{" "}
+      </span>
       <span className={styles["violet"]}>Account</span>
     </Link>
   );
