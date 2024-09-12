2e9e4fdea85cb70f8346d8ab2b31e6dad694a1bb
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 20:50:35 +0300

    Updated Switch Language functionality

--- src/components/common/BagHeader/BagCount/BagCount.js
+++ src/components/common/BagHeader/BagCount/BagCount.js
@@ -1,17 +1,22 @@
 import { memo } from "react";
 
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
 import { useBagContext } from "../../../../contexts/BagContext";
+
+import { ITEM_NAMING } from "./constants/languageRelated";
 
 import styles from "./BagCount.module.css";
 
 export const BagCount = memo(() => {
+  const { language } = useLanguageContext();
+
   const { bagTotalQuantity } = useBagContext();
 
   return (
     <h4 className={styles["title"]}>
       {bagTotalQuantity > 1
-        ? `(${bagTotalQuantity} items)`
-        : `(${bagTotalQuantity} item)`}
+        ? `(${bagTotalQuantity} ${ITEM_NAMING[language]["plural"]})`
+        : `(${bagTotalQuantity} ${ITEM_NAMING[language]["singular"]})`}
     </h4>
   );
 });
---
+++ src/components/common/BagHeader/BagCount/constants/languageRelated.js
@@ -0,0 +1,5 @@
+export const ITEM_NAMING = {
+    English: {plural: "items", singular: "item"},
+    Bulgarian: {plural: "броя", singular: "брой"},
+    Chinese: {plural: "件商品", singular: "件商品"}
+}
