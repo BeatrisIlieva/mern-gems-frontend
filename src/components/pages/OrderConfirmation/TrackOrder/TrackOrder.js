779fd5d29944ac0676be7685475a92ee1f95ce59
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 13:23:49 +0300

    Updated CSS

--- src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.js
+++ src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.js
@@ -6,14 +6,9 @@ import styles from "./TrackOrder.module.css";
 
 export const TrackOrder = () => {
   return (
-    <div className={styles["link-to-account"]}>
-      <NormalTitle
-        title={"You can track your order status in your"}
-        variant={"regular"}
-      />
-      <Link to={"/users/account"} className={styles["link"]}>
-        Account
-      </Link>
-    </div>
+    <Link to={"/users/account"} className={styles["link"]}>
+      <span className={styles["gray"]}>You can track your order status in your </span>
+      <span className={styles["violet"]}>Account</span>
+    </Link>
   );
 };
