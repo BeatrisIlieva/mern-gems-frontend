3bfb92c3992fd2cfca125dc38bf004409b00cda1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 11:59:35 +0300

    Updated constants

--- src/constants/cVVCode.js
+++ src/constants/cVVCode.js
@@ -1,4 +1,3 @@
 export const CVV_CODE_PATTERN = /^\d{3}$/;
 
-export const CVV_CODE_PATTERN_ERROR_MESSAGE =
-  "The CVV code should be exactly 3 digits long";
+export const CVV_CODE_PATTERN_ERROR_MESSAGE = "* This field requires 3 digits";
--- src/constants/cardNumber.js
+++ src/constants/cardNumber.js
@@ -1,5 +1,4 @@
 export const LONG_CARD_NUMBER_PATTERN = /^\d{16}$/;
 
 export const LONG_CARD_NUMBER_PATTERN_ERROR_MESSAGE =
-  "The card number should be exactly 16 digits long";
-
+  "* This field requires 16 digits";
