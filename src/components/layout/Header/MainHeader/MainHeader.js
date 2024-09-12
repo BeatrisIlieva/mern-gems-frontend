d6c869397a9dafb05648ffdfb145f1e6294aa997
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 16:58:56 +0300

    Updated Switch Language functionality

--- src/components/layout/Header/MainHeader/MainHeader.js
+++ src/components/layout/Header/MainHeader/MainHeader.js
@@ -42,6 +42,26 @@ export const MainHeader = () => {
 
   return (
     <header className={styles["header"]}>
+      <div className={styles["text-wrapper"]}>
+        <div className={styles["text-thumbnail"]}>
+          <img
+            className={styles["logo-text"]}
+            src="https://res.cloudinary.com/deztgvefu/image/upload/v1725291440/forget-me-not-collection/miniImages/Screenshot_2024-08-29_at_15.08.13_ycwzhl_1_qybyju.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <Link to="/" className={styles["logo-wrapper"]}>
+        <div className={styles["thumbnail"]}>
+          <img
+            className={styles["logo-image"]}
+            src={
+              "https://res.cloudinary.com/deztgvefu/image/upload/v1726147711/forget-me-not-collection/miniImages/logo2_zfmuo1.png"
+            }
+            alt="logo-image"
+          />
+        </div>
+      </Link>
       {isAuthenticated && <SwitchLanguage />}
       <nav className={styles["nav"]}>
         <ul role="list" className={styles["list"]}>
@@ -61,22 +81,9 @@ export const MainHeader = () => {
             />
           </li>
           <li className={styles["list-item"]}>
-            <Link to="/">
-              <div className={styles["thumbnail"]}>
-                <img
-                  className={styles["logo-image"]}
-                  src={
-                    "https://res.cloudinary.com/deztgvefu/image/upload/v1724933359/forget-me-not-collection/miniImages/Screenshot_2024-08-29_at_15.08.13_ycwzhl.png"
-                  }
-                  alt="logo-image"
-                />
-              </div>
-            </Link>
-          </li>
-          <li className={styles["list-item"]}>
             <NavLinkItem
               to={"/users/shopping-bag"}
-              label={"My Bag"}
+              label={LABEL_TITLES["My Bag"][language]}
               count={memoizedBagTotalQuantity}
               icon={faBagShopping}
             />
@@ -84,7 +91,7 @@ export const MainHeader = () => {
           <li className={styles["list-item"]}>
             <NavLinkItem
               to={"/users/account"}
-              label={"Account"}
+              label={LABEL_TITLES["Account"][language]}
               icon={faUser}
             />
           </li>
--- src/components/layout/Header/MainHeader/constants/labelTitles.js
+++ src/components/layout/Header/MainHeader/constants/labelTitles.js
@@ -1,4 +1,6 @@
 export const LABEL_TITLES = {
   Collection: { English: "Collection", Bulgarian: "Колекция" },
   Wishlist: { English: "Wishlist", Bulgarian: "Любими" },
+  "My Bag": { English: "My Bag", Bulgarian: "Моята чанта" },
+  Account: { English: "Account", Bulgarian: "Акаунт" },
 };
