bda2a3e8ddb391312efe455efc21ad1212485b2e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-12 20:56:58 +0300

    Updated Switch Language functionality

--- src/constants/languageRelated.js
+++ src/constants/languageRelated.js
@@ -3,3 +3,9 @@ export const SIZE_NAMING = {
   Chinese: "尺寸",
   Bulgarian: "Размер",
 };
+
+export const CONTINUE_CHECKOUT_BUTTON_NAMING = {
+  English: "Continue Checkout",
+  Chinese: "继续结账",
+  Bulgarian: "Продължи към плащане",
+};
