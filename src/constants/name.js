c403884bdc03911a1b7eba02e650949a1ef2c57c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-13 18:59:36 +0300

    Updated switch language functionality

--- src/constants/name.js
+++ src/constants/name.js
@@ -11,4 +11,8 @@ export const NAME_PATTERN = new RegExp(
   `^[A-Za-z\\u0400-\\u04FF\\u4E00-\\u9FFF]{${NAME_LENGTH.MIN},${NAME_LENGTH.MAX}}$`
 );
 
-export const NAME_ERROR_MESSAGE = `* This field requires ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} letters`;
+export const NAME_ERROR_MESSAGE = {
+  English: `* This field requires ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} letters`,
+  Chinese: `* 该字段要求 ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} 个字母`,
+  Bulgarian: `* Полето трябва да съдържа ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} букви`,
+};
