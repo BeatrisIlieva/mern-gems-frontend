4f36d1fdcbcf41bb10069a82666bd7dba3da5187
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-13 20:02:56 +0300

    Updated switch language functionality

--- src/constants/zipCode.js
+++ src/constants/zipCode.js
@@ -4,6 +4,8 @@ const ZIP_CODE_LENGTH = {
 };
 
 module.exports.ZIP_CODE_PATTERN = new RegExp(
-  `^[A-Za-z0-9 ]{${ZIP_CODE_LENGTH.MIN},${ZIP_CODE_LENGTH.MAX}}$`
+  `^[\\p{L}\\p{N} ]{${ZIP_CODE_LENGTH.MIN},${ZIP_CODE_LENGTH.MAX}}$`,
+  "u"
 );
+
 module.exports.ZIP_CODE_ERROR_MESSAGE = `This field requires ${ZIP_CODE_LENGTH.MIN}-${ZIP_CODE_LENGTH.MAX} characters`;
