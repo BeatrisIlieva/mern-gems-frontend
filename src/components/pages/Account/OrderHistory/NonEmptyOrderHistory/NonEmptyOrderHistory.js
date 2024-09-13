0a1d90e170b8b0e1364a26e87991fe457446d00e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-13 12:42:21 +0300

    Updated switch language functionality

--- src/components/pages/Account/OrderHistory/NonEmptyOrderHistory/NonEmptyOrderHistory.js
+++ src/components/pages/Account/OrderHistory/NonEmptyOrderHistory/NonEmptyOrderHistory.js
@@ -1,12 +1,20 @@
 import { OrderHistoryList } from "./OrderHistoryList/OrderHistoryList";
 import { LargeTitle } from "../../../../reusable/LargeTitle/LargeTitle";
 
+import { useLanguageContext } from "../../../../../contexts/LanguageContext";
+
+import { ORDER_HISTORY_NAMING } from "../constants/languageRelated";
+
 import styles from "./NonEmptyOrderHistory.module.css";
 
 export const NonEmptyOrderHistory = ({ orderItems }) => {
+  const { language } = useLanguageContext();
+
+  const title = ORDER_HISTORY_NAMING[language];
+
   return (
     <>
-      <LargeTitle title={"Order History"} textAlign={"align-center"} />
+      <LargeTitle title={title} textAlign={"align-center"} />
       <ul role="list" className={styles["non-empty-order-history"]}>
         {orderItems.map((item) => (
           <li key={item._id}>
