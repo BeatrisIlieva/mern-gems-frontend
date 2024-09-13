b4555f28287ac5da2fabce0eeeea6f28f68d251c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-13 20:01:23 +0300

    Updated switch language functionality

--- src/constants/name.js
+++ src/constants/name.js
@@ -3,5 +3,9 @@ const NAME_LENGTH = {
   MAX: 255,
 };
 
-module.exports.NAME_PATTERN = new RegExp(`^[A-Za-z]{${NAME_LENGTH.MIN},${NAME_LENGTH.MAX}}$`);
+module.exports.NAME_PATTERN = new RegExp(
+  `^(?!.*--)(?!.*  )[\\p{L}]+(?:[ -][\\p{L}]+)*$`,
+  "u"
+);
+
 module.exports.NAME_ERROR_MESSAGE = `This field requires ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} letters`;
