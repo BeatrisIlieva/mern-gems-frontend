c129378ba597d78590ba1593184dcaae3f5d51fa
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-13 19:54:49 +0300

    Updated switch language functionality

--- src/constants/apartment.js
+++ src/constants/apartment.js
@@ -3,10 +3,6 @@ const APARTMENT_LENGTH = {
   MAX: 10,
 };
 
-// export const APARTMENT_PATTERN = new RegExp(
-//   `^[a-zA-Z0-9]{${APARTMENT_LENGTH.MIN},${APARTMENT_LENGTH.MAX}}$`
-// );
-
 export const APARTMENT_PATTERN = new RegExp(
   `^[\\p{L}\\p{N} ]{${APARTMENT_LENGTH.MIN},${APARTMENT_LENGTH.MAX}}$`,
   "u"
