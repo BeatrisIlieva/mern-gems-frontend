761ed9ddf1c2dad155cfe992907b7678e58d9f88
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-13 11:49:49 +0300

    Updated switch language functionality

--- src/components/layout/Header/MiniHeader/MiniHeader.js
+++ src/components/layout/Header/MiniHeader/MiniHeader.js
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 import { NormalTitle } from "../../../reusable/NormalTitle/NormalTitle";
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
+
+import { GO_TO_BAG_BUTTON_NAMING } from "./constants/languageRelated";
 
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,11 +11,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./MiniHeader.module.css";
 
 export const MiniHeader = () => {
+  const { language } = useLanguageContext();
+
   return (
     <header className={styles["header"]}>
       <Link to={"/users/shopping-bag"} className={styles["wrapper"]}>
         <FontAwesomeIcon icon={faArrowLeftLong} className={styles["icon"]} />
-        <NormalTitle title={"Go To Bag"} />
+        <NormalTitle title={GO_TO_BAG_BUTTON_NAMING[language]} />
       </Link>
       <div>
         <img
