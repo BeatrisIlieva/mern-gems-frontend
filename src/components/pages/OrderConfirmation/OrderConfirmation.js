a9fe6995bd87bf8b57beed623afae5d7d55e974d
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 14:08:51 +0300

    Updated switch language functionality

--- src/components/pages/OrderConfirmation/OrderConfirmation.js
+++ src/components/pages/OrderConfirmation/OrderConfirmation.js
@@ -2,9 +2,17 @@ import { InfoMessage } from "../../reusable/InfoMessage/InfoMessage";
 import { TrackOrder } from "./TrackOrder/TrackOrder";
 import { ConfirmationEmail } from "./ConfirmationEmail/ConfirmationEmail";
 
+import { useLanguageContext } from "../../../contexts/LanguageContext";
+
+import { TITLE_BY_LANGUAGE } from "./constants/languageRelated";
+
 import styles from "./OrderConfirmation.module.css";
 
 export const OrderConfirmation = () => {
+  const { language } = useLanguageContext();
+
+  const title = TITLE_BY_LANGUAGE[language];
+
   return (
     <section id={styles["order-confirmation"]} data-testid="order-confirmation">
       <div className={styles["relative-container"]}>
@@ -15,10 +23,7 @@ export const OrderConfirmation = () => {
             alt="butterfly"
           />
         </div>
-        <InfoMessage
-          title={"Thank you for your purchase!"}
-          subtitle={<TrackOrder />}
-        />
+        <InfoMessage title={title} subtitle={<TrackOrder />} />
       </div>
       <ConfirmationEmail />
     </section>
---
+++ src/components/pages/OrderConfirmation/constants/languageRelated.js
@@ -0,0 +1,5 @@
+export const TITLE_BY_LANGUAGE = {
+  English: "Thank you for your purchase!",
+  Chinese: "感谢您的购买!",
+  Bulgarian: "Благодарим Ви за покупката!",
+};
