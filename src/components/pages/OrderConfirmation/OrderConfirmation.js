4d754e888212cbc74e33541fc932f549c21f2da0
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 15:04:17 +0300

    Updated switch language functionality

--- src/components/pages/OrderConfirmation/OrderConfirmation.js
+++ src/components/pages/OrderConfirmation/OrderConfirmation.js
@@ -13,18 +13,29 @@ export const OrderConfirmation = () => {
 
   const title = TITLE_BY_LANGUAGE[language];
 
+  const firstLetter = title.charAt(0);
+
+  const restOfText = title.slice(1);
+
   return (
     <section id={styles["order-confirmation"]} data-testid="order-confirmation">
-      <div className={styles["relative-container"]}>
-        <div className={styles["thumbnail"]} data-testid="butterfly-container">
-          <img
-            className={styles["butterfly"]}
-            src="https://res.cloudinary.com/deztgvefu/image/upload/v1723986117/forget-me-not-collection/miniImages/1042750_d9d98_vfqzme.gif"
-            alt="butterfly"
-          />
-        </div>
-        <InfoMessage title={title} subtitle={<TrackOrder />} />
-      </div>
+      <h1 className={styles["title"]}>
+        <span className={styles["letter-span"]}>
+          {firstLetter}
+          <div
+            className={styles["thumbnail"]}
+            data-testid="butterfly-container"
+          >
+            <img
+              className={styles["butterfly"]}
+              src="https://res.cloudinary.com/deztgvefu/image/upload/v1723986117/forget-me-not-collection/miniImages/1042750_d9d98_vfqzme.gif"
+              alt="butterfly"
+            />
+          </div>
+        </span>
+        <span>{restOfText}</span>
+      </h1>
+      <TrackOrder />
       <ConfirmationEmail />
     </section>
   );
