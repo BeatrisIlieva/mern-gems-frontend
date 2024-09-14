6d3b0ecd557afbefff7cc4a853d68a023ac4ab5b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 14:24:01 +0300

    Updated switch language functionality

--- src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.js
+++ src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.js
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
+
+import { MESSAGE_BY_LANGUAGE } from "./constants/languageRelated";
+
 import styles from "./TrackOrder.module.css";
 
 export const TrackOrder = () => {
+  const { language } = useLanguageContext();
+
+  const message = MESSAGE_BY_LANGUAGE[language];
+
   return (
     <Link to={"/users/account"} className={styles["link"]}>
-      <span className={styles["gray"]}>
-        You can track your order status in your{" "}
-      </span>
-      <span className={styles["violet"]}>Account</span>
+      <span className={styles["violet"]}>{message}</span>
     </Link>
   );
 };
---
+++ src/components/pages/OrderConfirmation/TrackOrder/constants/languageRelated.js
@@ -0,0 +1,5 @@
+export const MESSAGE_BY_LANGUAGE = {
+  English: "You can track your order status in your Account.",
+  Chinese: "您可以在您的账户中跟踪订单状态。",
+  Bulgarian: "Можете да следите статуса на поръчката във Вашия акаунт.",
+};
