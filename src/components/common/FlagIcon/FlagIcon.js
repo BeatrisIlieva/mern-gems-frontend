482b0077d6ef2e82448e39342967373874a6667e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 10:19:54 +0300

    Added switch language functionality

---
+++ src/components/common/FlagIcon/FlagIcon.js
@@ -0,0 +1,45 @@
+import { useLanguageContext } from "../../../contexts/LanguageContext";
+
+import { IMAGE_URLS } from "./constsnts/imageUrls";
+
+import styles from "./FlagIcon.module.css";
+
+export const FlagIcon = () => {
+  const { language, updateLanguage } = useLanguageContext();
+
+  const [selectedImage, setSelectedImage] = useState(IMAGE_URLS[language]);
+
+  const [displayDropdown, setDisplayDropdown] = useState(false);
+
+  const languageChangeHandler = (lang) => {
+    setSelectedImage(lang);
+
+    updateLanguage(lang);
+
+    setDisplayDropdown(false);
+  };
+
+  return (
+    <section className={styles["languages"]}>
+      <button className={styles["button"]} onClick={setDisplayDropdown(true)}>
+        {selectedImage}
+      </button>
+      {displayDropdown && (
+        <div className={styles["dropdown"]}>
+          {Object.keys(IMAGE_URLS)
+            .filter((lang) => lang !== selectedImage)
+            .map((lang) => (
+              <div
+                key={lang}
+                className={styles["thumbnail"]}
+                onClick={() => languageChangeHandler(lang)}
+              >
+                <img src={IMAGE_URLS[lang]} alt={`${lang} flag`} />
+                {lang}
+              </div>
+            ))}
+        </div>
+      )}
+    </section>
+  );
+};
---
+++ src/components/common/FlagIcon/constsnts/imageUrls.js
@@ -0,0 +1,8 @@
+export const IMAGE_URLS = {
+  English:
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1726250851/forget-me-not-collection/miniImages/british-antarctic-territory-flag-icon-in-the-form-of-a-heart-png_2_cnvh6g.webp",
+  Chinese:
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1726250852/forget-me-not-collection/miniImages/china-flag-icon-in-the-form-of-a-heart-png_jeli2i.webp",
+  Bulgarian:
+    "https://res.cloudinary.com/deztgvefu/image/upload/v1726250851/forget-me-not-collection/miniImages/bulgaria-flag-icon-in-the-form-a-heart-png_ajamqn.webp",
+};
