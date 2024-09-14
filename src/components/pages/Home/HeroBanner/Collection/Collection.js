1ff7605836f7143cbc3549b801fb95a181ac0026
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 19:41:48 +0300

    Updated CSS

--- src/components/pages/Home/HeroBanner/Collection/Collection.js
+++ src/components/pages/Home/HeroBanner/Collection/Collection.js
@@ -8,8 +8,13 @@ export const Collection = () => {
       <div className={styles["collection-overlay"]}></div>
       <Description />
       <img
-        className={styles["image"]}
+        className={styles["desktop-image"]}
         src="https://res.cloudinary.com/deztgvefu/image/upload/v1716995569/collections/forgetmenot_rz0umv.png"
+        alt=""
+      />
+      <img
+        className={styles["mobile-image"]}
+        src="https://res.cloudinary.com/deztgvefu/image/upload/v1726330964/forget-me-not-collection/miniImages/hero-mobile_k83pfc.jpg"
         alt=""
       />
     </article>
