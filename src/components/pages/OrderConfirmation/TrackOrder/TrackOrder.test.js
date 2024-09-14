dec19854be7cf8a56c3bd6b8253648ca7f921938
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 16:09:03 +0300

    Updated tests

--- src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.test.js
+++ src/components/pages/OrderConfirmation/TrackOrder/TrackOrder.test.js
@@ -4,7 +4,21 @@ import "@testing-library/jest-dom/extend-expect";
 
 import { TrackOrder } from "./TrackOrder";
 
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
+
+jest.mock("../../../../contexts/LanguageContext", () => ({
+  useLanguageContext: jest.fn(),
+}));
+
 describe("TrackOrder Component", () => {
+  const mockLanguage = "English";
+
+  beforeEach(() => {
+    useLanguageContext.mockReturnValue({ language: mockLanguage });
+
+    jest.clearAllMocks();
+  });
+
   test("renders the TrackOrder component with correct content and link", () => {
     render(
       <MemoryRouter>
