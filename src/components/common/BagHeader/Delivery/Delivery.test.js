b4b9e3473565849049b6b022e48f3478906f3ae3
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 15:44:27 +0300

    Updated tests

--- src/components/common/BagHeader/Delivery/Delivery.test.js
+++ src/components/common/BagHeader/Delivery/Delivery.test.js
@@ -2,9 +2,23 @@ import { render } from "@testing-library/react";
 
 import { Delivery } from "./Delivery";
 
+import { useLanguageContext } from "../../../../contexts/LanguageContext";
+
+jest.mock("../../../../contexts/LanguageContext", () => ({
+  useLanguageContext: jest.fn(),
+}));
+
 import styles from "./Delivery.module.css";
 
 describe("Delivery Component", () => {
+  const mockLanguage = "English";
+  
+  beforeEach(() => {
+    useLanguageContext.mockReturnValue({ language: mockLanguage });
+
+    jest.clearAllMocks();
+  });
+
   test("applies the correct CSS class to the outer div", () => {
     const { container } = render(<Delivery />);
 
