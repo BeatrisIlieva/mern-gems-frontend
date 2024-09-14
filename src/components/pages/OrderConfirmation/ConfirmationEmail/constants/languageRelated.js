f0cb319e4bd3bbcaf804f0bea49e1872c2766098
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-14 14:27:57 +0300

    Updated switch language functionality

---
+++ src/components/pages/OrderConfirmation/ConfirmationEmail/constants/languageRelated.js
@@ -0,0 +1,5 @@
+export const MESSAGE_BY_LANGUAGE = {
+  English: "A confirmation email has been sent to",
+  Chinese: "封确认邮件已经发送到",
+  Bulgarian: "Имейл, потвърждаващ вашата поръчка, бе изпратен на",
+};
