b51eb9196faac001f847a0788d64c5af57b4d2d0
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-15 11:29:56 +0300

    Updated switch language functionality

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,21 +1,13 @@
 const router = require("express").Router();
 
 const paymentManager = require("../managers/paymentManager");
-const userCardDetailsManager = require("../managers/userCardDetailsManager");
 
 router.put("/:userId", async (req, res) => {
   const userId = req.params.userId;
 
-  // const data = { ...req.body };
-
   const { selectedLanguage } = req.body;
 
-  // const cardData = { cardHolder, longCardNumber, cVVCode, expiryDate };
-
   try {
-    // await userCardDetailsManager.update(userId, cardData);
-    // await userCardDetailsManager.update(userId, data);
-
     await paymentManager.create(userId, selectedLanguage);
 
     res.status(204).json();
