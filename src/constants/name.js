8ca70e22ffdc1cb3032ad108e16794eb9ce51662
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-16 10:22:17 +0300

    Updated constants

--- src/constants/name.js
+++ src/constants/name.js
@@ -9,7 +9,7 @@ export const NAME_PATTERN = new RegExp(
 );
 
 export const NAME_ERROR_MESSAGE = {
-  English: `* This field requires ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} letters`,
+  English: `* This field requires ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} characters`,
   Chinese: `* 该字段要求 ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} 个字母`,
   Bulgarian: `* Полето трябва да съдържа ${NAME_LENGTH.MIN}-${NAME_LENGTH.MAX} знака`,
 };
