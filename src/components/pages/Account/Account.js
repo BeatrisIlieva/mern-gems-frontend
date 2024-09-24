8824d566a1b0df8aab63478d1431d69fd7d91608
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-24 09:59:57 +0300

    Updated Account

--- src/components/pages/Account/Account.js
+++ src/components/pages/Account/Account.js
@@ -3,17 +3,28 @@ import { ShippingDetails } from "./ShippingDetails/ShippingDetails";
 import { CardDetails } from "./CardDetails/CardDetails";
 import { AccountManagement } from "./AccountManagement/AccountManagement";
 
+import { useLanguageContext } from "../../../contexts/LanguageContext";
+
+import { HEADING_BY_LANGUAGE } from "./constants/languageRelated";
+
 import styles from "./Account.module.css";
 
 export const Account = () => {
+  const { language } = useLanguageContext();
+
+  const heading = HEADING_BY_LANGUAGE[language];
+
   return (
     <section id={styles["account"]}>
-      <div className={styles["flex-container"]}>
-        <OrderHistory />
-        <ShippingDetails />
-        <CardDetails />
+      <p className={styles["heading"]}>{heading}</p>
+      <div className={styles["wrapper"]}>
+        <div className={styles["flex-container"]}>
+          <OrderHistory />
+          <ShippingDetails />
+          <CardDetails />
+        </div>
+        <AccountManagement />
       </div>
-      <AccountManagement />
     </section>
   );
 };
---
+++ src/components/pages/Account/constants/languageRelated.js
@@ -0,0 +1,4 @@
+export const HEADING_BY_LANGUAGE = {
+  English:
+    "You can access all your previous transactions, set default shipping or billing addresses for faster checkout as well as update your login preferences.",
+};
