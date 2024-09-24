9bb19f7ad7da3546953e2641830c326d9ab61edd
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-24 10:08:00 +0300

    Updated Account

--- src/components/pages/Account/constants/languageRelated.js
+++ src/components/pages/Account/constants/languageRelated.js
@@ -1,4 +1,8 @@
 export const HEADING_BY_LANGUAGE = {
   English:
     "You can access all your previous transactions, set default shipping or billing addresses for faster checkout as well as update your login preferences.",
+  Chinese:
+    "您可以访问所有的过往交易，设置默认的收货地址或账单地址以加快结账速度，还可以更新您的登录偏好设置。",
+  Bulgarian:
+    "Можете да преглеждате всичките си предишни транзакции, да зададете адрес за доставка или метод на плащане по подразбиране за по-лесно пазаруване, както и да актуализирате предпочитанията си за вход.",
 };
