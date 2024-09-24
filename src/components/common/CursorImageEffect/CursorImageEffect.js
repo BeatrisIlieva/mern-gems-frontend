9e35c98388c716c8142adacc2400805576584dc8
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-24 13:37:39 +0300

    Updated CursorImageEffect

--- src/components/common/CursorImageEffect/CursorImageEffect.js
+++ src/components/common/CursorImageEffect/CursorImageEffect.js
@@ -1,38 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import styles from "./CursorImageEffect.module.css"
-import cursorImage from "../../../assets/images/xmark.png"
+import { useState, useEffect } from "react";
+
+import cursorImage from "../../../assets/images/xmark.png";
+
+import styles from "./CursorImageEffect.module.css";
 
 export const CursorImageEffect = () => {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
 
-  // Update cursor position
   const updateCursorPosition = (e) => {
     setCursorPos({ x: e.clientX, y: e.clientY });
   };
 
   useEffect(() => {
-    document.addEventListener('mousemove', updateCursorPosition);
+    document.addEventListener("mousemove", updateCursorPosition);
     return () => {
-      document.removeEventListener('mousemove', updateCursorPosition);
+      document.removeEventListener("mousemove", updateCursorPosition);
     };
-  }, []); // Empty dependency array ensures the effect runs only once
+  }, []);
 
-  // Adjust these offsets based on the image size
-  const imageOffsetX = 25; // Half of image width (50px)
-  const imageOffsetY = 25; // Half of image height (50px)
+  const imageOffsetX = 25;
+  const imageOffsetY = 25;
 
   return (
     <div className={styles["cursor-container"]}>
       <img
-        src={cursorImage} // Replace with your image path
+        src={cursorImage}
         alt="cursor image"
         className={styles["cursor-image"]}
         style={{
-          transform: `translate(${cursorPos.x - imageOffsetX}px, ${cursorPos.y - imageOffsetY}px)`
+          transform: `translate(${cursorPos.x - imageOffsetX}px, ${
+            cursorPos.y - imageOffsetY
+          }px)`,
         }}
       />
-
     </div>
   );
 };
-
