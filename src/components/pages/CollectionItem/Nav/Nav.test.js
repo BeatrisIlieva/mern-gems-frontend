7ee4f1579a03b76eee92f6792e89076245136219
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-24 16:52:46 +0300

    Updated tests

--- src/components/pages/CollectionItem/Nav/Nav.test.js
+++ src/components/pages/CollectionItem/Nav/Nav.test.js
@@ -1,17 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom"; // To mock the router
+import { MemoryRouter } from "react-router-dom";
 import { Nav } from "./Nav";
 import { useLanguageContext } from "../../../../contexts/LanguageContext";
 import { useLocation } from "react-router-dom";
-import { HOME_LINK_TITLE_BY_LANGUAGE, CATEGORY_TITLES_BY_LANGUAGE, COLLECTION_BY_LANGUAGE } from "../../../../constants/languageRelated";
 
-// Mock useLocation
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useLocation: jest.fn(),
 }));
 
-// Mock useLanguageContext
 jest.mock("../../../../contexts/LanguageContext", () => ({
   useLanguageContext: jest.fn(),
 }));
@@ -30,7 +27,6 @@ describe("Nav Component", () => {
   };
 
   it("renders the correct titles based on the language and location", () => {
-    // Mock the language as 'en' (English) and set the location to '/collection/bracelets'
     mockLanguageContext("English");
     mockLocation("/collection/bracelets");
 
@@ -40,17 +36,8 @@ describe("Nav Component", () => {
       </MemoryRouter>
     );
 
-    // Check that the home link displays the correct text
-    // const homeLinkTitle = HOME_LINK_TITLE_BY_LANGUAGE["English"];
     expect(screen.getByText("Home")).toBeInTheDocument();
 
-    // Check that the collection link displays the correct text
-    // const collectionLinkTitle = COLLECTION_BY_LANGUAGE["English"];
     expect(screen.getByText("Collection")).toBeInTheDocument();
-
-    // Check that the category (bracelets) is capitalized and correct
-    // const categoryTitle = CATEGORY_TITLES_BY_LANGUAGE["Bracelets"]["English"];
-    // expect(screen.getByText(categoryTitle)).toBeInTheDocument();
   });
-
 });
