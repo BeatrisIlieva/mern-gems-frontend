8199562d11cdf29da2eb790a5654fc899dd0d918
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-24 17:23:08 +0300

    Updated tests

--- src/components/pages/Account/reusable/SectionContainer/SectionContainer.test.js
+++ src/components/pages/Account/reusable/SectionContainer/SectionContainer.test.js
@@ -25,11 +25,6 @@ describe("SectionContainer Component", () => {
 
     const button = screen.getByText("Click Me");
     expect(button).toBeInTheDocument();
-
-    // const icon = screen
-    //   .getByRole("button")
-    //   .querySelector('i[data-icon="plus"]');
-    // expect(icon).toBeInTheDocument();
   });
 
   test("triggers callBackFunction on button click", () => {
